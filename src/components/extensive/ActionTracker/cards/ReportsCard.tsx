fd85046df575ac8866a5363f8bd7151a0d4a0ae8
Milad <milad@3sidedcube.com>
2023-12-05 11:21:04 +0000

    filter out actions when target is null on report action cards

--- src/components/extensive/ActionTracker/cards/ReportsCard.tsx
+++ src/components/extensive/ActionTracker/cards/ReportsCard.tsx
@@ -30,60 +30,62 @@ const ReportsCard = ({ actions }: ReportsCardProps) => {
   const reportActions = useMemo(() => {
     if (!actions) return [];
 
-    return sortByDate(actions, "target.due_at").map(action => {
-      const target = action.target;
-      // Project is either the target itself or, if it has a project object, it is that.
-      const project = action.target?.project ?? action.target;
-      const type = action.targetable_type;
-      const status = getEntityCombinedStatus(target);
-      // When true, the action is cleared on the client side when the user clicks it, otherwise this is handled BED side.
-      let canClearActionClientSide = status === "approved" || status === "rejected";
+    return sortByDate(actions, "target.due_at")
+      .filter(action => !!action.target)
+      .map(action => {
+        const target = action.target;
+        // Project is either the target itself or, if it has a project object, it is that.
+        const project = action.target?.project ?? action.target;
+        const type = action.targetable_type;
+        const status = getEntityCombinedStatus(target);
+        // When true, the action is cleared on the client side when the user clicks it, otherwise this is handled BED side.
+        let canClearActionClientSide = status === "approved" || status === "rejected";
 
-      let dueText = t("<strong>Due:</strong> {date}", { date: format(target?.due_at) });
-      let subtitle;
-      let ctaText;
-      let ctaLink;
+        let dueText = t("<strong>Due:</strong> {date}", { date: format(target?.due_at) });
+        let subtitle;
+        let ctaText;
+        let ctaLink;
 
-      switch (type) {
-        case "ProjectReport": {
-          ctaText = t("View Project Report");
-          subtitle = action.text;
+        switch (type) {
+          case "ProjectReport": {
+            ctaText = t("View Project Report");
+            subtitle = action.text;
 
-          if (status?.includes("due")) {
-            ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
-          } else ctaLink = getEntityDetailPageLink("project-reports", target?.uuid);
-          break;
+            if (status?.includes("due")) {
+              ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
+            } else ctaLink = getEntityDetailPageLink("project-reports", target?.uuid);
+            break;
+          }
+          case "NurseryReport": {
+            ctaText = t("View Nursery Report");
+            subtitle = t("<strong>Nursery:</strong> {name}", { name: target?.name });
+
+            if (status?.includes("due")) {
+              ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
+            } else ctaLink = `reports/nursery-report/${target?.uuid}`;
+            break;
+          }
+          case "SiteReport": {
+            ctaText = t("View Site Report");
+            subtitle = t("<strong>Site:</strong> {name}", { name: target?.name });
+
+            if (status?.includes("due")) {
+              ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
+            } else ctaLink = `reports/site-report/${target?.uuid}`;
+          }
         }
-        case "NurseryReport": {
-          ctaText = t("View Nursery Report");
-          subtitle = t("<strong>Nursery:</strong> {name}", { name: target?.name });
 
-          if (status?.includes("due")) {
-            ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
-          } else ctaLink = `reports/nursery-report/${target?.uuid}`;
-          break;
-        }
-        case "SiteReport": {
-          ctaText = t("View Site Report");
-          subtitle = t("<strong>Site:</strong> {name}", { name: target?.name });
-
-          if (status?.includes("due")) {
-            ctaLink = `/project/${target?.project.uuid}/reporting-task/${target?.task_uuid}`;
-          } else ctaLink = `reports/site-report/${target?.uuid}`;
-        }
-      }
-
-      return {
-        ...getActionCardStatusMapper(t)[status!],
-        ctaLink,
-        ctaText,
-        title: project?.name,
-        subtitle: `${subtitle ? `${subtitle}\n` : ""}${target?.due_at ? dueText : ""}`,
-        onClick: () => {
-          canClearActionClientSide && action.uuid && clearAction({ pathParams: { uuid: action.uuid } });
-        }
-      } as ActionTrackerCardRowProps;
-    });
+        return {
+          ...getActionCardStatusMapper(t)[status!],
+          ctaLink,
+          ctaText,
+          title: project?.name,
+          subtitle: `${subtitle ? `${subtitle}\n` : ""}${target?.due_at ? dueText : ""}`,
+          onClick: () => {
+            canClearActionClientSide && action.uuid && clearAction({ pathParams: { uuid: action.uuid } });
+          }
+        } as ActionTrackerCardRowProps;
+      });
   }, [actions, format, clearAction, t]);
 
   return (
