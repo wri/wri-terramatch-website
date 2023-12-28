c649fe68f3b347664a005435b981bdc102bc08f6
cesarLima1 <cesmaulim@gmail.com>
2023-12-28 11:55:50 -0400

    feat: add restrictions to create and delete reporting frameworks

--- src/admin/components/App.tsx
+++ src/admin/components/App.tsx
@@ -8,6 +8,7 @@ import UserIcon from "@mui/icons-material/Group";
 import LanguageIcon from "@mui/icons-material/Language";
 import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 import SummarizeIcon from "@mui/icons-material/Summarize";
+import { useEffect, useState } from "react";
 import { Admin, Resource } from "react-admin";
 
 import { authProvider } from "@/admin/apiProvider/authProvider";
@@ -18,118 +19,137 @@ import { theme } from "@/admin/components/theme";
 import modules from "../modules";
 import AdminLoginPage from "../pages/AdminLoginPage";
 
-const App = () => (
-  <Admin
-    theme={theme}
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    layout={AppLayout}
-    loginPage={AdminLoginPage}
-  >
-    <Resource
-      name={modules.user.ResourceName}
-      list={modules.user.List}
-      show={modules.user.Show}
-      edit={modules.user.Edit}
-      icon={UserIcon}
-    />
-    <Resource
-      name={modules.organisation.ResourceName}
-      list={modules.organisation.List}
-      show={modules.organisation.Show}
-      edit={modules.organisation.Edit}
-      icon={BusinessIcon}
-    />
-    <Resource
-      name={modules.pitch.ResourceName}
-      list={modules.pitch.List}
-      show={modules.pitch.Show}
-      edit={modules.pitch.Edit}
-      icon={ForestIcon}
-    />
-    <Resource
-      name={modules.fundingProgramme.ResourceName}
-      list={modules.fundingProgramme.List}
-      edit={modules.fundingProgramme.Edit}
-      show={modules.fundingProgramme.Show}
-      create={modules.fundingProgramme.Create}
-      icon={AttachMoneyIcon}
-      options={{ label: "Funding Programmes" }}
-    />
-    <Resource
-      name={modules.reportingFramework.ResourceName}
-      list={modules.reportingFramework.List}
-      show={modules.reportingFramework.Show}
-      edit={modules.reportingFramework.Edit}
-      create={modules.reportingFramework.Create}
-      icon={WatchLater}
-      options={{ label: "Reporting Frameworks" }}
-    />
-    <Resource
-      name={modules.application.ResourceName}
-      list={modules.application.List}
-      show={modules.application.Show}
-      icon={LibraryBooksIcon}
-    />
-    <Resource
-      name={modules.stage.ResourceName}
-      show={modules.stage.Show}
-      edit={modules.stage.Edit}
-      create={modules.stage.Create}
-    />
-    <Resource
-      name={modules.form.ResourceName}
-      list={modules.form.List}
-      edit={modules.form.Edit}
-      create={modules.form.Create}
-    />
-    <Resource
-      name={modules.project.ResourceName}
-      list={modules.project.List}
-      show={modules.project.Show}
-      edit={modules.project.Edit}
-      icon={ArticleIcon}
-    />
-    <Resource
-      name={modules.site.ResourceName}
-      list={modules.site.List}
-      show={modules.site.Show}
-      edit={modules.site.Edit}
-      icon={LanguageIcon}
-    />
-    <Resource
-      name={modules.nursery.ResourceName}
-      list={modules.nursery.List}
-      show={modules.nursery.Show}
-      edit={modules.nursery.Edit}
-      icon={FullscreenIcon}
-    />
-    <Resource
-      name={modules.projectReport.ResourceName}
-      list={modules.projectReport.List}
-      show={modules.projectReport.Show}
-      edit={modules.projectReport.Edit}
-      icon={SummarizeIcon}
-      options={{ label: "Project Reports" }}
-    />
-    <Resource
-      name={modules.siteReport.ResourceName}
-      list={modules.siteReport.List}
-      show={modules.siteReport.Show}
-      edit={modules.siteReport.Edit}
-      icon={SummarizeIcon}
-      options={{ label: "Site Reports" }}
-    />
-    <Resource
-      name={modules.nurseryReport.ResourceName}
-      list={modules.nurseryReport.List}
-      show={modules.nurseryReport.Show}
-      edit={modules.nurseryReport.Edit}
-      icon={SummarizeIcon}
-      options={{ label: "Nursery Reports" }}
-    />
-    <Resource name={modules.audit.ResourceName} />
-  </Admin>
-);
+const App = () => {
+  const [identity, setIdentity] = useState<any>(null);
+  const [canCreate, setCanCreate] = useState(false);
+
+  useEffect(() => {
+    const getIdentity = async () => {
+      const data: any = await authProvider?.getIdentity?.();
+      setIdentity(data);
+    };
+
+    getIdentity();
+  }, []);
+  useEffect(() => {
+    if (identity) {
+      setCanCreate(identity?.role === "admin-super" ? true : false);
+    }
+  }, [identity]);
+
+  return (
+    <Admin
+      theme={theme}
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      layout={AppLayout}
+      loginPage={AdminLoginPage}
+    >
+      <Resource
+        name={modules.user.ResourceName}
+        list={modules.user.List}
+        show={modules.user.Show}
+        edit={modules.user.Edit}
+        icon={UserIcon}
+      />
+      <Resource
+        name={modules.organisation.ResourceName}
+        list={modules.organisation.List}
+        show={modules.organisation.Show}
+        edit={modules.organisation.Edit}
+        icon={BusinessIcon}
+      />
+      <Resource
+        name={modules.pitch.ResourceName}
+        list={modules.pitch.List}
+        show={modules.pitch.Show}
+        edit={modules.pitch.Edit}
+        icon={ForestIcon}
+      />
+      <Resource
+        name={modules.fundingProgramme.ResourceName}
+        list={modules.fundingProgramme.List}
+        edit={modules.fundingProgramme.Edit}
+        show={modules.fundingProgramme.Show}
+        create={modules.fundingProgramme.Create}
+        icon={AttachMoneyIcon}
+        options={{ label: "Funding Programmes" }}
+      />
+      <Resource
+        name={modules.reportingFramework.ResourceName}
+        list={modules.reportingFramework.List}
+        show={modules.reportingFramework.Show}
+        edit={modules.reportingFramework.Edit}
+        {...(canCreate ? { create: modules.reportingFramework.Create } : null)}
+        icon={WatchLater}
+        options={{ label: "Reporting Frameworks" }}
+      />
+      <Resource
+        name={modules.application.ResourceName}
+        list={modules.application.List}
+        show={modules.application.Show}
+        icon={LibraryBooksIcon}
+      />
+      <Resource
+        name={modules.stage.ResourceName}
+        show={modules.stage.Show}
+        edit={modules.stage.Edit}
+        create={modules.stage.Create}
+      />
+      <Resource
+        name={modules.form.ResourceName}
+        list={modules.form.List}
+        edit={modules.form.Edit}
+        create={modules.form.Create}
+      />
+      <Resource
+        name={modules.project.ResourceName}
+        list={modules.project.List}
+        show={modules.project.Show}
+        edit={modules.project.Edit}
+        icon={ArticleIcon}
+      />
+      <Resource
+        name={modules.site.ResourceName}
+        list={modules.site.List}
+        show={modules.site.Show}
+        edit={modules.site.Edit}
+        icon={LanguageIcon}
+      />
+      <Resource
+        name={modules.nursery.ResourceName}
+        list={modules.nursery.List}
+        show={modules.nursery.Show}
+        edit={modules.nursery.Edit}
+        icon={FullscreenIcon}
+      />
+      <Resource
+        name={modules.projectReport.ResourceName}
+        list={modules.projectReport.List}
+        show={modules.projectReport.Show}
+        edit={modules.projectReport.Edit}
+        icon={SummarizeIcon}
+        options={{ label: "Project Reports" }}
+      />
+      <Resource
+        name={modules.siteReport.ResourceName}
+        list={modules.siteReport.List}
+        show={modules.siteReport.Show}
+        edit={modules.siteReport.Edit}
+        icon={SummarizeIcon}
+        options={{ label: "Site Reports" }}
+      />
+      <Resource
+        name={modules.nurseryReport.ResourceName}
+        list={modules.nurseryReport.List}
+        show={modules.nurseryReport.Show}
+        edit={modules.nurseryReport.Edit}
+        icon={SummarizeIcon}
+        options={{ label: "Nursery Reports" }}
+      />
+      <Resource name={modules.audit.ResourceName} />
+    </Admin>
+  );
+};
 
 export default App;
--- src/admin/modules/reportingFramework/components/ReportingFrameworkList.tsx
+++ src/admin/modules/reportingFramework/components/ReportingFrameworkList.tsx
@@ -3,8 +3,10 @@ import { FC } from "react";
 import { Datagrid, EditButton, List, ShowButton, TextField, WrapperField } from "react-admin";
 
 import CustomDeleteWithConfirmButton from "@/admin/components/Buttons/CustomDeleteWithConfirmButton";
+import { useGetUserRole } from "@/admin/hooks/useGetUserRole";
 
 export const ReportingFrameworkList: FC = () => {
+  const { isSuperAdmin } = useGetUserRole();
   return (
     <>
       <Stack gap={1} py={2}>
@@ -20,9 +22,11 @@ export const ReportingFrameworkList: FC = () => {
           <TextField source="total_projects_count" label="Enrolled Projects" />
           <ShowButton />
           <EditButton />
-          <WrapperField>
-            <CustomDeleteWithConfirmButton source="name" />
-          </WrapperField>
+          {isSuperAdmin && (
+            <WrapperField>
+              <CustomDeleteWithConfirmButton source="name" />
+            </WrapperField>
+          )}
         </Datagrid>
       </List>
     </>
