e3962d42c475b82229d3174213eb706036f848a8
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-01-15 18:22:56 +0000

    hotfix: make new stuff like terrafund

--- src/hooks/useFramework.ts
+++ src/hooks/useFramework.ts
@@ -6,6 +6,6 @@
 export const useFramework = (entity: any) => {
   return {
     isPPC: entity.framework_key === "ppc",
-    isTerrafund: entity.framework_key === "terrafund"
+    isTerrafund: entity.framework_key !== "ppc"
   };
 };
