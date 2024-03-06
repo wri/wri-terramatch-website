90e37f6bf18b376b202f6ce588ccb8f24f933aff
Nathan Curtis <nathan.curtis@gmail.com>
2024-03-05 16:06:25 -0800

    [TM-700] Add a missing dep on the useMemo call.

--- src/admin/components/ResourceTabs/ChangeRequestsTab/ChangeRequestsTab.tsx
+++ src/admin/components/ResourceTabs/ChangeRequestsTab/ChangeRequestsTab.tsx
@@ -67,7 +67,7 @@ const ChangeRequestsTab: FC<IProps> = ({ label, entity, singularEntity, ...rest 
   // @ts-ignore
   const form = currentValues?.data?.form;
 
-  const formSteps = useMemo(() => (form == null ? [] : getCustomFormSteps(form, t)), [form]);
+  const formSteps = useMemo(() => (form == null ? [] : getCustomFormSteps(form, t)), [form, t]);
   const formChanges = useFormChanges(current, changes, formSteps ?? []);
   const numFieldsAffected = useMemo(
     () =>
