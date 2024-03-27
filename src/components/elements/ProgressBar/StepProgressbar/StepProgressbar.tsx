1ad4def05c5fd235778072217d8411156fc6ac61
Diego <diego@vizonomy.com>
2024-03-27 15:42:29 -0400

    fix step progress

--- src/components/elements/ProgressBar/StepProgressbar/StepProgressbar.tsx
+++ src/components/elements/ProgressBar/StepProgressbar/StepProgressbar.tsx
@@ -28,37 +28,40 @@ const StepProgressbar = ({ value, color = "primary", labels = [], className, ...
   }, [labels.length, value, differentialThreshold]);
 
   return (
-    <div className="relative w-full">
-      <div className="w-full">
-        <div className="flex w-full justify-between">
-          {labels?.map((item, index) => (
-            <div key={item.id}>
-              <div className="flex flex-col items-center">
-                <Icon name={IconNames.CHECK_PROGRESSBAR} className="z-10 h-5 w-5" />
-                <Text
-                  variant="text-12"
-                  className={classNames(
-                    lastSelected == index && "!font-bold",
-                    index > lastSelected && "opacity-50",
-                    "max-w-[80px] text-center first:w-20 last:w-20"
-                  )}
-                >
-                  {item.label}
-                </Text>
+    <div className="px-4">
+      <div className="relative w-full">
+        <div className="mb-12 w-full">
+          <div className="flex w-full justify-between">
+            {labels?.map((item, index) => (
+              <div key={item.id} className="">
+                <div className="relative flex flex-col items-center">
+                  <Icon name={IconNames.CHECK_PROGRESSBAR} className="z-10 h-5 w-5" />
+                  <Text
+                    variant="text-12"
+                    className={classNames(
+                      lastSelected == index && "!font-bold",
+                      index > lastSelected && "opacity-50",
+                      "absolute top-[calc(100%_+_16px)] min-w-[64px] text-center"
+                    )}
+                  >
+                    {item.label}
+                  </Text>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div
-        {...rest}
-        className={`absolute top-[5px] left-10 h-[9px] w-[calc(100%_-_80px)] rounded-full bg-neutral-200 ${
-          className || ""
-        }`}
-        role="progressbar"
-      >
-        <div className={`h-full bg-${color} rounded-full transition-all duration-300`} style={{ width: `${value}%` }} />
+        <div
+          {...rest}
+          className={`absolute top-[5px] h-[9px] w-full rounded-full bg-neutral-200 ${className || ""}`}
+          role="progressbar"
+        >
+          <div
+            className={`h-full bg-${color} rounded-full transition-all duration-300`}
+            style={{ width: `${value}%` }}
+          />
+        </div>
       </div>
     </div>
   );
