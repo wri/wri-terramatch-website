ebdc01c3a729e6c826ff073e5284c8c39e5e9fa5
Dotty <dottykurmi123@gmail.com>
2024-04-04 12:47:00 -0400

    edit menu

--- src/components/elements/Menu/Menu.tsx
+++ src/components/elements/Menu/Menu.tsx
@@ -1,17 +1,16 @@
+import classNames from "classnames";
 import { ReactNode, useEffect, useRef, useState } from "react";
-import { When } from "react-if";
+import React from "react";
 
 import { MenuItem } from "../MenuItem/MenuItem";
 import { MENU_ITEM_VARIANT_BLUE } from "../MenuItem/MenuItemVariant";
-import { MENU_PLACEMENT_BOTTOM_RIGHT } from "./MenuVariant";
-
+import { MENU_PLACEMENT_BOTTOM_RIGHT, MENU_PLACEMENT_LEFT_BOTTOM, MENU_PLACEMENT_RIGHT_TOP } from "./MenuVariant";
 interface MenuItemProps {
   id: string;
   render: () => ReactNode;
   MenuItemVariant?: string;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
 }
-
 export interface MenuProps {
   menu: MenuItemProps[];
   setSelected?: (id: string) => void;
@@ -20,8 +19,9 @@ export interface MenuProps {
   variant?: string;
   menuItemVariant?: string;
   children: ReactNode;
+  className?: string;
+  container?: HTMLDivElement | null;
 }
-
 const Menu = (props: MenuProps) => {
   const {
     menu,
@@ -29,13 +29,14 @@ const Menu = (props: MenuProps) => {
     menuItemVariant = MENU_ITEM_VARIANT_BLUE,
     variant,
     children,
-    isDefaultOpen
+    isDefaultOpen,
+    className,
+    container
   } = props;
-  const ref = useRef<HTMLDivElement | null>(null);
   const [isOpen, setIsOpen] = useState(isDefaultOpen);
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current?.contains(event.target as Node)) {
+      if (menuContainerRef.current && !menuContainerRef.current?.contains(event.target as Node)) {
         setIsOpen(false);
       }
     };
@@ -44,12 +45,93 @@ const Menu = (props: MenuProps) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   });
+  const menuContainerRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const hideMenu = () => {
+      if (menuContainerRef.current) {
+        setIsOpen(false);
+      }
+    };
+    container?.addEventListener("scroll", hideMenu);
+    window.addEventListener("scroll", hideMenu);
+    hideMenu();
+    return () => {
+      container?.addEventListener("scroll", hideMenu);
+      window.removeEventListener("scroll", hideMenu);
+    };
+  }, [container]);
+  const calculateMenuStyleForBottom = () => {
+    if (!menuContainerRef.current) {
+      return {};
+    }
+    const rect = menuContainerRef.current.getBoundingClientRect();
+    const top = rect.top;
+    const height = rect.height;
+    const width = rect.width;
+    return {
+      top: top + height,
+      marginTop: "5px",
+      minWidth: width
+    };
+  };
+  const calculateMenuStyleForHorizontalTop = () => {
+    if (!menuContainerRef.current) {
+      return {};
+    }
+    if (!menuRef.current) {
+      return {};
+    }
+    const bottom = menuContainerRef.current.getBoundingClientRect().bottom;
+    const heightMenu = menuRef.current.getBoundingClientRect().height;
+    return {
+      top: bottom - heightMenu
+    };
+  };
+  const calculateMenuStyleForHorizontalBottom = () => {
+    if (!menuContainerRef.current) {
+      return {};
+    }
+    const top = menuContainerRef.current.getBoundingClientRect().top ?? 0;
+    return {
+      top: top
+    };
+  };
+  const calculateMenuStyle = () => {
+    const placeMap: { [key: string]: string } = {
+      [MENU_PLACEMENT_RIGHT_TOP]: "horizontalTop",
+      [MENU_PLACEMENT_LEFT_BOTTOM]: "hotizontalBottom"
+    };
+    const place = placeMap[placement] || "bottom";
+    let styles;
+    switch (place) {
+      case "horizontalTop":
+        styles = calculateMenuStyleForHorizontalTop();
+        break;
+      case "hotizontalBottom":
+        styles = calculateMenuStyleForHorizontalBottom();
+        break;
+      default:
+        styles = calculateMenuStyleForBottom();
+        break;
+    }
+    return styles;
+  };
   return (
-    <div className="relative" onClick={() => setIsOpen(!isOpen)} ref={ref}>
+    <div
+      ref={menuContainerRef}
+      className={classNames(className, "relative w-fit-content")}
+      onClick={() => setIsOpen(!isOpen)}
+    >
       {children}
-      <When condition={isOpen}>
+      <div
+        className={`absolute z-40 ${placement} ${isOpen ? "visible" : "invisible"}`}
+        style={{ width: `${menuRef.current?.clientWidth}px` }}
+      >
         <div
-          className={`absolute z-10 flex min-w-full flex-col gap-1 overflow-auto rounded-lg bg-white p-2 shadow-[0_0_5px_0_rgba(0,0,0,0.2)] ${placement} ${variant}`}
+          ref={menuRef}
+          className={`fixed z-40 flex flex-col gap-1 overflow-auto rounded-lg bg-white p-2 shadow-[0_0_5px_0_rgba(0,0,0,0.2)] ${variant}`}
+          style={calculateMenuStyle()}
         >
           {menu.map(item => (
             <MenuItem
@@ -60,7 +142,7 @@ const Menu = (props: MenuProps) => {
             />
           ))}
         </div>
-      </When>
+      </div>
     </div>
   );
 };
