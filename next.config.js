efcd4106c8cf4c82fd2ef9321c13b397ca4d5dc3
Nathan Curtis <nathan.curtis@gmail.com>
2024-04-19 14:21:11 -0700

    [TM-803] Allow local dev environments to customize the next image domains.

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
   publicRuntimeConfig: {
     TxNativePublicToken: process.env.TRANSIFEX_TOKEN
   },
-  images: { domains: ["s3-eu-west-1.amazonaws.com"] },
+  images: { domains: process.env.IMAGE_DOMAINS?.split(',') ?? ["s3-eu-west-1.amazonaws.com"] },
   // webpack5: true,
   webpack(config) {
     config.module.rules.push({
