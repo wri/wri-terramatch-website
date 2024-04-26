3ca805651fcef8e64c4fb58508a16d8c26384966
Dotty <dottykurmi123@gmail.com>
2024-04-25 21:32:43 -0400

    add rows-span

--- tailwind.config.js
+++ tailwind.config.js
@@ -116,10 +116,15 @@ module.exports = {
       15: "repeat(15, minmax(0, 1fr))"
     },
 
-    gridRow:{
-      'span-6': 'span 6 / span 6',
-      'span-7': 'span 7 / span 7',
-      'span-8': 'span 8 / span 8',
+    gridRow: {
+      "span-1": "span 1 / span 1",
+      "span-2": "span 2 / span 2",
+      "span-3": "span 3 / span 3",
+      "span-4": "span 4 / span 4",
+      "span-5": "span 5 / span 5",
+      "span-6": "span 6 / span 6",
+      "span-7": "span 7 / span 7",
+      "span-8": "span 8 / span 8"
     },
 
     boxShadow: {
