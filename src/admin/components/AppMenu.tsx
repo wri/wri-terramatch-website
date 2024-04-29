badda0db588865f4c08999ed29340adff33ed785
Diego <diego@vizonomy.com>
2024-04-29 10:15:25 -0400

    sidebar menu for resolutions 1900 and 2500

--- src/admin/components/AppMenu.tsx
+++ src/admin/components/AppMenu.tsx
@@ -6,57 +6,58 @@ import modules from "../modules";
 
 const AppMenu = () => {
   const location = useLocation();
+  const hash = location.pathname.split("/")[1];
   return (
-    <Menu>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "user" })}>
+    <Menu className="!w-full">
+      <div className={classNames({ "Sidebar-active": hash === "user" })}>
         <Menu.ResourceItem name={modules.user.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "organisation" })}>
+      <div className={classNames({ "Sidebar-active": hash === "organisation" })}>
         <Menu.ResourceItem name={modules.organisation.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "pitch" })}>
+      <div className={classNames({ "Sidebar-active": hash === "pitch" })}>
         <Menu.ResourceItem name={modules.pitch.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "fundingProgramme" })}>
+      <div className={classNames({ "Sidebar-active": hash === "fundingProgramme" })}>
         <Menu.ResourceItem name={modules.fundingProgramme.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "reportingFramework" })}>
+      <div className={classNames({ "Sidebar-active": hash === "reportingFramework" })}>
         <Menu.ResourceItem name={modules.reportingFramework.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "application" })}>
+      <div className={classNames({ "Sidebar-active": hash === "application" })}>
         <Menu.ResourceItem name={modules.application.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "stage" })}>
+      <div className={classNames({ "Sidebar-active": hash === "stage" })}>
         <Menu.ResourceItem name={modules.stage.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "form" })}>
+      <div className={classNames({ "Sidebar-active": hash === "form" })}>
         <Menu.ResourceItem name={modules.form.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "project" })}>
+      <div className={classNames({ "Sidebar-active": hash === "project" })}>
         <Menu.ResourceItem name={modules.project.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "site" })}>
+      <div className={classNames({ "Sidebar-active": hash === "site" })}>
         <Menu.ResourceItem name={modules.site.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "nursery" })}>
+      <div className={classNames({ "Sidebar-active": hash === "nursery" })}>
         <Menu.ResourceItem name={modules.nursery.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "task" })}>
+      <div className={classNames({ "Sidebar-active": hash === "task" })}>
         <Menu.ResourceItem name={modules.task.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "projectReport" })}>
+      <div className={classNames({ "Sidebar-active": hash === "projectReport" })}>
         <Menu.ResourceItem name={modules.projectReport.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "siteReport" })}>
+      <div className={classNames({ "Sidebar-active": hash === "siteReport" })}>
         <Menu.ResourceItem name={modules.siteReport.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "nurseryReport" })}>
+      <div className={classNames({ "Sidebar-active": hash === "nurseryReport" })}>
         <Menu.ResourceItem name={modules.nurseryReport.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "airtable" })}>
+      <div className={classNames({ "Sidebar-active": hash === "airtable" })}>
         <Menu.ResourceItem name={modules.airtable.ResourceName} />
       </div>
-      <div className={classNames({ "Sidebar-active": location.pathname.split("/")[1] === "audit" })}>
+      <div className={classNames({ "Sidebar-active": hash === "audit" })}>
         <Menu.ResourceItem name={modules.audit.ResourceName} />
       </div>
     </Menu>
