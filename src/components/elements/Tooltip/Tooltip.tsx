11f8ffc9230c6abc774a93a398adc7e8a74b9de4
Dotty <dottykurmi123@gmail.com>
2024-04-30 14:59:45 -0400

    edit tooltip width

--- src/components/elements/Tooltip/Tooltip.tsx
+++ src/components/elements/Tooltip/Tooltip.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { twMerge as tw } from "tailwind-merge";
 
 import Text from "../Text/Text";
 
@@ -15,8 +16,11 @@ const ToolTip = ({ children, content, width, placement, className, title }: Tool
   return (
     <div className={`group relative flex flex-col items-center ${className}`}>
       <div
-        className={`shadow-lg text-12 absolute bottom-full z-10 mb-1 hidden w-fit rounded bg-darkCustom p-2 text-center text-white
-        group-hover:inline-block ${width} ${placement} lg:p-3`}
+        className={tw(
+          "shadow-lg text-12 absolute bottom-full z-10 mb-1 hidden w-fit rounded bg-darkCustom p-2 text-center text-white group-hover:inline-block lg:p-3",
+          width,
+          placement
+        )}
       >
         <div className="absolute bottom-0 left-1/2 ml-[-4px] mb-[-9px] hidden  border-[5px] border-darkCustom border-r-transparent border-b-transparent border-l-transparent group-hover:block " />
         <Text variant="text-12-semibold">{title}</Text>
