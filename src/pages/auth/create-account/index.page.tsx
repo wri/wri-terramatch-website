04489417e619a0172396566e85ec92df8a061d06
Limber Mamani Vallejos <limbermv2@gmail.com>
2024-05-02 09:56:33 -0400

    fix: button continue classname

--- src/pages/auth/create-account/index.page.tsx
+++ src/pages/auth/create-account/index.page.tsx
@@ -90,7 +90,7 @@ const Page = () => {
             )}
           </div>
           <Button
-            className="text-14-bold border-blue-300 bg-blue-300 flex w-full items-center justify-center rounded-lg border-2 py-3.5 text-white hover:border-white"
+            className="text-14-bold flex w-full items-center justify-center rounded-lg border-2 border-primary bg-primary py-3.5 text-white hover:border-white"
             onClick={handleContinue}
           >
             Continue
