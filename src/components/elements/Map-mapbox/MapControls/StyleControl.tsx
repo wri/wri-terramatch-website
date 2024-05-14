c91ac949b8d1bb3ae43a91104b41da5ca9ba255b
cesarLima1 <cesmaulim@gmail.com>
2024-05-14 10:07:52 -0400

    avoid click twice same button

--- src/components/elements/Map-mapbox/MapControls/StyleControl.tsx
+++ src/components/elements/Map-mapbox/MapControls/StyleControl.tsx
@@ -9,20 +9,19 @@ import { MapStyle } from "@/components/elements/Map-mapbox/MapControls/types";
 export const StyleControl = ({ mapRef }: { mapRef: any }) => {
   const map = mapRef.current?.map;
   const t = useT();
-  // const refresh = useRefresh();
   const [currentStyle, setCurrentStyle] = useState(MapStyle.Satellite);
-  // useEffect(() => {
-  //   if (map?.areTilesLoaded()) {
-  //     refresh();
-  //   }
-  // }, [currentStyle, map]);
+
+  const setMapStyle = (style: MapStyle) => {
+    if (map && currentStyle !== style) {
+      map.setStyle(style);
+      setCurrentStyle(style);
+    }
+  };
+
   return (
     <ControlButtonsGroup direction="row" className="h-auto">
       <button
-        onClick={() => {
-          map?.setStyle(MapStyle.Street);
-          setCurrentStyle(MapStyle.Street);
-        }}
+        onClick={() => setMapStyle(MapStyle.Street)}
         className={classNames(
           "h-fit w-21 rounded-l-lg py-2",
           currentStyle === MapStyle.Street ? "text-body-500" : "text-body-400"
@@ -33,10 +32,7 @@ export const StyleControl = ({ mapRef }: { mapRef: any }) => {
       </button>
       <ControlDivider direction="vertical" className="m-0 h-auto bg-neutral-200" />
       <button
-        onClick={() => {
-          map?.setStyle(MapStyle.Satellite);
-          setCurrentStyle(MapStyle.Satellite);
-        }}
+        onClick={() => setMapStyle(MapStyle.Satellite)}
         className={classNames(
           "h-fit w-21 rounded-r-lg py-2",
           currentStyle === MapStyle.Satellite ? "text-body-500" : "text-body-400"
