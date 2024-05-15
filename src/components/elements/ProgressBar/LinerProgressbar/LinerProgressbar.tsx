a4d771ee0d4c4f59330f2dd0484ec8d9211776c7
Diego <diego@vizonomy.com>
2024-05-15 13:24:54 -0400

    add linerProgressbar

--- src/components/elements/ProgressBar/LinerProgressbar/LinerProgressbar.tsx
+++ src/components/elements/ProgressBar/LinerProgressbar/LinerProgressbar.tsx
@@ -5,12 +5,21 @@ import { Colors } from "@/types/common";
 export interface LinerProgressbarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   color?: Colors;
   value: number;
+  textColor?: string;
+  colorProgress?: string;
 }
 
-const LinerProgressbar = ({ value, color = "primary", className, ...rest }: LinerProgressbarProps) => {
+const LinerProgressbar = ({
+  value,
+  color = "primary",
+  textColor,
+  colorProgress,
+  className,
+  ...rest
+}: LinerProgressbarProps) => {
   return (
-    <div {...rest} className={`h-[9px] w-full bg-neutral-200 ${className || ""}`} role="progressbar">
-      <div className={`h-full bg-${color} transition-all duration-300`} style={{ width: `${value}%` }} />
+    <div {...rest} className={`h-[9px] w-full rounded-full bg-neutral-200 ${className || ""}`} role="progressbar">
+      <div className={`h-full bg-${color} rounded-full transition-all duration-300`} style={{ width: `${value}%` }} />
     </div>
   );
 };
