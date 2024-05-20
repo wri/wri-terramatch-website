6eeb957289ea45dfaad93f0f62bb90514e35a7e6
Dotty <dottykurmi123@gmail.com>
2024-05-20 15:06:07 -0400

    add t in label Pagination

--- src/components/extensive/Pagination/index.tsx
+++ src/components/extensive/Pagination/index.tsx
@@ -1,3 +1,4 @@
+import { useT } from "@transifex/react";
 import classNames from "classnames";
 
 import PageSelector, { PageSelectorProps } from "./PageSelector";
@@ -14,6 +15,7 @@ export interface PaginationProps extends PageSelectorProps {
 }
 
 function Pagination(props: PaginationProps) {
+  const t = useT();
   return props.treeSpeciesShow ? (
     <div className={classNames("flex items-center justify-between", props.containerClassName)}>
       <PageSelector variantText={props.variant?.VariantPageText} {...props} />
@@ -22,7 +24,7 @@ function Pagination(props: PaginationProps) {
     <div className={classNames("flex items-center justify-between", props.containerClassName)}>
       {props.hasPageSizeSelector ? (
         <PerPageSelector
-          label="Per page"
+          label={t("Per page")}
           options={[5, 10, 15, 20, 50]}
           variantText={props.variant?.VariantPrePageText}
           defaultValue={props.defaultPageSize}
