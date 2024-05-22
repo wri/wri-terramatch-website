8669272c3daa4948100369bbd68510899ee7aa1f
Dotty <dottykurmi123@gmail.com>
2024-05-22 17:07:14 -0400

    add gap in list attach

--- src/components/elements/ComentaryBox/ComentaryBox.tsx
+++ src/components/elements/ComentaryBox/ComentaryBox.tsx
@@ -120,21 +120,25 @@ const ComentaryBox = (props: ComentaryBoxProps) => {
           <Button variant="text" iconProps={{ name: IconNames.SEND, className: "h-4 w-4 text-darkCustom" }} />
         </When>
       </div>
-      {...files.map((file: any) => (
-        <div key={file.name} className="flex items-baseline justify-between rounded-xl bg-neutral-150 px-2 py-1">
-          <Text variant="text-14-light" className="text-grey-700">
-            {file?.name}
-          </Text>
-          <button
-            className="rounded-full bg-red p-2 text-white hover:bg-error-400 hover:text-red"
-            onClick={() => {
-              setFiles(files.filter((f: any) => f.name !== file.name));
-            }}
-          >
-            <Icon name={IconNames.CLEAR} className="h-3 w-3" />
-          </button>
+      <When condition={files.length > 0}>
+        <div className="grid gap-2">
+          {...files.map((file: any) => (
+            <div key={file.name} className="flex items-baseline justify-between rounded-xl bg-neutral-150 px-2 py-1">
+              <Text variant="text-14-light" className="text-grey-700">
+                {file?.name}
+              </Text>
+              <button
+                className="rounded-full bg-red p-2 text-white hover:bg-error-400 hover:text-red"
+                onClick={() => {
+                  setFiles(files.filter((f: any) => f.name !== file.name));
+                }}
+              >
+                <Icon name={IconNames.CLEAR} className="h-3 w-3" />
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </When>
       {error && <div className="text-red">{error}</div>}
       <When condition={!buttonSendOnBox}>
         <Button className="self-end" iconProps={{ name: IconNames.SEND, className: "h-4 w-4" }} onClick={submitComment}>
