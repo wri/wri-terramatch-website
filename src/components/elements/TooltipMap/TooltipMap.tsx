2bc27dd42056d422ec8ebbd8126e900f3c7a5f71
Donwilo <burgoadanilson@gmail.com>
2024-05-29 13:42:50 -0400

    Refactor popup use hook to get data

--- src/components/elements/TooltipMap/TooltipMap.tsx
+++ src/components/elements/TooltipMap/TooltipMap.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Icon, { IconNames } from "@/components/extensive/Icon/Icon";
+import { useGetV2TerrafundPolygonUuid } from "@/generated/apiComponents";
+import { SitePolygon } from "@/generated/apiSchemas";
 
+import { formatPlannedStartDate } from "../Map-mapbox/utils";
 import Text from "../Text/Text";
 export interface TooltipMapProps {
   setTooltipOpen: () => void;
   setEditPolygon: () => void;
   polygon: any;
   popup?: any;
-  formattedPlantStartDate: any;
 }
 const topBorderColorPopup: any = {
   submitted: "border-t-primary",
@@ -17,28 +19,45 @@ const topBorderColorPopup: any = {
 };
 
 const TooltipMap = (props: TooltipMapProps) => {
-  const { setTooltipOpen, setEditPolygon, polygon, formattedPlantStartDate } = props;
+  const { setTooltipOpen, setEditPolygon, polygon } = props;
+  const [polygonData, setPolygonData] = React.useState<SitePolygon>();
+  const { data: sitePolygonData } = useGetV2TerrafundPolygonUuid<{
+    site_polygon: SitePolygon;
+  }>({
+    pathParams: {
+      uuid: polygon
+    }
+  });
+
+  useEffect(() => {
+    setPolygonData(sitePolygonData?.site_polygon);
+  }, [sitePolygonData]);
+
+  const formatDate = (date: string | undefined) => {
+    const plantStartDate = date ? new Date(date) : null;
+    const formattedPlantStartDate = formatPlannedStartDate(plantStartDate);
+    return formattedPlantStartDate;
+  };
+
   return (
     <div
-      className={`absolute z-20 w-[295px] rounded border-t-[5px] ${
-        topBorderColorPopup[polygon?.status]
-      } bg-white px-3 pb-3 pt-[7px]`}
+      className={`w-[295px] rounded border-t-[5px] ${topBorderColorPopup[polygon?.status]} bg-white px-3 pb-3 pt-[7px]`}
       style={{
         bottom: "0",
         transform: "translate(-50%, 0px)"
       }}
     >
-      <button onClick={setTooltipOpen} className="absolute right-2 top-2 ml-2 rounded p-1 hover:bg-grey-800">
+      <button onClick={setTooltipOpen} className="right-2 top-2 ml-2 rounded p-1 hover:bg-grey-800">
         <Icon name={IconNames.CLEAR} className="h-3 w-3 text-darkCustom-100" />
       </button>
 
       <div className="text-10 flex items-center justify-center gap-1">
         <Text variant="text-10" className="mb-1 uppercase leading-[normal]">
-          {polygon?.site_name} SITE
+          {polygonData?.site_name} SITE
         </Text>
       </div>
       <Text variant="text-10-bold" className="text-center leading-[normal] text-black">
-        {polygon?.poly_name ? polygon?.poly_name : "Unnamed Polygon"}
+        {polygonData?.poly_name ? polygonData?.poly_name : "Unnamed Polygon"}
       </Text>
       <hr className="my-2 border border-grey-750" />
       <div className="grid grid-cols-2 gap-4">
@@ -47,7 +66,7 @@ const TooltipMap = (props: TooltipMapProps) => {
             Restoration Practice
           </Text>
           <Text variant="text-10-bold" className="leading-[normal] text-black ">
-            {polygon?.practice ? polygon?.practice : "Unknown"}
+            {polygonData?.practice ? polygonData?.practice : "Unknown"}
           </Text>
         </div>
         <div>
@@ -55,7 +74,7 @@ const TooltipMap = (props: TooltipMapProps) => {
             Target Land Use System
           </Text>
           <Text variant="text-10-bold" className="leading-[normal] text-black ">
-            {polygon?.target_sys ? polygon?.target_sys : "Unknown"}
+            {polygonData?.target_sys ? polygonData?.target_sys : "Unknown"}
           </Text>
         </div>
         <div>
@@ -63,7 +82,7 @@ const TooltipMap = (props: TooltipMapProps) => {
             Tree Distribution
           </Text>
           <Text variant="text-10-bold" className="leading-[normal] text-black">
-            {polygon?.distr ? polygon?.distr : "Unknown"}
+            {polygonData?.distr ? polygonData?.distr : "Unknown"}
           </Text>
         </div>
         <div>
@@ -71,7 +90,7 @@ const TooltipMap = (props: TooltipMapProps) => {
             Planting Start Date
           </Text>
           <Text variant="text-10-bold" className="leading-[normal] text-black ">
-            {formattedPlantStartDate}
+            {formatDate(polygonData?.plantstart)}
           </Text>
         </div>
       </div>
