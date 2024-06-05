1ef0eaf1ba4a5dbf68711afd6927a5999aaea9d6
Limber Mamani Vallejos <limbermv2@gmail.com>
2024-06-05 16:42:10 -0400

    add colors to status

--- src/components/elements/Inputs/Dropdown/Dropdown.tsx
+++ src/components/elements/Inputs/Dropdown/Dropdown.tsx
@@ -142,7 +142,9 @@ const Dropdown = (props: PropsWithChildren<DropdownProps>) => {
       submitted: "bg-blue",
       draft: "bg-pinkCustom",
       "Under Review": "bg-tertiary-600",
-      "needs-more-information": "bg-tertiary-600"
+      "needs-more-information": "bg-tertiary-600",
+      "planting-in-progress": "bg-blue",
+      "awaiting-approval": "bg-tertiary-600"
     };
 
     return colorMap[option] || "";
