4fe45229e1180c3b2c20b5dfedda7ee7c4da8c4e
Diego <diego@vizonomy.com>
2024-06-05 11:00:28 -0400

    Solving build

--- src/components/elements/Commentary/Commentary.stories.tsx
+++ src/components/elements/Commentary/Commentary.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 
-import { CommentaryFiles } from "@/components/extensive/Modal/ModalContent/MockedData";
+import { commentaryFiles } from "@/components/extensive/Modal/ModalContent/MockedData";
 
 import { CommentaryProps as Props } from "./Commentary";
 import Component from "./Commentary";
@@ -23,8 +23,8 @@ export const Default: Story = {
     name: "Ricardo",
     lastName: "Saavedra",
     date: "Oct 6, 2022 at 1:12 AM",
-    Commentary: `Don't see the outline. the source code also needs to be updated.re: aligned to one source. we need to make sure whether this is appropriate. consider that we have the organization in sign-up/profile, mask, and work request boards. On Thursday will provide the the source tables requested`,
-    files: CommentaryFiles,
+    commentary: `Don't see the outline. the source code also needs to be updated.re: aligned to one source. we need to make sure whether this is appropriate. consider that we have the organization in sign-up/profile, mask, and work request boards. On Thursday will provide the the source tables requested`,
+    files: commentaryFiles,
     status: "submitted"
   }
 };
