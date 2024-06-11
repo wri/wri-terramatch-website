7f7fcd9e07615518d9eed6759edccea227053427
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-11 16:13:45 -0400

    update stories tests

--- src/components/elements/CommentaryBox/CommentaryBox.stories.tsx
+++ src/components/elements/CommentaryBox/CommentaryBox.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { CommentaryBoxProps as Props } from "./CommentaryBox";
 import Component from "./CommentaryBox";
@@ -11,10 +12,14 @@ const meta: Meta<typeof Component> = {
 export default meta;
 type Story = StoryObj<typeof Component>;
 
+const client = new QueryClient();
+
 export const Default: Story = {
   render: (args: Props) => (
     <div className="flex items-center justify-center p-8">
-      <Component {...args} />
+      <QueryClientProvider client={client}>
+        <Component {...args} />
+      </QueryClientProvider>
     </div>
   ),
   args: {
