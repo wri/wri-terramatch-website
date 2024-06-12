a65497973fe2f35c4acf12dcd0b29000e1c69880
cesarLima1 <cesmaulim@gmail.com>
2024-06-12 16:48:54 -0400

    [TM-874] remove unused code

--- src/pages/site/[uuid]/tabs/Overview.tsx
+++ src/pages/site/[uuid]/tabs/Overview.tsx
@@ -1,32 +1,19 @@
 import { useT } from "@transifex/react";
-import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { When } from "react-if";
 
-import { polygonData } from "@/admin/components/ResourceTabs/PolygonReviewTab/components/Polygons";
 import Button from "@/components/elements/Button/Button";
 import GoalProgressCard from "@/components/elements/Cards/GoalProgressCard/GoalProgressCard";
 import ItemMonitoringCards from "@/components/elements/Cards/ItemMonitoringCard/ItemMonitoringCards";
 import Dropdown from "@/components/elements/Inputs/Dropdown/Dropdown";
-import { VARIANT_FILE_INPUT_MODAL_ADD_IMAGES } from "@/components/elements/Inputs/FileInput/FileInputVariants";
-import Menu from "@/components/elements/Menu/Menu";
-import { MENU_PLACEMENT_BOTTOM_BOTTOM } from "@/components/elements/Menu/MenuVariant";
-import StepProgressbar from "@/components/elements/ProgressBar/StepProgressbar/StepProgressbar";
 import Text from "@/components/elements/Text/Text";
 import Icon, { IconNames } from "@/components/extensive/Icon/Icon";
-import ModalAdd from "@/components/extensive/Modal/ModalAdd";
-import ModalConfirm from "@/components/extensive/Modal/ModalConfirm";
-import { uploadImageData } from "@/components/extensive/Modal/ModalContent/MockedData";
-import ModalSubmit from "@/components/extensive/Modal/ModalSubmit";
-import ModalWithMap from "@/components/extensive/Modal/ModalWithMap";
 import PageBody from "@/components/extensive/PageElements/Body/PageBody";
 import PageCard from "@/components/extensive/PageElements/Card/PageCard";
 import PageColumn from "@/components/extensive/PageElements/Column/PageColumn";
 import PageRow from "@/components/extensive/PageElements/Row/PageRow";
-import { useModalContext } from "@/context/modal.provider";
-import { useGetV2MODELUUIDImageLocations } from "@/generated/apiComponents";
 import { getEntityDetailPageLink } from "@/helpers/entity";
 import { useFramework } from "@/hooks/useFramework";
 
@@ -41,253 +28,6 @@ const SiteOverviewTab = ({ site }: SiteOverviewTabProps) => {
   const router = useRouter();
   const { isPPC } = useFramework(site);
   const [editPolygon, setEditPolygon] = useState(false);
-  const { openModal, closeModal } = useModalContext();
-  const openFormModalHandlerAddPolygon = () => {
-    openModal(
-      <ModalAdd
-        title="Add Polygons"
-        descriptionInput="Drag and drop a GeoJSON, Shapefile, or KML for your site Tannous/Brayton Road."
-        descriptionList={
-          <div className="mt-9 flex">
-            <Text variant="text-12-bold">{t("TerraMatch upload limits:")}&nbsp;</Text>
-            <Text variant="text-12-light">{t("50 MB per upload")}</Text>
-          </div>
-        }
-        onClose={closeModal}
-        content="Start by adding polygons to your site."
-        primaryButtonText="Close"
-        primaryButtonProps={{ className: "px-8 py-3", variant: "primary", onClick: closeModal }}
-      >
-        {/* Next div is only Mocked data delete this children later*/}
-        <div className="mb-6 flex flex-col gap-4">
-          {polygonData.map(polygon => (
-            <div
-              key={polygon.id}
-              className="border-grey-75 flex items-center justify-between rounded-lg border border-grey-750 py-[10px] pr-6 pl-4"
-            >
-              <div className="flex gap-3">
-                <div className="rounded-lg bg-neutral-150 p-2">
-                  <Icon name={IconNames.POLYGON} className="h-6 w-6 text-grey-720" />
-                </div>
-                <div>
-                  <Text variant="text-12">{polygon.name}</Text>
-                  <Text variant="text-12" className="opacity-50">
-                    {polygon.status}
-                  </Text>
-                </div>
-              </div>
-              <Icon
-                name={polygon.isUploaded ? IconNames.CHECK_POLYGON : IconNames.ELLIPSE_POLYGON}
-                className="h-6 w-6"
-              />
-            </div>
-          ))}
-        </div>
-      </ModalAdd>
-    );
-  };
-  const openFormModalHandlerUploadImages = () => {
-    openModal(
-      <ModalAdd
-        title="Upload Images"
-        variantFileInput={VARIANT_FILE_INPUT_MODAL_ADD_IMAGES}
-        descriptionInput="Drag and drop a geotagged or non-geotagged PNG, GIF or JPEG for your site Tannous/Brayton Road."
-        descriptionList={
-          <Text variant="text-12-bold" className="mt-9 ">
-            Uploaded Files
-          </Text>
-        }
-        onClose={closeModal}
-        content="Start by adding images for processing."
-        primaryButtonText="Save"
-        primaryButtonProps={{ className: "px-8 py-3", variant: "primary", onClick: closeModal }}
-      >
-        {/* Next div is only Mocked data delete this children later*/}
-        <div className="mb-6 flex flex-col gap-4">
-          {uploadImageData.map(image => (
-            <div
-              key={image.id}
-              className="border-grey-75 flex items-center justify-between rounded-lg border border-grey-750 py-[10px] px-4"
-            >
-              <div className="flex gap-3">
-                <div className="rounded-lg bg-neutral-150 p-2">
-                  <Icon name={IconNames.IMAGE} className="h-6 w-6 text-grey-720" />
-                </div>
-                <div>
-                  <Text variant="text-12">{image.name}</Text>
-                  <Text variant="text-12" className="opacity-50">
-                    {image.status}
-                  </Text>
-                </div>
-              </div>
-              <div
-                className={classNames("flex w-[146px] items-center justify-center rounded border py-2", {
-                  "border-blue": image.isVerified,
-                  "border-red": !image.isVerified
-                })}
-              >
-                <Text
-                  variant="text-12-bold"
-                  className={classNames("text-center", {
-                    "text-blue": image.isVerified,
-                    "text-red": !image.isVerified
-                  })}
-                >
-                  {image.isVerified ? "GeoTagged Verified" : "Not Verified"}
-                </Text>
-              </div>
-            </div>
-          ))}
-        </div>
-      </ModalAdd>
-    );
-  };
-
-  const openFormModalHandlerSubmitReviewConfirm = () => {
-    openModal(
-      <ModalConfirm
-        commentArea
-        className="max-w-xs"
-        title={"Confirm Polygon Submission"}
-        content={
-          <Text variant="text-12-light" as="p" className="text-center">
-            Are your sure you want to submit your polygons for the site <b style={{ fontSize: "inherit" }}>Iseme.</b>?
-          </Text>
-        }
-        onClose={closeModal}
-        onConfirm={() => {
-          closeModal;
-        }}
-      />
-    );
-  };
-
-  const openFormModalHandlerRequestPolygonSupport = () => {
-    openModal(
-      <ModalWithMap
-        title="Request Support"
-        onClose={closeModal}
-        content="Faja Lobi Project&nbsp;&nbsp;•&nbsp;&nbsp;Priceless Planet Coalition"
-        primaryButtonText="Submit"
-        primaryButtonProps={{ className: "px-8 py-3", variant: "primary", onClick: closeModal }}
-      ></ModalWithMap>
-    );
-  };
-
-  const openFormModalHandlerSubmitPolygon = () => {
-    openModal(
-      <ModalSubmit
-        title="Submit Polygons"
-        onClose={closeModal}
-        content="Project Developers may submit one or all polygons for review."
-        primaryButtonText="Next"
-        primaryButtonProps={{
-          className: "px-8 py-3",
-          variant: "primary",
-          onClick: () => {
-            closeModal();
-            openFormModalHandlerSubmitReviewConfirm();
-          }
-        }}
-        secondaryButtonText="Cancel"
-        secondaryButtonProps={{ className: "px-8 py-3", variant: "white-page-admin", onClick: closeModal }}
-        site={site}
-      ></ModalSubmit>
-    );
-  };
-
-  const polygonStatusLabels = [
-    { id: "1", label: "Site Approved" },
-    { id: "2", label: "Polygons Submitted" },
-    { id: "3", label: "Polygons Approved" },
-    { id: "4", label: "Monitoring Begins" }
-  ];
-
-  const itemsPrimaryMenu = [
-    {
-      id: "1",
-      render: () => (
-        <Text variant="text-14-semibold" className="flex items-center ">
-          Create Polygons
-        </Text>
-      ),
-      onClick: () => {
-        console.log("Create Polygons", editPolygon);
-        setEditPolygon(true);
-      }
-    },
-    {
-      id: "2",
-      render: () => (
-        <Text variant="text-14-semibold" className="flex items-center ">
-          Upload Data
-        </Text>
-      ),
-      onClick: () => openFormModalHandlerAddPolygon()
-    },
-    {
-      id: "3",
-      render: () => (
-        <Text variant="text-14-semibold" className="flex items-center ">
-          Upload Images
-        </Text>
-      ),
-      onClick: () => openFormModalHandlerUploadImages()
-    }
-  ];
-  const itemsSubmitPolygon = [
-    {
-      id: "1",
-      render: () => (
-        <Text variant="text-14-semibold" className="flex items-center ">
-          Request Support
-        </Text>
-      ),
-      onClick: () => openFormModalHandlerRequestPolygonSupport()
-    },
-    {
-      id: "2",
-      render: () => (
-        <Text variant="text-14-semibold" className="flex items-center ">
-          Submit for Review
-        </Text>
-      ),
-      onClick: () => openFormModalHandlerSubmitPolygon()
-    }
-  ];
-
-  const { data: allImages } = useGetV2MODELUUIDImageLocations({
-    pathParams: { model: "sites", uuid: site.uuid }
-  });
-  console.debug(allImages);
-  // const imagesGeoJson =
-  //   allImages?.data?.length! > 0
-  //     ? {
-  //         type: "FeatureCollection",
-  //         features: allImages?.data?.map(image => ({
-  //           type: "Feature",
-  //           properties: {
-  //             id: image.uuid,
-  //             image_url: image.thumb_url
-  //           },
-  //           geometry: {
-  //             type: "Point",
-  //             coordinates: [image.location?.lng, image.location?.lat]
-  //           }
-  //         }))
-  //       }
-  //     : undefined;
-
-  // const geoJSON = useMemo(() => {
-  //   try {
-  //     if (site.boundary_geojson) {
-  //       return JSON.parse(site.boundary_geojson);
-  //     }
-  //   } catch (e) {
-  //     return undefined;
-  //   }
-  //   return undefined;
-  // }, [site]);
 
   return (
     <PageBody>
@@ -331,10 +71,6 @@ const SiteOverviewTab = ({ site }: SiteOverviewTabProps) => {
             </div>
           </div>
         </PageCard>
-
-        {/* <PageCard title={t("Site Area")} tooltip=" ">
-          <Map className="rounded-lg" geojson={geoJSON} imageLayerGeojson={imagesGeoJson} />
-        </PageCard> */}
       </PageRow>
       <PageRow>
         <PageColumn>
@@ -349,78 +85,17 @@ const SiteOverviewTab = ({ site }: SiteOverviewTabProps) => {
                   modified in QGIS or ArcGIS and imported again; or fed through the mobile application.
                 </Text>
                 <div className="flex w-full gap-3">
-                  <Menu placement={MENU_PLACEMENT_BOTTOM_BOTTOM} menu={itemsPrimaryMenu}>
-                    <Button variant="white-border" className="" onChange={() => {}}>
-                      <Icon name={IconNames.PLUS_PA} className="h-4 w-4" />
-                      &nbsp; Add Data
-                    </Button>
-                  </Menu>
                   <Button variant="white-border" className="" onChange={() => {}}>
                     <Icon name={IconNames.DOWNLOAD_PA} className="h-4 w-4" />
                     &nbsp; Download
                   </Button>
-                  <Menu placement={MENU_PLACEMENT_BOTTOM_BOTTOM} menu={itemsSubmitPolygon}>
-                    <Button variant="primary" className="" onChange={() => {}}>
-                      SUBMIT Polygons
-                    </Button>
-                  </Menu>
                 </div>
-              </div>
-
-              <div className="w-[46%] rounded-lg border border-neutral-200 p-4">
-                <Text variant="text-14" className="mb-2">
-                  Polygon Status
-                </Text>
-                <StepProgressbar color="primary" value={80} labels={polygonStatusLabels} classNameLabels="" />
               </div>
             </div>
             <SiteArea sites={site} setEditPolygon={setEditPolygon} editPolygon={editPolygon} />
           </PageCard>
         </PageColumn>
       </PageRow>
-      {/* Old Site Information */}
-      {/* <PageRow>
-        <PageColumn>
-          <PageCard title={t("Site Information")} gap={8}>
-            <SelectImageListField
-              title={t("Target Land Use Types")}
-              options={landUseTypesOptions}
-              selectedValues={site.land_use_types}
-            />
-            <SelectImageListField
-              title={t("Restoration Strategies")}
-              options={restorationStrategyOptions}
-              selectedValues={site.restoration_strategy}
-            />
-          </PageCard>
-        </PageColumn>
-
-        <PageColumn>
-          <PageCard title={t("Site Details")} gap={4}>
-            <TextField label={t("Site Name")} value={site?.name} />
-            <When condition={isPPC}>
-              <TextField label={t("Site type")} value={site?.control_site ? t("Control Site") : t("Site")} />
-            </When>
-            <TextField label={t("Planting start date")} value={format(site.start_date)} />
-            <TextField label={t("Planting end date")} value={format(site.start_date)} />
-            <TextField label={t("Last Updated")} value={format(site.updated_at)} />
-          </PageCard>
-          <Paper>
-            <ButtonField
-              label={t("Tree Monitoring")}
-              subtitle={t(
-                "Tree monitoring must be completed for each site at baseline, 2.5 years and 5 years. Tree monitoring data is used to calculate the number of trees, natural regeneration, and survival rate of planted trees."
-              )}
-              buttonProps={{
-                as: Link,
-                variant: "secondary",
-                children: t("View"),
-                href: "https://ee.kobotoolbox.org/x/NKctF6KV"
-              }}
-            />
-          </Paper>
-        </PageColumn> 
-      </PageRow>*/}
       <PageRow>
         <PageColumn>
           <PageCard
