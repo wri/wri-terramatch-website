7a38c32d6aca860c8bfbdb58af0c71a647dd2de3
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-06-12 13:31:44 -0400

    change tabs to be dynamic

--- src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
+++ src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
@@ -2,34 +2,25 @@ import { FC } from "react";
 
 import Button from "@/components/elements/Button/Button";
 
-import { AuditLogButtonStates } from "../constants/enum";
-
 interface AuditLogSiteTabSelectionProps {
   buttonToogle: number;
   setButtonToogle: (buttonToogle: number) => void;
 }
 
+const tabNames = ["Project Status", "Site Status", "Polygon Status"];
+
 const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({ buttonToogle, setButtonToogle }) => {
   return (
     <div className="flex w-fit gap-1 rounded-lg bg-neutral-200 p-1">
-      <Button
-        variant={`${buttonToogle === AuditLogButtonStates.PROJECT ? "white-toggle" : "transparent-toggle"}`}
-        onClick={() => setButtonToogle(AuditLogButtonStates.PROJECT)}
-      >
-        Project Status
-      </Button>
-      <Button
-        variant={`${buttonToogle === AuditLogButtonStates.SITE ? "white-toggle" : "transparent-toggle"}`}
-        onClick={() => setButtonToogle(AuditLogButtonStates.SITE)}
-      >
-        Site Status
-      </Button>
-      <Button
-        variant={`${buttonToogle === AuditLogButtonStates.POLYGON ? "white-toggle" : "transparent-toggle"}`}
-        onClick={() => setButtonToogle(AuditLogButtonStates.POLYGON)}
-      >
-        Polygon Status
-      </Button>
+      {tabNames.map((tabName, index) => (
+        <Button
+          key={index}
+          variant={`${buttonToogle === index ? "white-toggle" : "transparent-toggle"}`}
+          onClick={() => setButtonToogle(index)}
+        >
+          {tabName}
+        </Button>
+      ))}
     </div>
   );
 };
