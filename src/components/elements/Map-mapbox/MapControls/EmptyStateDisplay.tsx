f6b523eb10e0da31c0fc913ac20ac6dc80dc1a2a
Dotty <dottykurmi123@gmail.com>
2024-06-13 17:39:09 -0400

    [TM-872] add style in EmptyStateDisplay

--- src/components/elements/Map-mapbox/MapControls/EmptyStateDisplay.tsx
+++ src/components/elements/Map-mapbox/MapControls/EmptyStateDisplay.tsx
@@ -5,9 +5,13 @@ import Text from "../../Text/Text";
 const EmptyStateDisplay = () => {
   const t = useT();
   return (
-    <div className="grid gap-2" id="emptystatedisplay">
-      <div className="rounded-lg bg-[#ffffff26] p-3 text-center text-white backdrop-blur-md">
-        <Text variant="text-10-light">{t("Polygons have not been created for this site")}</Text>
+    <div className="absolute left-[56%] top-[43%] z-30 grid gap-2" id="emptystatedisplay">
+      <div className="rounded-lg bg-[#ffffff26] py-3 px-11 text-center text-white backdrop-blur-md">
+        <Text variant="text-10-light">
+          {t("Polygons have not been")}
+          <br />
+          {t("created for this site")}
+        </Text>
       </div>
     </div>
   );
