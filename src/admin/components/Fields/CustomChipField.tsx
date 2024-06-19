d5d47937e6cb192f77b8219c29759af903ca6487
Limber Mamani Vallejos <limbermv2@gmail.com>
2024-06-19 14:28:48 -0400

    add conditional to stated status

--- src/admin/components/Fields/CustomChipField.tsx
+++ src/admin/components/Fields/CustomChipField.tsx
@@ -9,6 +9,7 @@ const STATUS_CLASSNAME_MAP: { [key: string]: string } = {
   "Awaiting approval": "bg-yellow-300 text-yellow-700",
   "Planting in progress": "bg-yellow-300 text-yellow-700",
   Draft: "bg-yellow-300 text-yellow-700",
+  Started: "bg-gray-300", // Draft Color
   Unknown: "",
   "Needs more information": "bg-yellow-300 text-yellow-700"
 };
@@ -23,7 +24,7 @@ const CustomChipField: React.FC<Omit<FunctionFieldProps, "render">> = props => (
           STATUS_CLASSNAME_MAP[record.readable_status]
         )}
       >
-        {record.readable_status}
+        {record.readable_status == "Started" ? "Draft" : record.readable_status}
       </div>
     )}
   />
