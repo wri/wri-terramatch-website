6f4cbf67117adbdfed1d2fd66c1401260de78f0f
cesarLima1 <cesmaulim@gmail.com>
2024-06-21 09:57:01 -0400

    [TM-850] update values for attributes options

--- src/admin/components/ResourceTabs/PolygonReviewTab/components/PolygonDrawer/components/AttributeInformation.tsx
+++ src/admin/components/ResourceTabs/PolygonReviewTab/components/PolygonDrawer/components/AttributeInformation.tsx
@@ -12,49 +12,49 @@ import { SitePolygon } from "@/generated/apiSchemas";
 const dropdownOptionsRestoration = [
   {
     title: "Tree Planting",
-    value: "Tree Planting"
+    value: "tree-planting"
   },
   {
     title: "Direct Seeding",
-    value: "Direct Seeding"
+    value: "direct-seeding"
   },
   {
     title: "Assisted Natural Regeneration",
-    value: "Assisted Natural Regeneration"
+    value: "assisted-natural-regeneration"
   }
 ];
 const dropdownOptionsTarget = [
   {
     title: "Agroforest",
-    value: "Agroforest"
+    value: "agroforest"
   },
   {
     title: "Natural Forest",
-    value: "Natural Forest"
+    value: "natural-forest"
   },
   {
     title: "Mangrove",
-    value: "Mangrove"
+    value: "mangrove"
   },
   {
     title: "Peatland",
-    value: "Peatland"
+    value: "peatland"
   },
   {
     title: "Riparian Area or Wetland",
-    value: "Riparian Area or Wetland"
+    value: "riparian-area-or-wetland"
   },
   {
     title: "Silvopasture",
-    value: "Silvopasture"
+    value: "silvopasture"
   },
   {
     title: "Woodlot or Plantation",
-    value: "Woodlot or Plantation"
+    value: "woodlot-or-plantation"
   },
   {
     title: "Urban Forest",
-    value: "Urban Forest"
+    value: "urban-forest"
   }
 ];
 
