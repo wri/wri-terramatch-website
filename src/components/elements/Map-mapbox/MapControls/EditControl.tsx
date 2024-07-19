3ae1bc293e3f2a6530bf4d39d0c5193053d1ae4d
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-07-19 15:40:49 -0400

    change conditional to use react-if

--- src/components/elements/Map-mapbox/MapControls/EditControl.tsx
+++ src/components/elements/Map-mapbox/MapControls/EditControl.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { When } from "react-if";
 
 import { useMapAreaContext } from "@/context/mapArea.provider";
 
@@ -18,46 +19,44 @@ const EditControl = ({ onClick, onSave, onCancel }: { onClick?: any; onSave?: an
     setIsEditing(false);
   };
   return (
-    <>
-      {!selectedPolyVersion?.uuid && (
-        <div className="flex w-[160px] flex-col items-center gap-1">
-          <Button
-            id="buttonEditPolygon"
-            variant="white"
-            className="w-full py-2"
-            onClick={() => {
-              setIsEditing(true);
-              onClick();
-            }}
-          >
-            <Text variant="text-12-bold" className="leading-[normal] text-black">
-              Edit Polygon
-            </Text>
-          </Button>
-          {isEditing && (
-            <div className="flex w-full items-center gap-1">
-              <Button onClick={handleSaveButton} className="w-full">
-                <Text variant="text-12-bold" className="leading-[normal]">
-                  Save
-                </Text>
-              </Button>
-              <Button
-                variant="sky-page-admin"
-                className="w-full"
-                onClick={() => {
-                  setIsEditing(false);
-                  onCancel();
-                }}
-              >
-                <Text variant="text-12-bold" className="leading-[normal]">
-                  Cancel
-                </Text>
-              </Button>
-            </div>
-          )}
-        </div>
-      )}
-    </>
+    <When condition={!selectedPolyVersion?.uuid}>
+      <div className="flex w-[160px] flex-col items-center gap-1">
+        <Button
+          id="buttonEditPolygon"
+          variant="white"
+          className="w-full py-2"
+          onClick={() => {
+            setIsEditing(true);
+            onClick();
+          }}
+        >
+          <Text variant="text-12-bold" className="leading-[normal] text-black">
+            Edit Polygon
+          </Text>
+        </Button>
+        <When condition={isEditing}>
+          <div className="flex w-full items-center gap-1">
+            <Button onClick={handleSaveButton} className="w-full">
+              <Text variant="text-12-bold" className="leading-[normal]">
+                Save
+              </Text>
+            </Button>
+            <Button
+              variant="sky-page-admin"
+              className="w-full"
+              onClick={() => {
+                setIsEditing(false);
+                onCancel();
+              }}
+            >
+              <Text variant="text-12-bold" className="leading-[normal]">
+                Cancel
+              </Text>
+            </Button>
+          </div>
+        </When>
+      </div>
+    </When>
   );
 };
 
