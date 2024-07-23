3463760bd7c7206c91bcb5c9bb04c2a8d2e323b5
Limber Mamani Vallejos <limbermv2@gmail.com>
2024-07-23 10:48:15 -0400

    [TM-1074] fixes

--- src/admin/components/ResourceTabs/InformationTab/components/ReportInformationAside/HighLevelMetrics.tsx
+++ src/admin/components/ResourceTabs/InformationTab/components/ReportInformationAside/HighLevelMetrics.tsx
@@ -4,7 +4,7 @@ import { Labeled, NumberField, useShowContext } from "react-admin";
 import { When } from "react-if";
 
 import { ContextCondition } from "@/context/ContextCondition";
-import { Framework } from "@/context/framework.provider";
+import { ALL_TF, Framework } from "@/context/framework.provider";
 
 const HighLevelMetics: FC = () => {
   const { record, resource } = useShowContext();
@@ -15,8 +15,7 @@ const HighLevelMetics: FC = () => {
   };
 
   const workdaysType = resource === "projectReport" ? "Project" : resource === "siteReport" ? "Site" : null;
-  const isTerrafund = record.framework_key === Framework.TF || record.framework_key === Framework.TF_LANDSCAPES;
-  const isSiteReport = resource === "siteReport" && isTerrafund;
+
   return (
     <Card>
       <Box paddingX={3.75} paddingY={2}>
@@ -27,49 +26,49 @@ const HighLevelMetics: FC = () => {
 
       <Box paddingX={3.75} paddingY={2}>
         <Stack gap={3}>
-          <When condition={!isSiteReport}>
-            <ContextCondition frameworksHide={[Framework.TF, Framework.TF_LANDSCAPES]}>
-              <Labeled label="Total Number Of Workdays Created" sx={inlineLabelSx}>
-                <NumberField source={record.workdays_total ? "workdays_total" : "total_workdays_count"} emptyText="0" />
+          <ContextCondition frameworksHide={ALL_TF}>
+            <Labeled label="Total Number Of Workdays Created" sx={inlineLabelSx}>
+              <NumberField source={record.workdays_total ? "workdays_total" : "total_workdays_count"} emptyText="0" />
+            </Labeled>
+            <When condition={workdaysType != null}>
+              <Labeled label={`Total Number Of Paid ${workdaysType} Workdays Created`} sx={inlineLabelSx}>
+                <NumberField source="workdays_paid" emptyText="0" />
               </Labeled>
-              <When condition={workdaysType != null}>
-                <Labeled label={`Total Number Of Paid ${workdaysType} Workdays Created`} sx={inlineLabelSx}>
-                  <NumberField source="workdays_paid" emptyText="0" />
-                </Labeled>
-                <Labeled label={`Total Number Of Volunteer ${workdaysType} Workdays Created`} sx={inlineLabelSx}>
-                  <NumberField source="workdays_volunteer" emptyText="0" />
-                </Labeled>
-              </When>
-            </ContextCondition>
-            <ContextCondition frameworksShow={[Framework.TF, Framework.TF_LANDSCAPES]}>
+              <Labeled label={`Total Number Of Volunteer ${workdaysType} Workdays Created`} sx={inlineLabelSx}>
+                <NumberField source="workdays_volunteer" emptyText="0" />
+              </Labeled>
+            </When>
+          </ContextCondition>
+          <ContextCondition frameworksShow={ALL_TF}>
+            <When condition={resource !== "siteReport"}>
               <Labeled label="Total Number Of Jobs Created" sx={inlineLabelSx}>
                 <NumberField source="total_jobs_created" emptyText="0" />
               </Labeled>
-            </ContextCondition>
-          </When>
+            </When>
+          </ContextCondition>
           <Labeled label="Total Number Of Trees Planted" sx={inlineLabelSx}>
             <NumberField
               source={record.trees_planted_count ? "trees_planted_count" : "total_trees_planted_count"}
               emptyText="0"
             />
           </Labeled>
-          <When condition={!isSiteReport}>
-            <ContextCondition frameworksShow={[Framework.PPC]}>
-              <When condition={resource === "projectReport" || resource === "siteReport"}>
-                <Labeled label="Total Number Of Seeds Planted" sx={inlineLabelSx}>
-                  <NumberField
-                    source={record.seeds_planted_count ? "trees_planted_count" : "total_seeds_planted_count"}
-                    emptyText="0"
-                  />
-                </Labeled>
-              </When>
-            </ContextCondition>
-            <ContextCondition frameworksShow={[Framework.TF, Framework.TF_LANDSCAPES]}>
+          <ContextCondition frameworksShow={[Framework.PPC]}>
+            <When condition={resource === "projectReport" || resource === "siteReport"}>
+              <Labeled label="Total Number Of Seeds Planted" sx={inlineLabelSx}>
+                <NumberField
+                  source={record.seeds_planted_count ? "trees_planted_count" : "total_seeds_planted_count"}
+                  emptyText="0"
+                />
+              </Labeled>
+            </When>
+          </ContextCondition>
+          <ContextCondition frameworksShow={ALL_TF}>
+            <When condition={resource !== "siteReport"}>
               <Labeled label="Total Number Of Seedlings" sx={inlineLabelSx}>
                 <NumberField source="seedlings_grown" emptyText="0" />
               </Labeled>
-            </ContextCondition>
-          </When>
+            </When>
+          </ContextCondition>
         </Stack>
       </Box>
     </Card>
