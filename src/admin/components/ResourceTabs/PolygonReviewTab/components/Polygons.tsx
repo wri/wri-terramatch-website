8124a6adc4fc69de2a7f4f9ec44c53665bbf4911
JORGE <egrojmonroy@gmail.com>
2024-08-06 11:25:26 -0400

    [TM-1183] get correct geojson of version polygon

--- src/admin/components/ResourceTabs/PolygonReviewTab/components/Polygons.tsx
+++ src/admin/components/ResourceTabs/PolygonReviewTab/components/Polygons.tsx
@@ -201,6 +201,7 @@ const Polygons = (props: IPolygonProps) => {
           isPolygonStatusOpen={isPolygonStatusOpen}
           refresh={props?.refresh}
           isOpenPolygonDrawer={isOpenPolygonDrawer}
+          setPolygonFromMap={setPolygonFromMap}
         />
       </Drawer>
       <div className="mb-4 flex items-center gap-1">
