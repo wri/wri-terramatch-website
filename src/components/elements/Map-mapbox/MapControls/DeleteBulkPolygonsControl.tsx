7985141c7cbd8a12a49b4773d47ce0b4007cb887
cesarLima1 <cesmaulim@gmail.com>
2024-08-21 16:32:08 -0400

    [TM-1198] add delete bulk polygon modal

--- src/components/elements/Map-mapbox/MapControls/DeleteBulkPolygonsControl.tsx
+++ src/components/elements/Map-mapbox/MapControls/DeleteBulkPolygonsControl.tsx
@@ -1,11 +1,48 @@
 import { useT } from "@transifex/react";
 
+import { ModalId } from "@/components/extensive/Modal/ModalConst";
+import ModalDeleteBulkPolygons from "@/components/extensive/Modal/ModalDeleteBulkPolygons";
+import { useMapAreaContext } from "@/context/mapArea.provider";
+import { useModalContext } from "@/context/modal.provider";
+import { useSitePolygonData } from "@/context/sitePolygon.provider";
+import { SitePolygon } from "@/generated/apiSchemas";
+
 import Button from "../../Button/Button";
 import Text from "../../Text/Text";
 
 const DeleteBulkPolygonsControl = () => {
   const t = useT();
+  const { openModal, closeModal } = useModalContext();
+  const context = useSitePolygonData();
+  const { selectedPolygonsInCheckbox } = useMapAreaContext();
+  const sitePolygonData = context?.sitePolygonData as undefined | Array<SitePolygon>;
 
+  const openFormModalHandlerDeleteBulkPolygons = () => {
+    openModal(
+      ModalId.SUBMIT_POLYGONS,
+      <ModalDeleteBulkPolygons
+        title={t("Delete Polygons")}
+        onClose={() => closeModal(ModalId.SUBMIT_POLYGONS)}
+        content={t("Confirm that the following polygons will be deleted. This operation is not reversible.")}
+        primaryButtonText={t("Delete")}
+        primaryButtonProps={{
+          className: "px-8 py-3",
+          variant: "primary",
+          onClick: () => {
+            closeModal(ModalId.SUBMIT_POLYGONS);
+          }
+        }}
+        secondaryButtonText={t("Cancel")}
+        secondaryButtonProps={{
+          className: "px-8 py-3",
+          variant: "white-page-admin",
+          onClick: () => closeModal(ModalId.SUBMIT_POLYGONS)
+        }}
+        sitePolygonData={sitePolygonData}
+        selectedPolygonsInCheckbox={selectedPolygonsInCheckbox}
+      />
+    );
+  };
   return (
     <div className="flex-col items-center gap-1">
       <div className="rounded-lg bg-[#ffffff26] bg-white p-3 text-center text-white">
@@ -15,7 +52,7 @@ const DeleteBulkPolygonsControl = () => {
         <Button
           variant="text"
           className="text-10-bold my-2 flex w-full justify-center rounded-lg border border-red bg-red p-2 hover:border-white"
-          onClick={() => {}}
+          onClick={() => openFormModalHandlerDeleteBulkPolygons()}
         >
           {t("Delete Polygons")}
         </Button>
