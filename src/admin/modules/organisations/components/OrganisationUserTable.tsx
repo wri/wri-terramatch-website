4ebf20ae3d186129c46beaf3e0a2bf605ab1db66
Dotty <dottykurmi123@gmail.com>
2024-08-27 11:34:24 -0400

    [TM-1227] add colums in OrganisationUserTable

--- src/admin/modules/organisations/components/OrganisationUserTable.tsx
+++ src/admin/modules/organisations/components/OrganisationUserTable.tsx
@@ -1,11 +1,37 @@
 import { Typography } from "@mui/material";
 import { Datagrid, DateField, FunctionField, ReferenceManyField, TextField } from "react-admin";
 
+import Menu from "@/components/elements/Menu/Menu";
+import { MENU_PLACEMENT_RIGHT_BOTTOM } from "@/components/elements/Menu/MenuVariant";
+import Text from "@/components/elements/Text/Text";
+import Icon, { IconNames } from "@/components/extensive/Icon/Icon";
 import { V2AdminUserRead } from "@/generated/apiSchemas";
 
 import modules from "../..";
 
 const OrganisationUserTable = () => {
+  const tableItemMenu = () => [
+    {
+      id: "1",
+      render: () => (
+        <div className="flex items-center gap-2" onClick={() => {}}>
+          <Text variant="text-12-bold" className="pr-2">
+            Approved
+          </Text>
+        </div>
+      )
+    },
+    {
+      id: "2",
+      render: () => (
+        <div className="flex items-center gap-2" onClick={() => {}}>
+          <Text variant="text-12-bold" className="pr-2">
+            Rejected
+          </Text>
+        </div>
+      )
+    }
+  ];
   return (
     <div>
       <Typography variant="h6" component="h3" mb={2}>
@@ -21,6 +47,20 @@ const OrganisationUserTable = () => {
           <FunctionField label="Name" render={(user: V2AdminUserRead) => `${user.first_name} ${user.last_name}`} />
           <TextField source="email_address" />
           <DateField label="Last login date" source="last_logged_in_at" locales="en-GB" />
+          <DateField label="Status" source="status" locales="en-GB" />
+          <FunctionField
+            label=""
+            render={() => (
+              <Menu menu={tableItemMenu()} placement={MENU_PLACEMENT_RIGHT_BOTTOM}>
+                <div className="rounded p-1 hover:bg-primary-200">
+                  <Icon
+                    name={IconNames.ELIPSES}
+                    className="roudn h-4 w-4 rounded-sm text-grey-720 hover:bg-primary-200"
+                  />
+                </div>
+              </Menu>
+            )}
+          />
         </Datagrid>
       </ReferenceManyField>
     </div>
