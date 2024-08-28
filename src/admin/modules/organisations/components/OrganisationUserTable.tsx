00c0ac6d250fdafca18a78ef0aa68ffe8b0ce6c2
Limber Mamani Vallejos <limbermv2@gmail.com>
2024-08-28 15:58:31 -0400

    [TM-1227] change page size

--- src/admin/modules/organisations/components/OrganisationUserTable.tsx
+++ src/admin/modules/organisations/components/OrganisationUserTable.tsx
@@ -139,7 +139,7 @@ const OrganisationUserTable = () => {
         hasPagination={true}
         classNameWrapper="max-h-[560px]"
         initialTableState={{
-          pagination: { pageSize: 5, pageIndex: 0 }
+          pagination: { pageSize: 50, pageIndex: 0 }
         }}
         key={tableKey}
         columns={[
