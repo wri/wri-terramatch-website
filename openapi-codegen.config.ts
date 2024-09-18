8ca593ae738df3583e4e9343e7965d69cf7cffc5
Nathan Curtis <nathan.curtis@gmail.com>
2024-09-17 17:02:33 -0700

    [TM-1272] Initial API fetcher and types generation for v3.
    (cherry picked from commit f65bfc718d6b3e73325cffbc1ab94a0d1383bcac)

--- openapi-codegen.config.ts
+++ openapi-codegen.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "@openapi-codegen/cli";
-import { generateReactQueryComponents, generateSchemaTypes } from "@openapi-codegen/typescript";
+import { generateFetchers, generateReactQueryComponents, generateSchemaTypes } from "@openapi-codegen/typescript";
 import dotenv from "dotenv";
 dotenv.config();
-
 export default defineConfig({
   api: {
     from: {
@@ -13,31 +12,43 @@ export default defineConfig({
     to: async context => {
       let paths = context.openAPIDocument.paths;
       let newPaths: any = {};
-
       //! Treat carefully this might potentially break the api generation
       // This Logic will make sure every sigle endpoint has a `operationId` key (needed to generate endpoints)
       Object.keys(paths).forEach((k, i) => {
         newPaths[k] = {};
         const eps = Object.keys(paths[k]).filter(ep => ep !== "parameters");
-
         eps.forEach((ep, i) => {
           const current = paths[k][ep];
           const operationId = ep + k.replaceAll("/", "-").replaceAll("{", "").replaceAll("}", "");
-
           newPaths[k][ep] = {
             ...current,
             operationId
           };
         });
       });
-
       context.openAPIDocument.paths = newPaths;
-
       const filenamePrefix = "api";
       const { schemasFiles } = await generateSchemaTypes(context, {
         filenamePrefix
       });
       await generateReactQueryComponents(context, {
+        filenamePrefix,
+        schemasFiles
+      });
+    }
+  },
+  apiV3: {
+    from: {
+      source: "url",
+      url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/user-service/api-json`
+    },
+    outputDir: "src/generated/v3",
+    to: async context => {
+      const filenamePrefix = "apiV3";
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix
+      });
+      await generateFetchers(context, {
         filenamePrefix,
         schemasFiles
       });
