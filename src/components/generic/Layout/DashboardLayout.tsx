1bcf0b175b60257f403ff72555af3834f80105c9
diego-morales-flores-1996 <diego@vizonomy.com>
2024-09-20 16:26:21 -0400

    TM-1313 header filterst for dashboard

--- src/components/generic/Layout/DashboardLayout.tsx
+++ src/components/generic/Layout/DashboardLayout.tsx
@@ -1,7 +1,8 @@
 import { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from "react";
 
-import Button from "@/components/elements/Button/Button";
+import Dropdown from "@/components/elements/Inputs/Dropdown/Dropdown";
 import Text from "@/components/elements/Text/Text";
+import { OptionValue } from "@/types/common";
 
 import Sidebar from "../Sidebar/Sidebar";
 
@@ -19,9 +20,65 @@ const DashboardLayout = (props: PropsWithChildren<DashboardLayoutProps>) => {
             <Text variant={"text-28-bold"} className="text-white">
               TerraMatch Insights
             </Text>
-            <Button variant="sky-page-admin">Export</Button>
+            <div className="relative">
+              <div className="absolute h-full w-full rounded bg-white bg-opacity-20 backdrop-blur-md" />
+              <button className="relative z-10 px-4 py-2 font-bold leading-normal text-white">Export</button>
+            </div>
           </div>
-          <div>filters</div>
+          <div className="flex items-center gap-3">
+            <div className="relative rounded-lg border border-[#989E97]">
+              <div className="absolute h-full w-full rounded-lg bg-white bg-opacity-20 backdrop-blur-md" />
+              <Dropdown
+                containerClassName="relative z-10"
+                className="text-white"
+                placeholder="Framework"
+                options={[]}
+                onChange={function (value: OptionValue[]): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            </div>
+            <div className="relative rounded-lg border border-[#989E97]">
+              <div className="absolute h-full w-full rounded-lg bg-white bg-opacity-20 backdrop-blur-md" />
+              <Dropdown
+                containerClassName="relative z-10"
+                className="text-white"
+                placeholder="Landscape"
+                options={[]}
+                onChange={function (value: OptionValue[]): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            </div>
+            <div className="relative rounded-lg border border-[#989E97]">
+              <div className="absolute h-full w-full rounded-lg bg-white bg-opacity-20 backdrop-blur-md" />
+              <Dropdown
+                containerClassName="relative z-10"
+                className="text-white"
+                placeholder="Country"
+                options={[]}
+                onChange={function (value: OptionValue[]): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            </div>
+            <div className="relative rounded-lg border border-[#989E97]">
+              <div className="absolute h-full w-full rounded-lg bg-white bg-opacity-20 backdrop-blur-md" />
+              <Dropdown
+                containerClassName="relative z-10"
+                className="text-white"
+                placeholder="Organization"
+                options={[]}
+                onChange={function (value: OptionValue[]): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            </div>
+            <div className="relative -scale-x-150">
+              <div className="absolute h-full w-full rounded bg-white bg-opacity-20 backdrop-blur-md" />
+              <button className="text-8 relative z-10 p-1 text-white">X</button>
+            </div>
+          </div>
         </header>
         {props.children}
       </main>
