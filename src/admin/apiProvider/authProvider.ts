4e4f9c5d4175c1bcc4005f36a24d8d99769b85ed
Nathan Curtis <nathan.curtis@gmail.com>
2024-09-30 15:15:31 -0700

    [TM-1312] Get the admin site working again.

--- src/admin/apiProvider/authProvider.ts
+++ src/admin/apiProvider/authProvider.ts
@@ -1,54 +1,41 @@
 import { AuthProvider } from "react-admin";
 
-import { fetchGetAuthLogout } from "@/generated/apiComponents";
+import { isAdmin, UserRole } from "@/admin/apiProvider/utils/user";
+import { loginConnection, logout } from "@/connections/Login";
+import { myUserConnection } from "@/connections/User";
+import { loadConnection } from "@/utils/loadConnection";
 import Log from "@/utils/log";
 
-import { getAccessToken, removeAccessToken } from "./utils/token";
-
 export const authProvider: AuthProvider = {
-  // send username and password to the auth server and get back credentials
   login: async () => {
     Log.error("Admin app does not support direct login");
   },
 
-  // when the dataProvider returns an error, check if this is an authentication error
-  checkError: () => {
-    return Promise.resolve();
-  },
+  checkError: async () => {},
 
   // when the user navigates, make sure that their credentials are still valid
   checkAuth: async () => {
-    const token = getAccessToken();
-    if (!token) return Promise.reject();
+    const { user } = await loadConnection(myUserConnection);
+    if (user == null) throw "No user logged in.";
 
-    // TODO (TM-1312) Once we have a connection for the users/me object, we can check the cached
-    //  value without re-fetching on every navigation in the admin UI. The previous implementation
-    //  is included below for reference until that ticket is complete.
-    // return new Promise((resolve, reject) => {
-    //   fetchGetAuthMe({})
-    //     .then(res => {
-    //       //@ts-ignore
-    //       if (isAdmin(res.data.role)) resolve();
-    //       else reject("Only admins are allowed.");
-    //     })
-    //     .catch(() => reject());
-    // });
+    if (!isAdmin(user.primaryRole as UserRole)) throw "Only admins are allowed.";
   },
-  // remove local credentials and notify the auth server that the user logged out
-  logout: async () => {
-    const token = getAccessToken();
-    if (!token) return Promise.resolve();
 
-    return new Promise(resolve => {
-      fetchGetAuthLogout({})
-        .then(async () => {
-          removeAccessToken();
-          window.location.replace("/auth/login");
-        })
-        .catch(() => {
-          resolve();
-        });
-    });
+  // remove local credentials
+  logout: async () => {
+    console.log("LOGOUT");
+    const { isLoggedIn } = await loadConnection(loginConnection);
+    if (isLoggedIn) {
+      logout();
+      window.location.replace("/auth/login");
+    }
+  },
+
+  getIdentity: async () => {
+    const { user } = await loadConnection(myUserConnection);
+    if (user == null) throw "No user logged in.";
+
+    return { id: user.uuid, fullName: user.fullName, primaryRole: user.primaryRole };
   },
 
   // get the user permissions (optional)
