28e520caa42bdf7964059d2cee3a398e1e5f88e3
Ismael Martinez <admin@scriptmatico.com>
2024-10-14 14:15:51 -0600

    feat: Implement Workdays For HBF Reporting

--- src/components/extensive/WorkdayCollapseGrid/WorkdaySection.tsx
+++ src/components/extensive/WorkdayCollapseGrid/WorkdaySection.tsx
@@ -5,23 +5,32 @@ import { When } from "react-if";
 
 import Text from "@/components/elements/Text/Text";
 import WorkdayRow from "@/components/extensive/WorkdayCollapseGrid/WorkdayRow";
+import { Framework } from "@/context/framework.provider";
 
 import Icon, { IconNames } from "../Icon/Icon";
 import { useSectionData } from "./hooks";
-import { Demographic, DEMOGRAPHIC_TYPE_MAP, DemographicType, WorkdayGridVariantProps } from "./types";
-
+import {
+  Demographic,
+  DEMOGRAPHIC_TYPE_MAP,
+  DemographicType,
+  HBF_DEMOGRAPHIC_TYPE_MAP,
+  HBFDemographicType,
+  WorkdayGridVariantProps
+} from "./types";
 export interface WorkdaySectionProps {
   demographics: Demographic[];
-  type: DemographicType;
+  type: DemographicType | HBFDemographicType;
   variant: WorkdayGridVariantProps;
+  framework: Framework;
   onChange?: (demographics: Demographic[]) => void;
 }
 
-const WorkdaySection = ({ demographics, type, variant, onChange }: WorkdaySectionProps) => {
+const WorkdaySection = ({ demographics, type, variant, onChange, framework }: WorkdaySectionProps) => {
   const [openMenu, setOpenMenu] = useState(false);
   const t = useT();
-  const { title, rows, total, position, subtypes } = useSectionData(type, demographics);
-  const { addSubtypeLabel } = DEMOGRAPHIC_TYPE_MAP[type];
+  const { title, rows, total, position, subtypes } = useSectionData(framework, type, demographics);
+  const demographicTypes = framework === Framework.HBF ? HBF_DEMOGRAPHIC_TYPE_MAP : DEMOGRAPHIC_TYPE_MAP;
+  const { addSubtypeLabel } = demographicTypes[type];
 
   const onRowChange = useCallback(
     (index: number, name: string, amount: number, userLabel?: string) => {
--- src/components/extensive/WorkdayCollapseGrid/hooks.ts
+++ src/components/extensive/WorkdayCollapseGrid/hooks.ts
@@ -1,7 +1,18 @@
 import { Dictionary, findLastIndex, uniq } from "lodash";
 import { useMemo } from "react";
 
-import { Demographic, DEMOGRAPHIC_TYPE_MAP, DEMOGRAPHIC_TYPES, DemographicType, Status } from "./types";
+import { Framework } from "@/context/framework.provider";
+
+import {
+  Demographic,
+  DEMOGRAPHIC_TYPE_MAP,
+  DEMOGRAPHIC_TYPES,
+  DemographicType,
+  HBF_DEMOGRAPHIC_TYPE_MAP,
+  HBF_DEMOGRAPHIC_TYPES,
+  HBFDemographicType,
+  Status
+} from "./types";
 
 export type Position = "first" | "last" | null;
 
@@ -19,29 +30,29 @@ export function calculateTotals(demographics: Demographic[]) {
       counts[type] += amount;
       return counts;
     },
-    { gender: 0, age: 0, ethnicity: 0 }
+    { gender: 0, age: 0, ethnicity: 0, caste: 0 }
   );
 
-  const total = Math.max(counts.age, counts.gender, counts.ethnicity);
-  const countsMatch = uniq([counts.age, counts.gender, counts.ethnicity]).length === 1;
-  return { total, countsMatch };
+  const total = Math.max(counts.age, counts.gender, counts.ethnicity, counts.caste);
+  const countsMatch = uniq([counts.age, counts.gender, counts.ethnicity, counts.caste]).length === 1;
+  return { counts, total, countsMatch };
 }
 
-export function useTableStatus(demographics: Demographic[]): { total: number; status: Status } {
+export function useTableStatus(framework: Framework, demographics: Demographic[]): { total: number; status: Status } {
   return useMemo(
     function () {
-      const { total, countsMatch } = calculateTotals(demographics);
+      const { counts, total, countsMatch } = calculateTotals(demographics);
 
       let status: Status = "in-progress";
       if (total === 0) {
         status = "not-started";
-      } else if (countsMatch) {
+      } else if (countsMatch || (framework === Framework.HBF && counts.gender !== 0)) {
         status = "complete";
       }
 
-      return { total, status };
+      return { total: framework === Framework.HBF ? counts.gender : total, status };
     },
-    [demographics]
+    [demographics, framework]
   );
 }
 
@@ -70,19 +81,37 @@ function mapRows(usesSubtype: boolean, typeMap: Dictionary<string>, demographics
     };
   });
 }
+export type FrameworkDemographicTypes<T extends Framework> = T extends Framework.HBF
+  ? HBFDemographicType
+  : DemographicType;
 
-export function useSectionData(type: DemographicType, demographics: Demographic[]) {
+function getDemographicTypes<T extends Framework>(framework: T): Readonly<string[]> {
+  return framework === Framework.HBF ? HBF_DEMOGRAPHIC_TYPES : DEMOGRAPHIC_TYPES;
+}
+
+function getDemographicTypesMap<T extends Framework>(framework: T) {
+  return framework === Framework.HBF ? HBF_DEMOGRAPHIC_TYPE_MAP : DEMOGRAPHIC_TYPE_MAP;
+}
+
+export function useSectionData<T extends Framework>(
+  framework: T,
+  type: FrameworkDemographicTypes<T>,
+  demographics: Demographic[]
+) {
+  const demographicTypes = getDemographicTypes(framework);
+  const demographicTypesMap = getDemographicTypesMap(framework);
+
   return useMemo(
     function () {
-      const { title, addSubtypeLabel, typeMap } = DEMOGRAPHIC_TYPE_MAP[type];
+      const { title, addSubtypeLabel, typeMap } = demographicTypesMap[type];
       const usesSubtype = addSubtypeLabel != null;
       const rows = mapRows(usesSubtype, typeMap, demographics);
       const total = rows.reduce((total, { amount }) => total + amount, 0);
-      const index = DEMOGRAPHIC_TYPES.indexOf(type);
+      const index = demographicTypes.indexOf(type);
       const position: Position = index == 0 ? "first" : index == DEMOGRAPHIC_TYPES.length - 1 ? "last" : null;
       const subtypes = usesSubtype ? typeMap : undefined;
       return { title, rows, total, position, subtypes };
     },
-    [demographics, type]
+    [demographics, type, demographicTypes, demographicTypesMap]
   );
 }
