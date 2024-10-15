2f8541ea36f0813773d33c82dc69f1753257d7e9
diego-morales-flores-1996 <diego@vizonomy.com>
2024-10-15 12:32:34 -0400

    TM-1343 fix build

--- src/pages/dashboard/components/TooltipGridMap.tsx
+++ src/pages/dashboard/components/TooltipGridMap.tsx
@@ -26,7 +26,7 @@ const TooltipGridMap = (props: TooltipGridProps) => {
   const t = useT();
   return (
     <div className="w-auto rounded bg-white p-2">
-      <div className="flex min-w-40 max-w-52 flex-col gap-1">
+      <div className="min-w-40 max-w-52 flex flex-col gap-1">
         <div className="mb-1 flex items-center gap-2">
           <img src={`/flags/${isoCountry?.toLowerCase()}.svg`} alt="flag" className="h-4 w-6 object-cover" />
           <Text className="text-start text-darkCustom" variant="text-12-bold">
