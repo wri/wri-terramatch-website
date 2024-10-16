4b83fa5ec7780b1e68dff292ee6d62d5bdb373ed
Dotty <dottykurmi123@gmail.com>
2024-10-16 12:13:07 -0400

    Monitored map

--- src/admin/components/ResourceTabs/MonitoredTab/MonitoredTab.tsx
+++ src/admin/components/ResourceTabs/MonitoredTab/MonitoredTab.tsx
@@ -11,20 +11,24 @@ const MonitoredTab: FC<IProps> = ({ label, ...rest }) => {
   return (
     <TabbedShowLayout.Tab label={label ?? "Monitored Data"} {...rest}>
       <div className="flex w-full gap-4">
-        <div className="w-1/5">
-          Map
+        <div className="flex w-1/5 flex-col gap-4">
+          <div className="relative overflow-hidden rounded-lg">
+            <img src="/images/map-img.png" alt="Monitored" />
+            <div className="absolute z-10 flex content-center ">
+              <button className="rounded-full bg-white px-2 py-1 text-primary">View Map</button>
+            </div>
+          </div>
           <Input
             name="email"
             type="text"
             label="Polygon Name"
-            variant={"login"}
+            variant={"monitored"}
             required={false}
             placeholder=" "
             id="email"
-            labelClassName="opacity-50 text-blueCustom-700 origin-left
-              transition-transform duration-[0.3s,color] delay-[0.3s]
-              absolute label-login text-14-light normal-case"
+            labelClassName="capitalize text-14 mb-2"
             classNameContainerInput="!mt-0"
+            containerClassName="flex flex-col gap-1.5"
             classNameError="!mt-0"
           />
         </div>
