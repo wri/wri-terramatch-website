3dc91b5a3f922c918f893c9137c737c09dab0fbd
Dotty <dottykurmi123@gmail.com>
2024-11-07 16:41:57 -0400

    [TM-1407] add blur container

---
+++ src/components/extensive/BlurContainer/BlurContainer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { When } from "react-if";
+
+import Text from "@/components/elements/Text/Text";
+
+export interface BlurContainerProps {
+  isBlur: boolean;
+  textInformation?: string;
+  children: React.ReactNode;
+}
+
+const BlurContainer = ({ isBlur, textInformation, children, ...props }: BlurContainerProps) => {
+  return (
+    <div className="relative w-full text-black">
+      <div
+        className={`absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-xl   ${
+          isBlur ? "z-10 bg-[#9d9a9a29] backdrop-blur-md" : ""
+        }`}
+      >
+        <When condition={isBlur && textInformation}>
+          <Text variant="text-12-semibold" className="h-fit w-fit max-w-[80%] rounded-lg bg-white px-4 py-3">
+            {textInformation}
+          </Text>
+        </When>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default BlurContainer;
