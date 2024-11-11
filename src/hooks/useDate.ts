9fa88aa7884ac85981cbccb5e6740dda7cbd8d31
Ismael Martinez <admin@scriptmatico.com>
2024-11-11 17:40:43 -0600

    fix: "Nothing to report" selection bumps user to page 1 of reports

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,7 +1,9 @@
 import { format as _format, subMonths, subWeeks } from "date-fns";
-import * as Locales from "date-fns/locale";
+import * as DateLocales from "date-fns/locale";
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
+const Locales = DateLocales as Record<string, any>;
 /**
  * Collection of most used date manipulator and formatter.
  * @returns { format, subMonths, subWeeks }
@@ -16,11 +18,14 @@ export const useDate = () => {
    * @param format default: dd/MM/yyyy
    * @returns string
    */
-  const format = (date?: string | number | Date, format = "dd/MM/yyyy") => {
-    if (!date) return "";
-    // @ts-ignore
-    return _format(new Date(date), format, { locale: Locales[formattedLocale] ?? Locales.enUS });
-  };
+  const format = useCallback(
+    (date?: string | number | Date, format = "dd/MM/yyyy") => {
+      if (!date) return "";
+
+      return _format(new Date(date), format, { locale: Locales[formattedLocale] ?? Locales.enUS });
+    },
+    [formattedLocale]
+  );
 
   return { format, subMonths, subWeeks };
 };
