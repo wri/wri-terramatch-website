a129870777103a52fa035dfa9c2032b20ba80c95
Dotty <dottykurmi123@gmail.com>
2024-11-12 14:26:22 -0400

    [TM-1378] [TM-1375] Increase the height of the tables

--- src/components/elements/Table/TableVariants.ts
+++ src/components/elements/Table/TableVariants.ts
@@ -126,7 +126,7 @@ export const VARIANT_TABLE_ORGANISATION = {
 export const VARIANT_TABLE_DASHBOARD_COUNTRIES = {
   table: "border-collapse",
   name: "border-airtable",
-  tableWrapper: "border border-neutral-200 rounded-lg overflow-auto max-h-[267px] lg:max-h-[284px] wide:max-h-[304px]",
+  tableWrapper: "border border-neutral-200 rounded-lg overflow-auto max-h-[534px] lg:max-h-[568px] wide:max-h-[608px]",
   trHeader: "bg-neutral-150 sticky top-0 z-auto",
   thHeader: "text-nowrap first:pl-3 first:pr-2 last:pl-2 last:pr-3 border-y border-neutral-200 text-12 px-3 border-t-0",
   tBody: "",
