fad0b77d1214e0266f998d1912a0b51d4dcffa38
Dotty <dottykurmi123@gmail.com>
2024-12-19 12:18:42 -0400

    [TM-1561] fix width

--- src/admin/components/ResourceTabs/PolygonReviewTab/components/PolygonDrawer/components/VersionHistory.tsx
+++ src/admin/components/ResourceTabs/PolygonReviewTab/components/PolygonDrawer/components/VersionHistory.tsx
@@ -363,7 +363,7 @@ const VersionHistory = ({
             options={polygonVersionData ?? []}
             optionVariant="text-12-light"
             titleClassname="one-line-text !w-full !text-nowrap"
-            titleContainerClassName="!w-[92%] !text-nowrap"
+            titleContainerClassName="!w-[calc(100%-25px)] !text-nowrap"
             defaultValue={[selectPolygonVersion?.uuid ?? selectedPolygon?.uuid] as string[]}
             onChange={e => {
               const polygonVersionData = (data as SitePolygonsDataResponse)?.find(item => item.uuid === e[0]);
