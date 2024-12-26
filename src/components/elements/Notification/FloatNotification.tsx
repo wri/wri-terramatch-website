2d6f02d20ba0136d3beac02fe9c0fd878f710da6
Jorge Monroy <egrojmonroy@gmail.com>
2024-12-26 11:49:41 -0400

    [TM-1531] add job connection (#745)
    * [TM-1531] add job service url and script
    
    * [TM-1531] apiSlice add job resource
    
    * [TM-1531] updates for job service
    
    * [TM-1531] sort alphabetical
    
    * [TM-1531] add connection for list the delayed jobs
    
    * [TM-1531] remove unnecesary code and consistency in selector and API request
    
    * [TM-1531] add bulk update call on clear
    
    * [TM-1531] add entityname and display data in notification
    
    * [TM-1531] use store and display only not acknowledged
    
    * [TM-1531] remove from apifetcher call and add continous call for delayed jobs
    
    * [TM-1531] rollback apifetcher functionality
    
    * [TM-1531] adding message to notification
    
    * [TM-1531] display error messages in notifications
    
    * [TM-1531] add error messages for failed jobs
    
    * [TM-1531] improve connection and remove awaits and asyncs
    
    * [TM-1531] changes and remove log
    
    * [TM-1531] yarn generate:services
    
    * [TM-1531] divide names loading for connections
    
    * [TM-1531] running with build
    
    * [TM-1531] only send not pending
    
    * [TM-1531] fix loaded delayedjobs and filter
    
    * [TM-1531] remove logs and unused variable
    
    * [TM-1531] join both connections
    
    * [TM-1531] transifex and fix selector

--- src/components/elements/Notification/FloatNotification.tsx
+++ src/components/elements/Notification/FloatNotification.tsx
@@ -1,8 +1,13 @@
+import { LinearProgress } from "@mui/material";
+import { useT } from "@transifex/react";
 import classNames from "classnames";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { When } from "react-if";
 
 import Icon, { IconNames } from "@/components/extensive/Icon/Icon";
+import { triggerBulkUpdate, useDelayedJobs } from "@/connections/DelayedJob";
+import { DelayedJobData, DelayedJobDto } from "@/generated/v3/jobService/jobServiceSchemas";
+import { getErrorMessageFromPayload } from "@/utils/errors";
 
 import LinearProgressBar from "../ProgressBar/LinearProgressBar/LinearProgressBar";
 import Text from "../Text/Text";
@@ -17,8 +22,50 @@ export interface FloatNotificationProps {
   data: FloatNotificationDataProps[];
 }
 
-const FloatNotification = ({ data }: FloatNotificationProps) => {
+const FloatNotification = () => {
+  const t = useT();
   const [openModalNotification, setOpenModalNotification] = useState(false);
+  const [isLoaded, { delayedJobs }] = useDelayedJobs();
+  const [notAcknowledgedJobs, setNotAcknowledgedJobs] = useState<DelayedJobDto[]>([]);
+  const clearJobs = () => {
+    if (delayedJobs === undefined) return;
+    const newJobsData: DelayedJobData[] = delayedJobs
+      .filter((job: DelayedJobDto) => job.status !== "pending")
+      .map((job: DelayedJobDto) => {
+        return {
+          uuid: job.uuid,
+          type: "delayedJobs",
+          attributes: {
+            isAcknowledged: true
+          }
+        };
+      });
+    triggerBulkUpdate(newJobsData);
+  };
+  useEffect(() => {
+    if (delayedJobs === undefined) return;
+    const notAcknowledgedJobs = delayedJobs.filter((job: DelayedJobDto) => !job.isAcknowledged);
+    setNotAcknowledgedJobs(notAcknowledgedJobs);
+  }, [delayedJobs]);
+  useEffect(() => {
+    if (!notAcknowledgedJobs.length) {
+      setOpenModalNotification(false);
+    }
+  }, [notAcknowledgedJobs]);
+  const listOfPolygonsFixed = (data: Record<string, any> | null) => {
+    if (data?.updated_polygons) {
+      const updatedPolygonNames = data.updated_polygons
+        ?.map((p: any) => p.poly_name)
+        .filter(Boolean)
+        .join(", ");
+      if (updatedPolygonNames) {
+        return "Success! The following polygons have been fixed: " + updatedPolygonNames;
+      } else {
+        return "No polygons were fixed";
+      }
+    }
+    return null;
+  };
 
   return (
     <div className="fixed bottom-10 right-10 z-50">
@@ -31,43 +78,88 @@ const FloatNotification = ({ data }: FloatNotificationProps) => {
           )}
         >
           <Text variant="text-20-bold" className="border-b border-grey-350 p-6 text-blueCustom-900">
-            Notifications
+            {t("Notifications")}
           </Text>
           <div className="flex flex-col overflow-hidden px-6 pb-8 pt-6">
             <div className="mb-2 flex items-center justify-between">
               <Text variant="text-14-light" className="text-neutral-400">
-                Actions Taken
+                {t("Actions Taken")}
               </Text>
-              <Text variant="text-12-semibold" className="text-primary">
-                Clear completed
+              <Text variant="text-12-semibold" className="text-primary" onClick={clearJobs}>
+                {t("Clear completed")}
               </Text>
             </div>
             <div className="-mr-2 flex flex-1 flex-col gap-3 overflow-auto pr-2">
-              {data.map((item, index) => (
-                <div key={index} className="rounded-lg border-2 border-grey-350 bg-white p-4 hover:border-primary">
-                  <div className="mb-2 flex items-center gap-1">
-                    <div className="h-2 w-2 rounded-full bg-primary" />
-                    <Text variant="text-14-light" className="leading-[normal] text-darkCustom " as={"span"}>
-                      {item.label}
+              {isLoaded &&
+                notAcknowledgedJobs &&
+                notAcknowledgedJobs.map((item, index) => (
+                  <div key={index} className="rounded-lg border-2 border-grey-350 bg-white p-4 hover:border-primary">
+                    <div className="mb-2 flex items-center gap-1">
+                      <div className="h-2 w-2 rounded-full bg-primary" />
+                      <Text variant="text-14-light" className="leading-[normal] text-darkCustom " as={"span"}>
+                        {item.name}
+                      </Text>
+                    </div>
+                    <Text variant="text-14-light" className="text-darkCustom">
+                      Site: <b>{item.entityName}</b>
                     </Text>
+                    <div className="mt-2">
+                      {item.status === "failed" ? (
+                        <Text variant="text-12-semibold" className="text-error-600">
+                          {item.payload ? t(getErrorMessageFromPayload(item.payload)) : t("Failed to complete")}
+                        </Text>
+                      ) : (
+                        <div className="flex items-center gap-2">
+                          {item.name === "Polygon Upload" &&
+                          (item.processedContent === null || item.totalContent === null) &&
+                          item.status === "pending" ? (
+                            <div style={{ width: "100%" }}>
+                              <LinearProgress
+                                sx={{
+                                  height: 9,
+                                  borderRadius: 99,
+                                  backgroundColor: "#a9e7d6",
+                                  "& .MuiLinearProgress-bar": { backgroundColor: "#29c499" }
+                                }}
+                              />
+                            </div>
+                          ) : (
+                            <LinearProgressBar
+                              value={
+                                item.status === "succeeded"
+                                  ? 100
+                                  : ((item.processedContent ?? 0) / (item.totalContent ?? 1)) * 100
+                              }
+                              className="h-2 bg-success-40"
+                              color="success-600"
+                            />
+                          )}
+                          <Text variant="text-12-semibold" className="text-black">
+                            {item.name === "Polygon Upload"
+                              ? item.status === "succeeded"
+                                ? t("Done!")
+                                : ""
+                              : item.status === "succeeded"
+                              ? t("Done!")
+                              : `${Math.round(((item.processedContent ?? 0) / (item.totalContent ?? 1)) * 100)}%`}
+                          </Text>
+                        </div>
+                      )}
+
+                      {item.status === "succeeded" && (
+                        <Text variant="text-12-light" className="mt-2 text-neutral-500">
+                          {listOfPolygonsFixed(item.payload)}
+                        </Text>
+                      )}
+                    </div>
                   </div>
-                  <Text variant="text-14-light" className="text-darkCustom">
-                    Site: <b>{item.site}</b>
-                  </Text>
-                  <div className="mt-2 flex items-center gap-2">
-                    <LinearProgressBar value={parseInt(item.value)} className="h-2 bg-success-40" color="success-600" />
-                    <Text variant="text-12-semibold" className="text-black">
-                      {item.value}
-                    </Text>
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
-        <When condition={data.length > 0}>
+        <When condition={isLoaded && (notAcknowledgedJobs ?? []).length > 0}>
           <div className="text-14-bold absolute right-[-5px] top-[-5px] z-20 flex min-h-[24px] min-w-[24px] items-center justify-center rounded-full bg-red-300 leading-[normal] text-white">
-            {data.length}
+            {notAcknowledgedJobs?.length}
           </div>
         </When>
         <button
@@ -77,8 +169,8 @@ const FloatNotification = ({ data }: FloatNotificationProps) => {
           className={classNames(
             "z-10 flex h-15 w-15 items-center justify-center rounded-full border border-grey-950 bg-primary duration-300  hover:scale-105",
             {
-              hidden: data.length < 1,
-              visible: data.length > 0
+              hidden: (notAcknowledgedJobs?.length ?? 0) === 0,
+              visible: (notAcknowledgedJobs?.length ?? 0) > 0
             }
           )}
         >
