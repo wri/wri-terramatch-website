b86ce8c2d2695a226de3d467438e2fa808736103
Nathan Curtis <nathan.curtis@gmail.com>
2025-01-24 10:30:22 -0800

    [TM-1641] Make the useOnMount hook able to accept an async function.

--- src/hooks/useOnMount.ts
+++ src/hooks/useOnMount.ts
@@ -1,9 +1,18 @@
+import { isFunction } from "lodash";
 import { useEffect } from "react";
 
 /**
  * A simple hook to run an effect only once when the component mounts.
+ *
+ * Unlike the raw useEffect hook from React, an async function may be passed in here. The return
+ * value of the callback is checked, and if a function is returned, it is returned to useEffect,
+ * otherwise it is ignored.
  */
-export function useOnMount(callback: () => void) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(callback, []);
+export function useOnMount(callback: () => unknown) {
+  useEffect(() => {
+    const result = callback();
+    // only return the result of the callback if it's a cleanup function.
+    if (isFunction(result)) return result;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 }
