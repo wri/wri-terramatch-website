661c995b31c6137f5963843ac4d102a176d6f9e9
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-02-05 12:28:06 -0400

    [TM-1614] add non trees progress section to hbf site (#889)
    * [TM-1614] add non trees progress section to hbf site
    
    * [TM-1614] Tree => Sapling

--- src/pages/project/[uuid]/tabs/GoalsAndProgress.tsx
+++ src/pages/project/[uuid]/tabs/GoalsAndProgress.tsx
@@ -349,7 +349,9 @@ const GoalsAndProgressTab = ({ project }: GoalsAndProgressProps) => {
       </PageRow>
 
       <PageRow>
-        <PageCard title={t("Tree Planting Progress")}>
+        <PageCard
+          title={t(project.framework_key == Framework.HBF ? "Sapling Planting Progress" : "Tree Planting Progress")}
+        >
           <div className="grid grid-cols-2 gap-16">
             <div className="flex flex-col gap-4">
               <ContextCondition frameworksShow={[Framework.PPC]}>
@@ -740,8 +742,8 @@ const GoalsAndProgressTab = ({ project }: GoalsAndProgressProps) => {
                 collection="non-tree"
                 modelUUID={project.uuid}
                 visibleRows={5}
-                setTotalNonTree={setTotalNonTreeSpecies}
                 setTotalCount={setNonTreeCount}
+                setTotalNonTreeSpecies={setTotalNonTreeSpecies}
               />
             </div>
           </PageCard>
