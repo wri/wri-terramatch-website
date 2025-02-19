a9cf1624fb08d4cf3d00589061d57b3a5eb331a9
cesarLima1 <cesmaulim@gmail.com>
2025-02-19 13:15:15 -0400

    [TM-1704] add space between labels

--- src/components/extensive/Modal/ModalStory.tsx
+++ src/components/extensive/Modal/ModalStory.tsx
@@ -61,7 +61,7 @@ const ModalStory = ({ className, preview, data, ...rest }: ModalStoryProps) => {
               variant={isMobile ? "text-12-light" : "text-14-light"}
               className="mt-4 leading-[normal] text-darkCustom"
             >
-              <b className={isMobile ? "text-12-bold" : "text-14-bold"}>Date Added: </b>
+              <b className={isMobile ? "text-12-bold" : "text-14-bold"}>{`Date Added: `}</b>
               {new Date(data?.date).toLocaleDateString("en-GB", {
                 day: "numeric",
                 month: "long",
