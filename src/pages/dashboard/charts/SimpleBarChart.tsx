4af8554d4a4d46a8fd4d242c1db1d5e8c248d8d0
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-02-25 17:11:48 -0400

    [MERGE] main -> staging post YY hotfix release (#985)
    * [RELEASE] Youthful Yucca hotfix 2025 02 21 (#973)
    
    * [TM-1785] remove thumbnail from preview component in impact stories (#967)
    
    * [TM-1787]  update style in section and modal impact story (#968)
    
    * [TM-1786] remove thumbnain from full screen impact story (#969)
    
    * [TM-1787]  update style in section and modal impact story (#971)
    
    * [TM-1784] Add info icon text for the Impact Story section (#972)
    
    * Fix/TM-1784 dashboard add info icon text impact story (#974)
    
    * [TM-1784] Add info icon text for the Impact Story section
    
    * [TM-1784] Add uppercase in tooltip
    
    * [TM-1790] get data when open modal in impact story list (#975)
    
    ---------
    
    Co-authored-by: Dotnara Condori <dottykurmi123@gmail.com>
    
    * [RELEASE] Youthful Yucca hotfix 2025 02 25
    
    * [TM-1692] add missing translations from simple bar chart
    
    * [TM-1692] add missing translations on GroupedBarChart
    
    ---------
    
    Co-authored-by: cesarLima1 <105736261+cesarLima1@users.noreply.github.com>
    Co-authored-by: Dotnara Condori <dottykurmi123@gmail.com>

--- src/pages/dashboard/charts/SimpleBarChart.tsx
+++ src/pages/dashboard/charts/SimpleBarChart.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useT } from "@transifex/react";
+import React, { useMemo } from "react";
 import { Bar, BarChart, CartesianGrid, Cell, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 import { getBarColorRestoration } from "@/utils/dashboardUtils";
@@ -21,11 +22,26 @@ type FormattedData = {
 };
 
 const SimpleBarChart = ({ data, total }: { data: ResturationStrategy[]; total?: number }) => {
-  const formattedData: FormattedData[] = data.map(item => ({
-    name: item.label.split(",").join(" + ").replace(/-/g, " "),
-    Value: item.value,
-    total: total ?? 0
-  }));
+  const t = useT();
+
+  const formattedData: FormattedData[] = useMemo(() => {
+    const mappingLabels = {
+      "Direct Seeding": t("Direct Seeding"),
+      "Assisted Natural Regeneration": t("Assisted Natural Regeneration"),
+      "Tree Planting": t("Tree Planting"),
+      "Multiple Strategies": t("Multiple Strategies")
+    };
+
+    return data.map(item => {
+      const labelName = item.label.split(",").join(" + ").replace(/-/g, " ");
+      const label = mappingLabels[labelName as keyof typeof mappingLabels] || labelName;
+      return {
+        name: label,
+        Value: item.value,
+        total: total ?? 0
+      };
+    });
+  }, [data, total, t]);
 
   return (
     <div className="h-72 w-full">
