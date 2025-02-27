8aca621550d9df45eeffad8a1ddbb00b5dd8cbe2
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-02-27 14:04:14 -0400

    add nursery log tab

--- src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
+++ src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
@@ -1,26 +1,39 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
 import Button from "@/components/elements/Button/Button";
 
 interface AuditLogSiteTabSelectionProps {
   buttonToggle: number;
   setButtonToggle: (buttonToggle: number) => void;
+  framework?: string;
 }
 
-const tabNames = ["Project Status", "Site Status", "Polygon Status"];
-
-const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({ buttonToggle, setButtonToggle }) => (
-  <div className="flex w-fit gap-1 rounded-lg bg-neutral-200 p-1">
-    {tabNames.map((tabName, index) => (
-      <Button
-        key={index}
-        variant={`${buttonToggle === index ? "white-toggle" : "transparent-toggle"}`}
-        onClick={() => setButtonToggle(index)}
-      >
-        {tabName}
-      </Button>
-    ))}
-  </div>
-);
+const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({
+  buttonToggle,
+  setButtonToggle,
+  framework = null
+}) => {
+  const tabNames = useMemo(() => {
+    const doesNotHaveNurseries = framework === null || ["ppc", "hbf"].includes(framework);
+    let tabs = ["Project Status", "Site Status", "Polygon Status"];
+    if (!doesNotHaveNurseries) {
+      tabs.push("Nursery Status");
+    }
+    return tabs;
+  }, [framework]);
+  return (
+    <div className="flex w-fit gap-1 rounded-lg bg-neutral-200 p-1">
+      {tabNames.map((tabName, index) => (
+        <Button
+          key={index}
+          variant={`${buttonToggle === index ? "white-toggle" : "transparent-toggle"}`}
+          onClick={() => setButtonToggle(index)}
+        >
+          {tabName}
+        </Button>
+      ))}
+    </div>
+  );
+};
 
 export default AuditLogSiteTabSelection;
