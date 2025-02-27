97a02e4f8e2df24924b1fd9574efa6b7125dbef3
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-02-27 14:18:12 -0400

    add project report

--- src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
+++ src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
@@ -6,21 +6,26 @@ interface AuditLogSiteTabSelectionProps {
   buttonToggle: number;
   setButtonToggle: (buttonToggle: number) => void;
   framework?: string;
+  isReport?: boolean;
 }
 
 const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({
   buttonToggle,
   setButtonToggle,
-  framework = null
+  framework = null,
+  isReport = false
 }) => {
   const tabNames = useMemo(() => {
+    if (isReport) {
+      return ["Project Report", "Site Report", "Polygon Report"];
+    }
     const doesNotHaveNurseries = framework === null || ["ppc", "hbf"].includes(framework);
     let tabs = ["Project Status", "Site Status", "Polygon Status"];
     if (!doesNotHaveNurseries) {
       tabs.push("Nursery Status");
     }
     return tabs;
-  }, [framework]);
+  }, [framework, isReport]);
   return (
     <div className="flex w-fit gap-1 rounded-lg bg-neutral-200 p-1">
       {tabNames.map((tabName, index) => (
---
+++ src/pages/reports/project-report/tabs/AuditLog.tsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { When } from "react-if";
+
+import AuditLogSiteTabSelection from "@/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection";
+import SiteAuditLogEntityStatus from "@/admin/components/ResourceTabs/AuditLogTab/components/SiteAuditLogEntityStatus";
+import SiteAuditLogEntityStatusSide from "@/admin/components/ResourceTabs/AuditLogTab/components/SiteAuditLogEntityStatusSide";
+import SiteAuditLogProjectStatus from "@/admin/components/ResourceTabs/AuditLogTab/components/SiteAuditLogProjectStatus";
+import { AuditLogButtonStates } from "@/admin/components/ResourceTabs/AuditLogTab/constants/enum";
+import PageBody from "@/components/extensive/PageElements/Body/PageBody";
+import PageCard from "@/components/extensive/PageElements/Card/PageCard";
+import PageColumn from "@/components/extensive/PageElements/Column/PageColumn";
+import PageRow from "@/components/extensive/PageElements/Row/PageRow";
+import LoadingContainer from "@/components/generic/Loading/LoadingContainer";
+import useAuditLogActions from "@/hooks/AuditStatus/useAuditLogActions";
+import { useValueChanged } from "@/hooks/useValueChanged";
+
+interface ReportingTasksProps {
+  projectReport: any;
+  label?: string;
+  refresh?: () => void;
+  enableChangeStatus?: number;
+}
+
+const AuditLog = ({
+  label,
+  projectReport,
+  refresh: refreshProject,
+  enableChangeStatus,
+  ...rest
+}: ReportingTasksProps) => {
+  const ButtonStates = {
+    PROJECT_REPORT: 0,
+    SITE_REPORT: 1,
+    NURSERY_REPORT: 2
+  };
+  const [buttonToggle, setButtonToggle] = useState(ButtonStates.PROJECT_REPORT);
+
+  const {
+    mutateEntity,
+    valuesForStatus,
+    statusLabels,
+    entityType,
+    entityListItem,
+    loadEntityList,
+    selected,
+    setSelected,
+    auditLogData,
+    refetch,
+    isLoading,
+    checkPolygonsSite
+  } = useAuditLogActions({
+    record: projectReport,
+    buttonToggle,
+    entityLevel: AuditLogButtonStates.PROJECT_REPORT
+  });
+
+  useValueChanged(buttonToggle, () => {
+    refetch();
+    loadEntityList();
+  });
+
+  return (
+    <PageBody>
+      <PageRow>
+        <PageColumn>
+          <LoadingContainer wrapInPaper loading={isLoading}>
+            <PageCard>
+              <div className="flex max-h-[200vh] gap-6 overflow-auto">
+                <div className="grid w-[64%] gap-6">
+                  <AuditLogSiteTabSelection
+                    buttonToggle={buttonToggle}
+                    setButtonToggle={setButtonToggle}
+                    isReport={true}
+                    framework={projectReport?.frameworkKey as string}
+                  />
+                  <When condition={buttonToggle === ButtonStates.PROJECT_REPORT}>
+                    <SiteAuditLogProjectStatus viewPD={true} record={projectReport} auditLogData={auditLogData} />
+                  </When>
+                  <When condition={buttonToggle !== ButtonStates.PROJECT_REPORT}>
+                    <SiteAuditLogEntityStatus
+                      record={selected}
+                      auditLogData={auditLogData}
+                      refresh={refetch}
+                      buttonToggle={buttonToggle}
+                      entityType={entityType}
+                      viewPD={true}
+                    />
+                  </When>
+                </div>
+                <div className="w-[32%] pl-8">
+                  <SiteAuditLogEntityStatusSide
+                    getValueForStatus={valuesForStatus}
+                    progressBarLabels={statusLabels}
+                    mutate={mutateEntity}
+                    refresh={() => {
+                      loadEntityList();
+                      refetch();
+                      refreshProject?.();
+                    }}
+                    record={selected}
+                    polygonList={entityListItem}
+                    selectedPolygon={selected}
+                    setSelectedPolygon={setSelected}
+                    checkPolygonsSite={checkPolygonsSite}
+                    entityType={entityType}
+                    showChangeRequest={false}
+                    viewPD={true}
+                  />
+                </div>
+              </div>
+            </PageCard>
+          </LoadingContainer>
+        </PageColumn>
+      </PageRow>
+      <br />
+      <br />
+    </PageBody>
+  );
+};
+
+export default AuditLog;
