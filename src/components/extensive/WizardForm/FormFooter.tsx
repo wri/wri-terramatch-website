0e80f53bd6a83f65a8220d0c6d5cd587f227a106
diego-morales-flores-1996 <diego@vizonomy.com>
2025-02-28 15:15:49 -0400

    TM-1697 sticky footbar for forms

--- src/components/extensive/WizardForm/FormFooter.tsx
+++ src/components/extensive/WizardForm/FormFooter.tsx
@@ -7,13 +7,18 @@ import Button, { IButtonProps } from "@/components/elements/Button/Button";
 interface FormFooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   submitButtonProps?: Omit<IButtonProps, "type" | "variant">;
   backButtonProps?: Omit<IButtonProps, "color" | "variant">;
+  variant?: "default" | "sticky";
 }
+const footerVariants = {
+  sticky: "sticky bottom-0 w-full bg-white py-4 px-16 shadow-top",
+  default: "w-full"
+} as const;
 
 export const FormFooter = (props: FormFooterProps) => {
   const { className, submitButtonProps, backButtonProps, ...rest } = props;
 
   return (
-    <div className="w-full">
+    <div className={classNames(footerVariants[props.variant ?? "default"], className)}>
       <div {...rest} className={classNames("flex w-full items-center justify-between", className)}>
         <When condition={!!backButtonProps}>
           <Button {...backButtonProps!} variant="secondary" />
