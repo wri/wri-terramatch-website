4a917d99e997b656240ab6c40954e70e773d4012
Limber Mamani Vallejos <limbermv2@gmail.com>
2025-02-28 11:12:24 -0400

    [TM-1058] add audit log funcionality to project report

--- src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
+++ src/admin/components/ResourceTabs/AuditLogTab/components/AuditLogSiteTabSelection.tsx
@@ -16,10 +16,14 @@ const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({
   isReport = false
 }) => {
   const tabNames = useMemo(() => {
-    if (isReport) {
-      return ["Project Report", "Site Report", "Polygon Report"];
-    }
     const doesNotHaveNurseries = framework === null || ["ppc", "hbf"].includes(framework);
+    if (isReport) {
+      let tabsReport = ["Project Report", "Site Report"];
+      if (!doesNotHaveNurseries) {
+        tabsReport.push("Nursery Report");
+      }
+      return tabsReport;
+    }
     let tabs = ["Project Status", "Site Status", "Polygon Status"];
     if (!doesNotHaveNurseries) {
       tabs.push("Nursery Status");
@@ -28,15 +32,18 @@ const AuditLogSiteTabSelection: FC<AuditLogSiteTabSelectionProps> = ({
   }, [framework, isReport]);
   return (
     <div className="flex w-fit gap-1 rounded-lg bg-neutral-200 p-1">
-      {tabNames.map((tabName, index) => (
-        <Button
-          key={index}
-          variant={`${buttonToggle === index ? "white-toggle" : "transparent-toggle"}`}
-          onClick={() => setButtonToggle(index)}
-        >
-          {tabName}
-        </Button>
-      ))}
+      {tabNames.map((tabName, index) => {
+        const realIndex = isReport ? index + 4 : index;
+        return (
+          <Button
+            key={realIndex}
+            variant={`${buttonToggle === realIndex ? "white-toggle" : "transparent-toggle"}`}
+            onClick={() => setButtonToggle(realIndex)}
+          >
+            {tabName}
+          </Button>
+        );
+      })}
     </div>
   );
 };
--- src/pages/reports/project-report/tabs/AuditLog.tsx
+++ src/pages/reports/project-report/tabs/AuditLog.tsx
@@ -6,12 +6,16 @@ import SiteAuditLogEntityStatus from "@/admin/components/ResourceTabs/AuditLogTa
 import SiteAuditLogEntityStatusSide from "@/admin/components/ResourceTabs/AuditLogTab/components/SiteAuditLogEntityStatusSide";
 import SiteAuditLogProjectStatus from "@/admin/components/ResourceTabs/AuditLogTab/components/SiteAuditLogProjectStatus";
 import { AuditLogButtonStates } from "@/admin/components/ResourceTabs/AuditLogTab/constants/enum";
+import { AuditLogEntity } from "@/admin/components/ResourceTabs/AuditLogTab/constants/types";
 import PageBody from "@/components/extensive/PageElements/Body/PageBody";
 import PageCard from "@/components/extensive/PageElements/Card/PageCard";
 import PageColumn from "@/components/extensive/PageElements/Column/PageColumn";
 import PageRow from "@/components/extensive/PageElements/Row/PageRow";
 import LoadingContainer from "@/components/generic/Loading/LoadingContainer";
+import { NURSERY_REPORT, PROJECT_REPORT, SITE_REPORT } from "@/constants/entities";
+import { useGetV2TasksUUIDReports } from "@/generated/apiComponents";
 import useAuditLogActions from "@/hooks/AuditStatus/useAuditLogActions";
+import { SelectedItem } from "@/hooks/AuditStatus/useLoadEntityList";
 import { useValueChanged } from "@/hooks/useValueChanged";
 
 interface ReportingTasksProps {
@@ -20,6 +24,12 @@ interface ReportingTasksProps {
   refresh?: () => void;
   enableChangeStatus?: number;
 }
+
+const reportTypesMappging: { [key: number]: string } = {
+  4: "project-reports",
+  5: "site-reports",
+  6: "nursery-reports"
+};
 
 const AuditLog = ({
   label,
@@ -33,31 +43,98 @@ const AuditLog = ({
     SITE_REPORT: 1,
     NURSERY_REPORT: 2
   };
-  const [buttonToggle, setButtonToggle] = useState(ButtonStates.PROJECT_REPORT);
+  const [buttonToggle, setButtonToggle] = useState(AuditLogButtonStates.PROJECT_REPORT);
+  const [selectedReport, setSelectedReport] = useState<SelectedItem | null>(null);
+
+  const { data: reportsResponse, isLoading: isLoadingReports } = useGetV2TasksUUIDReports({
+    pathParams: { uuid: projectReport.task_uuid }
+  });
+
+  const statusActionsMap = {
+    [AuditLogButtonStates.PROJECT_REPORT as number]: {
+      entityType: PROJECT_REPORT,
+      list: reportsResponse?.data
+        ?.filter(entity => entity.type == "project-report")
+        .map(report => ({
+          title: report.report_title,
+          uuid: report.uuid,
+          status: report.status,
+          value: report.uuid,
+          meta: report.status,
+          poly_id: undefined
+        }))
+    },
+    [AuditLogButtonStates.SITE_REPORT as number]: {
+      entityType: SITE_REPORT,
+      list: reportsResponse?.data
+        ?.filter(entity => entity.type == "site-report")
+        .map(report => ({
+          title: report.report_title,
+          uuid: report.uuid,
+          status: report.status,
+          value: report.uuid,
+          meta: report.status,
+          poly_id: undefined
+        }))
+    },
+    [AuditLogButtonStates.NURSERY_REPORT as number]: {
+      entityType: NURSERY_REPORT,
+      list: reportsResponse?.data
+        ?.filter(entity => entity.type == "nursery-report")
+        .map(report => ({
+          title: report.report_title,
+          uuid: report.uuid,
+          status: report.status,
+          value: report.uuid,
+          meta: report.status,
+          poly_id: undefined
+        }))
+    }
+  };
 
   const {
     mutateEntity,
     valuesForStatus,
     statusLabels,
     entityType,
-    entityListItem,
-    loadEntityList,
-    selected,
-    setSelected,
     auditLogData,
     refetch,
     isLoading,
     checkPolygonsSite
   } = useAuditLogActions({
-    record: projectReport,
+    record: selectedReport,
     buttonToggle,
-    entityLevel: AuditLogButtonStates.PROJECT_REPORT
+    entityLevel: buttonToggle
   });
 
-  useValueChanged(buttonToggle, () => {
+  useValueChanged(isLoadingReports, async () => {
+    if (!isLoadingReports) {
+      console.log("test");
+      await setSelectedReport(statusActionsMap[buttonToggle].list?.[0] ?? null);
+    }
     refetch();
-    loadEntityList();
   });
+
+  useValueChanged(buttonToggle, async () => {
+    if (statusActionsMap[buttonToggle].list) {
+      console.log("test1");
+      await setSelectedReport(statusActionsMap[buttonToggle].list[0] ?? null);
+    }
+    refetch();
+  });
+
+  const verifyEntityReport = () => {
+    switch (reportTypesMappging[buttonToggle!]) {
+      case "project-reports":
+        return PROJECT_REPORT;
+      case "site-reports":
+        return SITE_REPORT;
+      case "nursery-reports":
+        return NURSERY_REPORT;
+      default:
+        return entityType;
+    }
+  };
 
   return (
     <PageBody>
@@ -71,18 +148,18 @@ const AuditLog = ({
                     buttonToggle={buttonToggle}
                     setButtonToggle={setButtonToggle}
                     isReport={true}
-                    framework={projectReport?.frameworkKey as string}
+                    framework={projectReport?.framework_key as string}
                   />
                   <When condition={buttonToggle === ButtonStates.PROJECT_REPORT}>
                     <SiteAuditLogProjectStatus viewPD={true} record={projectReport} auditLogData={auditLogData} />
                   </When>
                   <When condition={buttonToggle !== ButtonStates.PROJECT_REPORT}>
                     <SiteAuditLogEntityStatus
-                      record={selected}
+                      record={selectedReport}
                       auditLogData={auditLogData}
                       refresh={refetch}
                       buttonToggle={buttonToggle}
-                      entityType={entityType}
+                      entityType={statusActionsMap[buttonToggle].entityType as AuditLogEntity}
                       viewPD={true}
                     />
                   </When>
@@ -93,16 +170,14 @@ const AuditLog = ({
                     progressBarLabels={statusLabels}
                     mutate={mutateEntity}
                     refresh={() => {
-                      loadEntityList();
                       refetch();
-                      refreshProject?.();
                     }}
-                    record={selected}
-                    polygonList={entityListItem}
-                    selectedPolygon={selected}
-                    setSelectedPolygon={setSelected}
+                    record={selectedReport}
+                    polygonList={statusActionsMap[buttonToggle].list}
+                    selectedPolygon={selectedReport}
+                    setSelectedPolygon={setSelectedReport}
                     checkPolygonsSite={checkPolygonsSite}
-                    entityType={entityType}
+                    entityType={verifyEntityReport()}
                     showChangeRequest={false}
                     viewPD={true}
                   />
