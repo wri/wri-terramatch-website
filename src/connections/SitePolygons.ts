04141c55cd6d5c507dc799a67ce13b2a283dbab4
Jorge Monroy <egrojmonroy@gmail.com>
2025-03-11 14:06:23 -0400

    [TM-1759] research service: add connection (#1014)
    * [TM-1759] add researchService values
    
    * [TM-1759] start adding tree cover table
    
    * [TM-1759] add connection for sitepolygons
    
    * [TM-1759] add site name to table
    
    * [TM-1759] research services missing
    
    * [TM-1759] fix style in table tree cover
    
    * [TM-1759] remove unused code
    
    * [TM-1759] fix rerendering warning
    
    * [TM-1759] fix dropdown issue
    
    * [TM-1759] remove redundant variables
    
    * [TM-1759] start adding indexes and cache to sitepolygon selector
    
    * [TM-1759] use props instead of params in connection sitepolygons
    
    * [TM-1759] improve code
    
    * [TM-1759] remove unused variables
    
    ---------
    
    Co-authored-by: cesarLima1 <cesmaulim@gmail.com>
    Co-authored-by: Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
    Co-authored-by: Dotty <dottykurmi123@gmail.com>

---
+++ src/connections/SitePolygons.ts
@@ -0,0 +1,78 @@
+import { createSelector } from "reselect";
+
+import { sitePolygonsIndex } from "@/generated/v3/researchService/researchServiceComponents";
+import { sitePolygonsIndexFetchFailed } from "@/generated/v3/researchService/researchServicePredicates";
+import { SitePolygonDto } from "@/generated/v3/researchService/researchServiceSchemas";
+import { ApiDataStore, indexMetaSelector, PendingErrorState } from "@/store/apiSlice";
+import { Connection } from "@/types/connection";
+import { connectionHook, connectionLoader } from "@/utils/connectionShortcuts";
+import { selectorCache } from "@/utils/selectorCache";
+
+export type SitePolygonIndexConnectionProps = {
+  entityName: string;
+  entityUuid: string;
+  pageSize?: number;
+  pageNumber?: number;
+};
+
+const ENTITY_QUERY_KEYS: Record<string, string> = {
+  projects: "projectId[]",
+  sites: "siteId[]"
+};
+
+export type SitePolygonIndexConnection<SitePolygonDto> = {
+  sitePolygons?: SitePolygonDto[];
+  fetchFailure?: PendingErrorState | null;
+};
+
+const sitePolygonQueryParams = (props: SitePolygonIndexConnectionProps) => {
+  const queryKey = ENTITY_QUERY_KEYS[props.entityName];
+  return {
+    queryParams: {
+      "page[number]": props.pageNumber ?? 0,
+      "page[size]": props.pageSize ?? 10,
+      ...(queryKey ? { [queryKey]: props.entityUuid } : {})
+    }
+  };
+};
+
+const indexIsLoaded = ({ sitePolygons, fetchFailure }: SitePolygonIndexConnection<SitePolygonDto>) =>
+  sitePolygons != null || fetchFailure != null;
+
+const sitePolygonCacheKey = (props: SitePolygonIndexConnectionProps) =>
+  `${props.entityName}:${props.entityUuid}:${props.pageSize}:${props.pageNumber}`;
+
+const sitePolygonsConnection: Connection<
+  SitePolygonIndexConnection<SitePolygonDto>,
+  SitePolygonIndexConnectionProps
+> = {
+  load: (connection, props) => {
+    if (!indexIsLoaded(connection)) sitePolygonsIndex(sitePolygonQueryParams(props));
+  },
+
+  isLoaded: indexIsLoaded,
+
+  selector: selectorCache(
+    props => sitePolygonCacheKey(props),
+    props =>
+      createSelector(
+        [
+          indexMetaSelector("sitePolygons", sitePolygonQueryParams(props)),
+          (store: ApiDataStore) => store.sitePolygons,
+          sitePolygonsIndexFetchFailed(sitePolygonQueryParams(props))
+        ],
+        (indexMeta, sitePolygonsStore, fetchFailure) => {
+          if (!indexMeta) return { fetchFailure };
+
+          const sitePolygons: SitePolygonDto[] = indexMeta.ids
+            .map(id => sitePolygonsStore[id]?.attributes as SitePolygonDto)
+            .filter(Boolean);
+
+          return { sitePolygons, fetchFailure };
+        }
+      )
+  )
+};
+
+export const useSitePolygons = connectionHook(sitePolygonsConnection);
+export const loadSitePolygons = connectionLoader(sitePolygonsConnection);
