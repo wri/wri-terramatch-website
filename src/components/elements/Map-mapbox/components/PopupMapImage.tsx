cccd4bb6bc78b647b040987b972eff17a9caa0aa
Jorge Monroy <egrojmonroy@gmail.com>
2025-04-07 15:59:18 -0400

    [TM-1919] dashboard project popup (#1129)
    * [TM-1855]  fix alignment for hectares under restoration graph
    
    * [TM-1919] UI: Only Show Projects With Polygons on Map and Clean-up Project Pop-up
    
    * [TM-1919] add popup content
    
    * [TM-1919] UI: style in close button popup map
    
    * [TM-1919] UI: style text  popup map
    
    * [TM-1919] add image value
    
    * [TM-1919] add learn more to popup
    
    ---------
    
    Co-authored-by: Dotty <dottykurmi123@gmail.com>

---
+++ src/components/elements/Map-mapbox/components/PopupMapImage.tsx
@@ -0,0 +1,48 @@
+import Text from "../../Text/Text";
+
+export type Item = {
+  id: string;
+  title: string;
+  value: string;
+};
+
+type PopupMapImageProps = {
+  label: string;
+  imageUrl?: string;
+  items?: Item[];
+  learnMore?: () => void;
+};
+
+const PopupMapImage = ({ label, imageUrl, items = [], learnMore }: PopupMapImageProps) => {
+  return (
+    <div
+      className="popup-project-image-map w-auto min-w-[17vw] max-w-[20vw] cursor-pointer rounded-lg bg-white lg:min-w-[17vw] lg:max-w-[15vw]"
+      onClick={learnMore}
+    >
+      <div className="flex flex-col">
+        <img
+          src={imageUrl || "/images/no-image-available.png"}
+          alt="Map preview of the project location"
+          className="h-30 w-full rounded-t-lg object-cover"
+        />
+        <div className="flex flex-col gap-2 p-2">
+          <Text variant="text-12-bold" className="leading-[normal]">
+            {label}
+          </Text>
+          {items.map(item => (
+            <div key={item.id} className="flex flex-col gap-0.5">
+              <Text variant="text-12-light" className="leading-[normal]">
+                {item.title}
+              </Text>
+              <Text variant="text-12-semibold" className="leading-[normal]">
+                {item.value}
+              </Text>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopupMapImage;
