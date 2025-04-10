44fbf5f26695f984c0f8b4670f6a34503164c9b9
Dotty <dottykurmi123@gmail.com>
2025-04-10 11:44:43 -0400

    [TM-1906] add close in popup mobile

--- src/components/elements/Map-mapbox/components/PopupMobile.tsx
+++ src/components/elements/Map-mapbox/components/PopupMobile.tsx
@@ -2,6 +2,7 @@ import { useT } from "@transifex/react";
 import React from "react";
 
 import Text from "@/components/elements/Text/Text";
+import Icon, { IconNames } from "@/components/extensive/Icon/Icon";
 import { LAYERS_NAMES } from "@/constants/layers";
 import { CountriesProps } from "@/context/dashboard.provider";
 import { usePopupData } from "@/pages/dashboard/hooks/usePopupsData";
@@ -32,7 +33,12 @@ export const PopupMobile: React.FC<MobilePopupProps> = ({ event }) => {
     }
   };
   return (
-    <div className="shadow-lg fixed bottom-4 left-1/2 z-50 w-[90vw] max-w-md -translate-x-1/2 rounded-xl bg-white p-4">
+    <div className="fixed bottom-4 left-1/2 z-50 w-[90vw] max-w-md -translate-x-1/2 rounded-xl bg-white p-3 shadow-monitored">
+      <div className="relative w-full">
+        <button className="absolute -right-4 -top-4 z-10 rounded-full border border-neutral-600 bg-white p-1.5 text-neutral-600 hover:border-primary hover:text-primary">
+          <Icon name={IconNames.CLEAR} className="h-2.5 w-2.5" />
+        </button>
+      </div>
       <div className="flex flex-col gap-2 p-2">
         <Text variant="text-12-bold" className="overflow-hidden leading-[normal] line-clamp-2">
           {t(label)}
