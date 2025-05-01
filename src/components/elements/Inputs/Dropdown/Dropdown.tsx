cbaba5a213d74df324f5a1f01bde7b90e3377b58
Nathan Curtis <nathan.curtis@gmail.com>
2025-05-01 13:25:27 -0700

    [TM-1958] Fix placeholder display.

--- src/components/elements/Inputs/Dropdown/Dropdown.tsx
+++ src/components/elements/Inputs/Dropdown/Dropdown.tsx
@@ -1,7 +1,7 @@
 import { Listbox, Transition } from "@headlessui/react";
 import { useT } from "@transifex/react";
 import classNames from "classnames";
-import { uniq } from "lodash";
+import { isEmpty, uniq } from "lodash";
 import React, { ChangeEvent, Fragment, PropsWithChildren, useCallback, useEffect, useMemo, useState } from "react";
 import { ErrorOption, FieldError } from "react-hook-form";
 import { twMerge as tw } from "tailwind-merge";
@@ -176,190 +176,186 @@ const Dropdown = (props: PropsWithChildren<DropdownProps>) => {
   return (
     <div className={tw("space-y-2", props.containerClassName, variant.containerClassName)}>
       <Listbox value={selected} defaultValue={selected} onChange={onChange} multiple={props.multiSelect}>
-        {({ open, value }) => (
-          <>
-            {props.label && (
-              <Listbox.Label as={Fragment}>
-                <div className="flex items-baseline gap-1">
-                  <InputLabel
-                    required={props.required}
-                    feedbackRequired={props.feedbackRequired}
-                    className={props.labelClassName}
-                    labelVariant={props.labelVariant}
-                  >
-                    {props.label}
-                  </InputLabel>
-                  {props.suffixLabelView && <div className="flex items-center">{props.suffixLabel}</div>}
-                </div>
-              </Listbox.Label>
-            )}
-            {props.description && <InputDescription>{props.description}</InputDescription>}
-            <Listbox.Button
-              as="div"
-              className={tw(
-                "flex h-10 items-center justify-between gap-3 rounded-lg px-3 py-2 hover:cursor-pointer",
-                props.error && "border border-error focus:border-error",
-                props.className,
-                variant.className
+        {({ open, value }) => {
+          const valuesFormatted = formatSelectedValues(
+            selected,
+            options,
+            value,
+            props.showLabelAsMultiple,
+            props.placeholder,
+            props.multipleText
+          );
+          return (
+            <>
+              {props.label && (
+                <Listbox.Label as={Fragment}>
+                  <div className="flex items-baseline gap-1">
+                    <InputLabel
+                      required={props.required}
+                      feedbackRequired={props.feedbackRequired}
+                      className={props.labelClassName}
+                      labelVariant={props.labelVariant}
+                    >
+                      {props.label}
+                    </InputLabel>
+                    {props.suffixLabelView && <div className="flex items-center">{props.suffixLabel}</div>}
+                  </div>
+                </Listbox.Label>
               )}
-            >
-              {props.prefix}
-              <div
-                className={tw(
-                  "flex items-center gap-2",
-                  variant.titleContainerClassName,
-                  props.titleContainerClassName
-                )}
-              >
-                <Text
-                  variant={props.inputVariant ?? "text-14-light"}
-                  className={tw("w-full", variant.titleClassname, props.titleClassname)}
-                  title={formatSelectedValues(
-                    selected,
-                    options,
-                    value,
-                    props.showLabelAsMultiple,
-                    props.placeholder,
-                    props.multipleText
-                  )}
-                >
-                  {formatSelectedValues(
-                    selected,
-                    options,
-                    value,
-                    props.showLabelAsMultiple,
-                    props.placeholder,
-                    props.multipleText
-                  ) ?? props.placeholder}
-                </Text>
-              </div>
-              {selected.length > 0 && showClear ? (
-                <div
-                  className={variant.iconClearContainerClassName}
-                  onClick={e => {
-                    e.stopPropagation();
-                    setSelected([]);
-                    onClear?.();
-                  }}
-                >
-                  <Icon
-                    name={variant.iconNameClear ?? IconNames.CLEAR}
-                    className={tw("fill-neutral-900", variant.iconClearClassName)}
-                  />
-                </div>
-              ) : (
-                <Icon
-                  name={variant.iconName ?? IconNames.CHEVRON_DOWN}
-                  className={tw("fill-neutral-900 transition", open && "rotate-180", variant.iconClassName)}
-                />
-              )}
-            </Listbox.Button>
-            <Transition
-              className="relative z-50 !m-0"
-              show={open}
-              enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
-            >
-              <Listbox.Options
+              {props.description && <InputDescription>{props.description}</InputDescription>}
+              <Listbox.Button
                 as="div"
                 className={tw(
-                  "border-light absolute mt-2 max-h-[235px] min-w-full overflow-auto rounded-lg bg-white outline-none lg:max-h-[250px] wide:max-h-[266px]",
-                  props.optionsClassName,
-                  variant.optionsClassName
+                  "flex h-10 items-center justify-between gap-3 rounded-lg px-3 py-2 hover:cursor-pointer",
+                  props.error && "border border-error focus:border-error",
+                  props.className,
+                  variant.className
                 )}
               >
-                {props.multiSelect && showSelectAll ? (
-                  <>
-                    <Listbox.Option
-                      as="div"
-                      key="all"
-                      value="all"
-                      className={classNames(
-                        tw(
-                          "w-full cursor-pointer p-3 hover:bg-primary-100",
-                          selected.length === options.length && "bg-primary-100",
-                          props.optionClassName
-                        )
-                      )}
-                      disabled={verifyDisableOption("All")}
-                    >
-                      <div className="flex items-center justify-between">
-                        <Checkbox
-                          name=""
-                          label="Select All"
-                          textClassName={classNames(variant.optionLabelClassName, "whitespace-nowrap")}
-                          inputClassName={classNames(variant.optionCheckboxClassName, "checked:bg-dash")}
-                          className={tw("flex flex-row-reverse items-center gap-3", variant.optionClassName)}
-                          checked={selected.length === options.length}
-                          onChange={() => {
-                            if (selected.length === options.length) {
-                              onChange([]);
-                            } else {
-                              onChange(options.map(option => option.value));
-                            }
-                          }}
-                        />
-                      </div>
-                    </Listbox.Option>
-                    <hr className="mx-3 border-grey-350" />
-                  </>
-                ) : null}
-                {options.map(option => {
-                  const isSelected = selected?.includes(option.value);
-                  return (
-                    <Listbox.Option
-                      as="div"
-                      key={option.value}
-                      value={option.value}
-                      className={classNames(
-                        tw(
-                          "w-full cursor-pointer p-3 hover:bg-primary-100",
-                          isSelected && !props.multiSelect && "bg-primary-100",
-                          props.optionClassName,
-                          verifyDisableOption(option.title) ? "cursor-not-allowed bg-grey-750 hover:bg-grey-750" : ""
-                        )
-                      )}
-                      disabled={verifyDisableOption(option.title)}
-                    >
-                      {props.multiSelect ? (
-                        <Checkbox
-                          name=""
-                          checked={isSelected}
-                          label={option.title}
-                          textClassName={variant.optionLabelClassName}
-                          inputClassName={variant.optionCheckboxClassName}
-                          className={tw("flex flex-row-reverse items-center gap-3", variant.optionClassName)}
-                          onChange={() => {
-                            !isSelected
-                              ? setSelected([...selected, option.value])
-                              : setSelected(selected.filter(value => value !== option.value));
-                          }}
-                        />
-                      ) : (
-                        <div className="flex items-center gap-2">
-                          {option.prefix}
-                          <Text
-                            variant={`${props.optionVariant ?? "text-14-light"}`}
-                            className={tw(
-                              option.meta ? "w-[63%] break-words" : "break-words",
-                              props.optionTextClassName
-                            )}
-                          >
-                            {option.title}
-                          </Text>
+                {props.prefix}
+                <div
+                  className={tw(
+                    "flex items-center gap-2",
+                    variant.titleContainerClassName,
+                    props.titleContainerClassName
+                  )}
+                >
+                  <Text
+                    variant={props.inputVariant ?? "text-14-light"}
+                    className={tw("w-full", variant.titleClassname, props.titleClassname)}
+                    title={valuesFormatted}
+                  >
+                    {isEmpty(valuesFormatted) ? props.placeholder : valuesFormatted}
+                  </Text>
+                </div>
+                {selected.length > 0 && showClear ? (
+                  <div
+                    className={variant.iconClearContainerClassName}
+                    onClick={e => {
+                      e.stopPropagation();
+                      setSelected([]);
+                      onClear?.();
+                    }}
+                  >
+                    <Icon
+                      name={variant.iconNameClear ?? IconNames.CLEAR}
+                      className={tw("fill-neutral-900", variant.iconClearClassName)}
+                    />
+                  </div>
+                ) : (
+                  <Icon
+                    name={variant.iconName ?? IconNames.CHEVRON_DOWN}
+                    className={tw("fill-neutral-900 transition", open && "rotate-180", variant.iconClassName)}
+                  />
+                )}
+              </Listbox.Button>
+              <Transition
+                className="relative z-50 !m-0"
+                show={open}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Listbox.Options
+                  as="div"
+                  className={tw(
+                    "border-light absolute mt-2 max-h-[235px] min-w-full overflow-auto rounded-lg bg-white outline-none lg:max-h-[250px] wide:max-h-[266px]",
+                    props.optionsClassName,
+                    variant.optionsClassName
+                  )}
+                >
+                  {props.multiSelect && showSelectAll ? (
+                    <>
+                      <Listbox.Option
+                        as="div"
+                        key="all"
+                        value="all"
+                        className={classNames(
+                          tw(
+                            "w-full cursor-pointer p-3 hover:bg-primary-100",
+                            selected.length === options.length && "bg-primary-100",
+                            props.optionClassName
+                          )
+                        )}
+                        disabled={verifyDisableOption("All")}
+                      >
+                        <div className="flex items-center justify-between">
+                          <Checkbox
+                            name=""
+                            label="Select All"
+                            textClassName={classNames(variant.optionLabelClassName, "whitespace-nowrap")}
+                            inputClassName={classNames(variant.optionCheckboxClassName, "checked:bg-dash")}
+                            className={tw("flex flex-row-reverse items-center gap-3", variant.optionClassName)}
+                            checked={selected.length === options.length}
+                            onChange={() => {
+                              if (selected.length === options.length) {
+                                onChange([]);
+                              } else {
+                                onChange(options.map(option => option.value));
+                              }
+                            }}
+                          />
                         </div>
-                      )}
-                    </Listbox.Option>
-                  );
-                })}
-              </Listbox.Options>
-            </Transition>
-          </>
-        )}
+                      </Listbox.Option>
+                      <hr className="mx-3 border-grey-350" />
+                    </>
+                  ) : null}
+                  {options.map(option => {
+                    const isSelected = selected?.includes(option.value);
+                    return (
+                      <Listbox.Option
+                        as="div"
+                        key={option.value}
+                        value={option.value}
+                        className={classNames(
+                          tw(
+                            "w-full cursor-pointer p-3 hover:bg-primary-100",
+                            isSelected && !props.multiSelect && "bg-primary-100",
+                            props.optionClassName,
+                            verifyDisableOption(option.title) ? "cursor-not-allowed bg-grey-750 hover:bg-grey-750" : ""
+                          )
+                        )}
+                        disabled={verifyDisableOption(option.title)}
+                      >
+                        {props.multiSelect ? (
+                          <Checkbox
+                            name=""
+                            checked={isSelected}
+                            label={option.title}
+                            textClassName={variant.optionLabelClassName}
+                            inputClassName={variant.optionCheckboxClassName}
+                            className={tw("flex flex-row-reverse items-center gap-3", variant.optionClassName)}
+                            onChange={() => {
+                              !isSelected
+                                ? setSelected([...selected, option.value])
+                                : setSelected(selected.filter(value => value !== option.value));
+                            }}
+                          />
+                        ) : (
+                          <div className="flex items-center gap-2">
+                            {option.prefix}
+                            <Text
+                              variant={`${props.optionVariant ?? "text-14-light"}`}
+                              className={tw(
+                                option.meta ? "w-[63%] break-words" : "break-words",
+                                props.optionTextClassName
+                              )}
+                            >
+                              {option.title}
+                            </Text>
+                          </div>
+                        )}
+                      </Listbox.Option>
+                    );
+                  })}
+                </Listbox.Options>
+              </Transition>
+            </>
+          );
+        }}
       </Listbox>
       {otherIsSelected ? (
         <Input
