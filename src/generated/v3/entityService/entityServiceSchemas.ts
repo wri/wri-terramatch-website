4a994cb75162339b3c86c5dbf64a906cce08fb80
Ismael Martinez <admin@scriptmatico.com>
2025-05-14 16:47:56 -0600

    [TM-1995] Update API

--- src/generated/v3/entityService/entityServiceSchemas.ts
+++ src/generated/v3/entityService/entityServiceSchemas.ts
@@ -486,12 +486,7 @@ export type ProjectFullDto = {
   isTest: boolean;
   feedback: string | null;
   feedbackFields: string[] | null;
-  cohort: string | null;
   continent: string | null;
-  /**
-   * The associated organisation type
-   */
-  organisationType: string | null;
   states: string[] | null;
   projectCountyDistrict: string | null;
   /**
