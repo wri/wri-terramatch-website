b0bd8ac3c494d7b8457626d7e89f7989f3aaacb8
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-05-16 16:14:18 -0400

    [TM-1943] add collection conditional to financial section (#1218)
    * [TM-1943] add collection conditional to financial section
    
    * [TM-1943] add t() from transifex to descriptions

--- src/components/elements/Inputs/FinancialTableInput/RHFFinancialIndicatorTable.tsx
+++ src/components/elements/Inputs/FinancialTableInput/RHFFinancialIndicatorTable.tsx
@@ -33,6 +33,7 @@ export interface RHFFinancialIndicatorsDataTableProps
   onChangeCapture?: () => void;
   formHook?: UseFormReturn;
   years?: Array<number>;
+  model?: string;
   formSubmissionOrg?: any;
 }
 
@@ -194,7 +195,7 @@ const RHFFinancialIndicatorsDataTable = ({
   const { field } = useController(props);
   const value = field?.value || [];
   const [files, setFiles] = useState<Partial<UploadedFile>[]>();
-  const { years, formSubmissionOrg } = props;
+  const { years, formSubmissionOrg, model } = props;
   const [selectCurrency, setSelectCurrency] = useState<OptionValue>(formSubmissionOrg?.currency);
   const [selectFinancialMonth, setSelectFinancialMonth] = useState<OptionValue>(formSubmissionOrg?.start_month);
   const [resetTable, setResetTable] = useState(0);
@@ -860,9 +861,8 @@ const RHFFinancialIndicatorsDataTable = ({
   useEffect(() => {
     const payload = {
       organisation_id: formSubmissionOrg?.uuid,
-      profit_analysis_data: formSubmissionOrg?.type?.includes("for-profit")
-        ? forProfitAnalysisData
-        : nonProfitAnalysisData,
+      profit_analysis_data: forProfitAnalysisData,
+      non_profit_analysis_data: nonProfitAnalysisData,
       current_radio_data: currentRadioData,
       documentation_data: documentationData,
       local_currency: selectCurrency as string,
@@ -921,57 +921,67 @@ const RHFFinancialIndicatorsDataTable = ({
       <div className="mb-10 space-y-6">
         <Dropdown
           options={getCurrencyOptions(t)}
-          label="Local Currency"
-          placeholder="USD - US Dollar"
+          label={t("Local Currency")}
+          placeholder={t("USD - US Dollar")}
           value={[selectCurrency]}
           defaultValue={formSubmissionOrg?.currency ? [formSubmissionOrg?.currency] : [selectCurrency]}
           onChange={e => setSelectCurrency(e?.[0])}
         />
         <Dropdown
           options={getMonthOptions(t)}
-          label="Financial Year Start Month"
-          placeholder="Select Month"
+          label={t("Financial Year Start Month")}
+          placeholder={t("Select Month")}
           value={[selectFinancialMonth]}
           defaultValue={formSubmissionOrg?.start_month ? [formSubmissionOrg?.start_month] : [selectFinancialMonth]}
           onChange={e => setSelectFinancialMonth(e?.[0])}
         />
       </div>
-      <When condition={formSubmissionOrg?.type?.includes("for-profit")}>
+      <When condition={model?.includes("profit")}>
         <div className="mb-10">
           <FinancialTableInput
             resetTable={resetTable}
-            label="Profit Analysis"
-            description="Revenue is defined as the total amount of money the business earns from selling its goods or services during their financial period, before any expenses are deducted.Expenses are defined as the sum of all the costs the business incurs to operate and generate revenue during their financial period, including taxes."
+            label={t("Profit Analysis")}
+            description={t(
+              "Revenue is defined as the total amount of money the business earns from selling its goods or services during their financial period, before any expenses are deducted.Expenses are defined as the sum of all the costs the business incurs to operate and generate revenue during their financial period, including taxes."
+            )}
             tableColumns={forProfitAnalysisColumns}
             value={forProfitAnalysisData ?? []}
           />
         </div>
       </When>
-      <When condition={formSubmissionOrg?.type?.includes("non-profit")}>
+      <When condition={model?.includes("budget")}>
         <div className="mb-10">
           <FinancialTableInput
             resetTable={resetTable}
-            label="Budget Analysis"
-            description="The budget represents the total amount of money allocated for the organization's operations and activities during the financial period. It includes all planned expenses for program services, administrative costs, and other operational needs."
+            label={t("Budget Analysis")}
+            description={t(
+              "The budget represents the total amount of money allocated for the organization's operations and activities during the financial period. It includes all planned expenses for program services, administrative costs, and other operational needs."
+            )}
             tableColumns={nonProfitAnalysisColumns}
             value={nonProfitAnalysisData ?? []}
+          />
+        </div>
+      </When>
+      <When condition={model?.includes("current-radio")}>
+        <div className="mb-10">
+          <FinancialTableInput
+            resetTable={resetTable}
+            label={t("Current Ratio")}
+            description={t(
+              "Current assets are defined as: Cash, accounts receivable, inventory, and other assets that are expected to be converted to cash within one year.Current liabilities are defined as: Accounts payable, short-term debt, and other obligations due within one year.Current ratio is defined as: Current assets divided by current liabilities. A ratio above 1.0 indicates the company can pay its short-term obligations."
+            )}
+            tableColumns={currentRadioColumns}
+            value={currentRadioData ?? []}
           />
         </div>
       </When>
       <div className="mb-10">
         <FinancialTableInput
           resetTable={resetTable}
-          label="Current Ratio"
-          description="Current assets are defined as: Cash, accounts receivable, inventory, and other assets that are expected to be converted to cash within one year.Current liabilities are defined as: Accounts payable, short-term debt, and other obligations due within one year.Current ratio is defined as: Current assets divided by current liabilities. A ratio above 1.0 indicates the company can pay its short-term obligations."
-          tableColumns={currentRadioColumns}
-          value={currentRadioData ?? []}
-        />
-      </div>
-      <div className="mb-10">
-        <FinancialTableInput
-          resetTable={resetTable}
-          label="Documentation"
-          description="Please provide supporting documentation for each year's financial data and add any relevant notes or context about your financial position."
+          label={t("Documentation")}
+          description={t(
+            "Please provide supporting documentation for each year's financial data and add any relevant notes or context about your financial position."
+          )}
           tableColumns={documentationColumns}
           value={documentationData ?? []}
         />
