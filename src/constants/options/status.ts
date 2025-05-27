210694340702cc80e83fb91c0e13c4de67659b73
Ismael Martinez <admin@scriptmatico.com>
2025-05-27 14:09:20 -0600

    [TM-2016] Update "Nothing to Report" option values booleans in status.ts

--- src/constants/options/status.ts
+++ src/constants/options/status.ts
@@ -93,11 +93,11 @@ export const getChangeRequestStatusOptions = (t: typeof useT | Function = (t: st
 export const getNothingReportOptions = (t: typeof useT | Function = (t: string) => t) =>
   [
     {
-      value: "1",
+      value: "true",
       title: t("Reported Info")
     },
     {
-      value: "-1",
+      value: "false",
       title: t("Nothing to Report")
     }
   ] as Option[];
