4e27ac4f517a0a1f6d12fc54580c508c7ffa905d
diego-morales-flores-1996 <diego@vizonomy.com>
2025-06-03 23:10:09 -0400

    update task UI and header

--- src/admin/modules/tasks/components/TasksList.tsx
+++ src/admin/modules/tasks/components/TasksList.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { FC } from "react";
 import {
   AutocompleteInput,
@@ -11,6 +11,13 @@ import {
   TextField
 } from "react-admin";
 
+import ListActions from "@/admin/components/Actions/ListActions";
+import ExportProcessingAlert from "@/admin/components/Alerts/ExportProcessingAlert";
+import FrameworkSelectionDialog, { useFrameworkExport } from "@/admin/components/Dialogs/FrameworkSelectionDialog";
+import Button from "@/components/elements/Button/Button";
+import { StatusEnum } from "@/components/elements/Status/constants/statusMap";
+import Status from "@/components/elements/Status/Status";
+import Text from "@/components/elements/Text/Text";
 import { useFrameworkChoices } from "@/constants/options/frameworks";
 import { getTaskStatusOptions } from "@/constants/options/status";
 import { useUserFrameworkChoices } from "@/constants/options/userFrameworksChoices";
@@ -61,16 +68,47 @@ export const TasksList: FC = () => {
     <SelectInput key="framework_key" label="Framework" source="framework_key" choices={frameworkChoices} />
   ];
 
+  const { exporting, onClickExportButton, frameworkDialogProps } = useFrameworkExport("tasks", frameworkChoices);
+
   return (
     <>
       <Stack gap={1} py={2}>
-        <Typography variant="h5">Tasks</Typography>
-        <Divider />
+        <Text variant="text-36-bold" className="leading-none">
+          Tasks
+        </Text>
       </Stack>
 
-      <List filters={filters}>
+      <List actions={<ListActions onExport={onClickExportButton} />} filters={filters}>
+        <div className="m-6 flex items-center justify-between gap-6 rounded-2xl border border-neutral-300 px-6 py-4">
+          <Text variant="text-20-bold" className="w-full leading-none">
+            GBM PPC Project
+          </Text>
+          <div className="grid shrink-0 grid-cols-3 items-center gap-x-6 gap-y-2">
+            <Text variant="text-12-light" className="leading-none">
+              Status
+            </Text>
+            <Text variant="text-12-light" className="leading-none">
+              Due Time
+            </Text>
+            <Text variant="text-12-light" className="whitespace-nowrap leading-none">
+              Trees Planted
+            </Text>
+            <Status status={StatusEnum.NEEDS_MORE_INFORMATION} variant="small" />
+            <Text variant="text-14-bold" className="leading-none">
+              06/08/2021
+            </Text>
+            <Text variant="text-14-bold" className="leading-none">
+              0
+            </Text>
+          </div>
+          <Button variant="primary">Bulk Approve &quot;Nothing to Report&quot;</Button>
+        </div>
         <TaskDataGrid />
       </List>
+
+      <FrameworkSelectionDialog {...frameworkDialogProps} />
+
+      <ExportProcessingAlert show={exporting} />
     </>
   );
 };
