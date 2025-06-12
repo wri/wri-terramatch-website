6f857c5b129cb692f6c5ef7d1946a119e3e2ff6e
Nathan Curtis <nathan.curtis@gmail.com>
2025-06-12 10:57:30 -0700

    [TM-1994] Wrap form summaries on application overview in the framework provider.

--- src/pages/applications/components/ApplicationOverview.tsx
+++ src/pages/applications/components/ApplicationOverview.tsx
@@ -3,7 +3,7 @@ import { useT } from "@transifex/react";
 import Tabs from "@/components/elements/Tabs/Default/Tabs";
 import Text from "@/components/elements/Text/Text";
 import FormSummary from "@/components/extensive/WizardForm/FormSummary";
-import { useFramework } from "@/context/framework.provider";
+import FrameworkProvider, { useFramework } from "@/context/framework.provider";
 import { FormSubmissionRead } from "@/generated/apiSchemas";
 import { normalizedFormDefaultValue } from "@/helpers/customForms";
 import { useGetCustomFormSteps } from "@/hooks/useGetCustomFormSteps/useGetCustomFormSteps";
@@ -45,9 +45,11 @@ const Item = ({ submission }: { submission?: FormSubmissionRead }) => {
   const formSteps = useGetCustomFormSteps(submission?.form!, currentPitchEntity, framework);
   const values = normalizedFormDefaultValue(submission?.answers, formSteps);
   return (
-    <div className="flex flex-col gap-6 bg-white p-8">
-      <FormSummary steps={formSteps!} values={values} entity={currentPitchEntity} />
-    </div>
+    <FrameworkProvider frameworkKey={framework}>
+      <div className="flex flex-col gap-6 bg-white p-8">
+        <FormSummary steps={formSteps!} values={values} entity={currentPitchEntity} />
+      </div>
+    </FrameworkProvider>
   );
 };
 
