b10fccd14fd54e4cf0cec3a169c4a7af3bd9f90a
LimberHope <limbermv2@gmail.com>
2025-07-08 11:15:07 -0400

    [TM-2261] update action tracker card

--- src/components/extensive/ActionTracker/ActionTrackerCardRow.tsx
+++ src/components/extensive/ActionTracker/ActionTrackerCardRow.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 
 import { StatusEnum } from "@/components/elements/Status/constants/statusMap";
 import Status from "@/components/elements/Status/Status";
+import StatusPill from "@/components/elements/StatusPill/StatusPill";
 import Text from "@/components/elements/Text/Text";
+import { Status as PillStatuses } from "@/types/common";
 
 import Icon, { IconNames } from "../Icon/Icon";
 
@@ -20,6 +22,8 @@ export interface ActionTrackerCardRowProps {
   onClick?: () => {};
 }
 
+const statusPillStatuses = ["edit", "error", "success", "awaiting", "warning", "restoration"];
+
 const ActionTrackerCardRow = ({
   title,
   subtitle,
@@ -35,42 +39,46 @@ const ActionTrackerCardRow = ({
   <Link
     href={ctaLink!}
     title={title}
-    className={classnames("group flex cursor-pointer flex-col  rounded-xl p-0 shadow", className)}
+    className={classnames("group flex cursor-pointer flex-col  rounded-md p-0 shadow", className)}
     onClick={onClick}
   >
-    <div className="mx-4 my-4 wide:mx-6 wide:mt-6 wide:mb-5">
-      <div className="flex items-start gap-3">
-        <Text variant="text-14-bold" className="flex-1">
+    <div className="mx-3 mb-2 mt-3 wide:mx-6 wide:mt-6 wide:mb-5">
+      <div className="mb-1 flex items-start gap-3">
+        <Text variant="text-body-500" className="flex-1">
           {title}
         </Text>
-        <Status
-          status={status}
-          variant="small"
-          className={classnames("w-fit", {
-            "bg-darkCustom-100/10": status === "draft"
-          })}
-          classNameText={classnames({
-            "text-grey-500": status === "draft"
-          })}
-        />
+        {statusPillStatuses.includes(status) ? (
+          <StatusPill status={status as PillStatuses}>
+            <Text variant="text-bold-caption-100">{statusText}</Text>
+          </StatusPill>
+        ) : (
+          <Status
+            status={status}
+            variant="small"
+            className={classnames("w-fit", {
+              "bg-darkCustom-100/10": status === "draft"
+            })}
+            classNameText={classnames({
+              "text-grey-500": status === "draft"
+            })}
+          />
+        )}
       </div>
-      <Text variant="text-14-light" containHtml className="mb-2">
+      <Text variant="text-body-300" containHtml>
         {subtitle}
       </Text>
-      <Text variant="text-14-light">
-        <Text variant="text-12-bold" as="span">
-          Due{" "}
-        </Text>
-        <Text variant="text-12-light" as="span">
-          {updatedAt}
-        </Text>
+      <Text variant="text-body-300" containHtml>
+        {updatedAt}
+      </Text>
+      <Text variant="text-body-300" containHtml>
+        {updatedBy}
       </Text>
     </div>
     <div className="flex w-full items-center justify-between rounded-b-md bg-primary-200 p-3 group-hover:bg-primary-400 group-hover:text-white wide:p-6">
-      <Text variant="text-14-bold" className="text-primary group-hover:text-white">
+      <Text variant="text-button-700" className="group-hover:text-white">
         {ctaText}
       </Text>
-      <Icon name={IconNames.CHEVRON_RIGHT_SMALL} className="h-6 w-6 text-primary group-hover:text-white" />
+      <Icon name={IconNames.CHEVRON_RIGHT_SMALL} className="h-5 w-5" />
     </div>
   </Link>
 );
