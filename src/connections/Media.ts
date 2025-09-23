855af431fbfefc6c3fb9385a98e68588dd9b05af
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-09-23 10:07:40 -0400

    [TM-1775] add working connection to media

--- src/connections/Media.ts
+++ src/connections/Media.ts
@@ -4,7 +4,30 @@ import { MediaDto } from "@/generated/v3/entityService/entityServiceSchemas";
 
 import { v3Resource } from "./util/apiConnectionFactory";
 
-export const uploadFileConnection = v3Resource("media", uploadFile).create<MediaDto>().buildConnection();
+export type UploadFileProps = {
+  entity:
+    | "projects"
+    | "sites"
+    | "nurseries"
+    | "projectReports"
+    | "siteReports"
+    | "nurseryReports"
+    | "organisations"
+    | "auditStatuses"
+    | "forms"
+    | "formQuestionOptions"
+    | "fundingProgrammes"
+    | "impactStories"
+    | "financialIndicators";
+  collection: string;
+  uuid: string;
+};
+
+export const uploadFileConnection = v3Resource("media", uploadFile)
+  .create<MediaDto, UploadFileProps>(({ entity, collection, uuid }: UploadFileProps) => ({
+    pathParams: { entity, collection, uuid }
+  }))
+  .buildConnection();
 
 export const useUploadFile = connectionHook(uploadFileConnection);
 export const selectUploadFile = connectionSelector(uploadFileConnection);
