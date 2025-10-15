0c34131af5070f336dc357bb2040ea7a8653ddd6
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-10-15 04:53:48 -0400

    [TM-2502] update definition

--- src/connections/Media.ts
+++ src/connections/Media.ts
@@ -1,12 +1,6 @@
-import { mediaDelete, mediaGet } from "@/generated/v3/entityService/entityServiceComponents";
-import { MediaDto } from "@/generated/v3/entityService/entityServiceSchemas";
+import { mediaDelete } from "@/generated/v3/entityService/entityServiceComponents";
 
-import { v3Resource } from "./util/apiConnectionFactory";
 import { deleterAsync } from "./util/resourceDeleter";
-
-export const mediaConnection = v3Resource("media", mediaGet)
-  .singleResource<MediaDto>(({ id }) => ({ pathParams: { uuid: id! } }))
-  .buildConnection();
 
 const createEntityDeleter = () => deleterAsync("media", mediaDelete, uuid => ({ pathParams: { uuid: uuid } }));
 
