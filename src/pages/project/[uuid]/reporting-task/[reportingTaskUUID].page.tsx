1983b0b49af0a91c257cdadfbc7ae7fd384edeea
Dotnara Condori <dottykurmi123@gmail.com>
2025-10-17 15:40:51 -0400

    [TM-2527] Add Socio-Economic Report pages and components PD (#1537)
    * [TM-2527]  Add Socio-Economic Report pages and components
    
    * [TM-2527] Add Socio-Economic Report pages and components

--- src/pages/project/[uuid]/reporting-task/[reportingTaskUUID].page.tsx
+++ src/pages/project/[uuid]/reporting-task/[reportingTaskUUID].page.tsx
@@ -17,6 +17,7 @@ import Modal from "@/components/extensive/Modal/Modal";
 import { ModalId } from "@/components/extensive/Modal/ModalConst";
 import PageBody from "@/components/extensive/PageElements/Body/PageBody";
 import PageCard from "@/components/extensive/PageElements/Card/PageCard";
+import PageFooter from "@/components/extensive/PageElements/Footer/PageFooter";
 import PageSection from "@/components/extensive/PageElements/Section/PageSection";
 import { CompletionStatusMapping } from "@/components/extensive/Tables/ReportingTasksTable";
 import WelcomeTour from "@/components/extensive/WelcomeTour/WelcomeTour";
@@ -277,6 +278,90 @@ const ReportingTaskPage = () => {
     }
   ];
 
+  const tableColumnsSRP: ColumnDef<RowData>[] = [
+    {
+      accessorKey: "parentName",
+      header: t("Report")
+    },
+    {
+      accessorKey: "completionStatus",
+      header: t("Status"),
+      cell: props => {
+        const value = props.getValue() as string;
+        const { status, statusText } = CompletionStatusMapping(t)?.[value] || {};
+        if (!status) return null;
+
+        return (
+          <StatusPill status={status} className="w-fit">
+            <Text variant="text-bold-caption-100">{statusText}</Text>
+          </StatusPill>
+        );
+      }
+    },
+    {
+      accessorKey: "completion",
+      header: t("Completion"),
+      cell: props => {
+        return `${props.getValue()}%`;
+      }
+    },
+    {
+      accessorKey: "updatedAt",
+      header: t("Last Update")
+    },
+    {
+      accessorKey: "completionStatus",
+      id: "uuid",
+      header: "",
+      enableSorting: false,
+      cell: props => {
+        const record = props.row.original as TaskReport;
+        const { index } = props.row;
+        const { status, completion, uuid, completionStatus } = record;
+        const type = "socio-economic-report";
+        const shouldShowButton =
+          NOTHING_TO_REPORT_DISPLAYABLE_STATUSES.includes(status) &&
+          !(type === "socio-economic-report" || completion === 100);
+
+        const handleClick = useCallback(() => {
+          nothingToReportHandler("socio-economic-reports" as ReportsModelNames, uuid);
+        }, [uuid]);
+
+        return (
+          <div className="flex justify-end gap-4">
+            {shouldShowButton ? (
+              <Button id={`nothing-to-report-button-${index}`} variant="secondary" onClick={handleClick}>
+                {t("Nothing to report")}
+              </Button>
+            ) : null}
+            <Switch>
+              <Case condition={completionStatus === "not-started" || completionStatus === "nothing-to-report"}>
+                <Button as={Link} href={`/entity/${type}s/create/framework?entity_uuid=${uuid}`}>
+                  {t("Write report")}
+                </Button>
+              </Case>
+              <Case condition={["approved", "awaiting-approval"].includes(completionStatus)}>
+                <Button as={Link} href={`/reports/${type}/${uuid}`}>
+                  {t("View Completed Report")}
+                </Button>
+              </Case>
+              <Case condition={completionStatus === "needs-more-information"}>
+                <Button as={Link} href={`/reports/${type}/${uuid}`}>
+                  {t("View Feedback")}
+                </Button>
+              </Case>
+              <Default>
+                <Button as={Link} href={`/entity/${type}s/edit/${uuid}`}>
+                  {t("Continue report")}
+                </Button>
+              </Default>
+            </Switch>
+          </div>
+        );
+      }
+    }
+  ];
+
   return (
     projectLoaded && (
       <FrameworkProvider frameworkKey={project?.frameworkKey}>
@@ -287,6 +372,11 @@ const ReportingTaskPage = () => {
             <PageSection>
               <PageCard title={t("Mandatory Project Report")}>
                 <Table data={reports.mandatory} hasPagination={false} columns={tableColumns} />
+              </PageCard>
+            </PageSection>
+            <PageSection>
+              <PageCard title={t("SRP Reports")}>
+                <Table data={reports.mandatory} hasPagination={false} columns={tableColumnsSRP} />
               </PageCard>
             </PageSection>
             <PageSection>
@@ -336,6 +426,11 @@ const ReportingTaskPage = () => {
               onStart={() => setTourEnabled(true)}
               onFinish={() => setTourEnabled(false)}
             />
+            <br />
+            <br />
+            <br />
+
+            <PageFooter />
           </PageBody>
         </LoadingContainer>
       </FrameworkProvider>
