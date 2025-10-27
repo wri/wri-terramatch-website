9e7baa49bcf4e429dfd563cd317cd3f4d8e0f558
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2025-10-27 12:04:20 -0400

    [TM-2516] hide add report button (#1566)

--- src/pages/project/[uuid]/tabs/DisturbanceReports.tsx
+++ src/pages/project/[uuid]/tabs/DisturbanceReports.tsx
@@ -34,18 +34,19 @@ const DisturbanceReportsTab = ({ projectUUID }: DisturbanceReportsProps) => {
       <PageRow>
         <PageColumn>
           <PageCard
-            classNameSubTitle="block w-[80%]"
+            // classNameSubTitle="block w-[80%]"
             title={t("Disturbance Reports")}
             subtitle={t(
               "Disturbance reports allow champions to flag a major obstacle your project is facing outside of the 6 month progress reports. When you submit a disturbance report, your project manager will be notified and will work with you to handle the disturbance within your project scope and budget."
             )}
             subtitleMore
-            headerChildren={
-              <Button disabled={isCreating} onClick={() => create({ parentUuid: projectUUID })}>
-                <Icon name={IconNames.PLUS} className="h-3 w-3" />
-                <>{t("Add Report")}</>
-              </Button>
-            }
+            // TODO: hide add report button for now
+            // headerChildren={
+            //   <Button disabled={isCreating} onClick={() => create({ parentUuid: projectUUID })}>
+            //     <Icon name={IconNames.PLUS} className="h-3 w-3" />
+            //     <>{t("Add Report")}</>
+            //   </Button>
+            // }
           >
             <DisturbanceReportsTable projectUUID={projectUUID} alwaysShowPagination />
           </PageCard>
