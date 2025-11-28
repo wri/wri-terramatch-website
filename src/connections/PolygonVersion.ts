41a887d4e4f48e3f716a3b739b0f5497b252cbc5
JORGE <egrojmonroy@gmail.com>
2025-11-28 10:44:03 -0400

    [TM-2099] change update polygon version

--- src/connections/PolygonVersion.ts
+++ src/connections/PolygonVersion.ts
@@ -1,12 +1,14 @@
-import { v3Resource } from "@/connections/util/apiConnectionFactory";
+import { IdProp, v3Resource } from "@/connections/util/apiConnectionFactory";
 import { connectionHook, connectionLoader } from "@/connections/util/connectionShortcuts";
 import { deleterAsync } from "@/connections/util/resourceDeleter";
+import { resourceUpdater } from "@/connections/util/resourceMutator";
 import {
   deleteSitePolygonVersion,
   listSitePolygonVersions,
-  updateSitePolygonVersion
+  updateSitePolygonVersion,
+  UpdateSitePolygonVersionVariables
 } from "@/generated/v3/researchService/researchServiceComponents";
-import { SitePolygonLightDto } from "@/generated/v3/researchService/researchServiceSchemas";
+import { SitePolygonLightDto, VersionUpdateAttributes } from "@/generated/v3/researchService/researchServiceSchemas";
 import ApiSlice from "@/store/apiSlice";
 
 const listPolygonVersionsConnection = v3Resource("sitePolygons", listSitePolygonVersions)
@@ -23,22 +25,18 @@ const listPolygonVersionsConnection = v3Resource("sitePolygons", listSitePolygon
 export const useListPolygonVersions = connectionHook(listPolygonVersionsConnection);
 export const loadListPolygonVersions = connectionLoader(listPolygonVersionsConnection);
 
-export const updatePolygonVersionAsync = async (uuid: string, attributes: { isActive: boolean; comment?: string }) => {
-  const response = await updateSitePolygonVersion.fetch({
-    pathParams: { uuid },
-    body: {
-      data: {
-        type: "sitePolygons",
-        id: uuid,
-        attributes
-      }
-    }
-  });
+const polygonVersionUpdateConnection = v3Resource("sitePolygons", listSitePolygonVersions)
+  .singleResource<SitePolygonLightDto>(({ id }: IdProp) => (id == null ? undefined : { pathParams: { uuid: id } }))
+  .update<VersionUpdateAttributes, UpdateSitePolygonVersionVariables>(updateSitePolygonVersion)
+  .buildConnection();
 
+const updatePolygonVersionUpdater = resourceUpdater(polygonVersionUpdateConnection);
+
+export const updatePolygonVersionAsync = async (uuid: string, attributes: { isActive: boolean; comment?: string }) => {
+  const result = await updatePolygonVersionUpdater(attributes, { id: uuid });
   ApiSlice.pruneCache("sitePolygons", [uuid]);
   ApiSlice.pruneIndex("sitePolygons", "");
-
-  return response;
+  return result;
 };
 
 export const deletePolygonVersion = deleterAsync("sitePolygons", deleteSitePolygonVersion, (uuid: string) => ({
