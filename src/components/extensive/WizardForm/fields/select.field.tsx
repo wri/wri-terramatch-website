76ae1acc3367aaf9115e37a29e15fa09ef28d11e
Marcolr-dev <marc.antoni.lr@gmail.com>
2025-12-08 12:20:09 -0400

    [TM-2673] Enhance SelectField component with multiChoice styling options

--- src/components/extensive/WizardForm/fields/select.field.tsx
+++ src/components/extensive/WizardForm/fields/select.field.tsx
@@ -19,6 +19,9 @@ export const SelectField: FormFieldFactory = {
       optionsList={optionsList ?? undefined}
       linkedFieldKey={linkedFieldKey ?? undefined}
       enableAdditionalOptions
+      className={multiChoice ? "!h-auto min-h-[40px]" : undefined}
+      titleContainerClassName={multiChoice ? "flex items-start gap-2 min-w-0" : undefined}
+      titleClassname={multiChoice ? "!whitespace-normal !break-words" : undefined}
     />
   ),
 
