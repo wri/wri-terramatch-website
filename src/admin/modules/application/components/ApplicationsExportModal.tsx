f427fbaf03efa968f12e8913540c79eb86830f67
Nathan Curtis <nathan.curtis@gmail.com>
2025-12-16 14:24:16 -0800

    [TM-2655] Remove the need for a stage index by changing application export a bit.

--- src/admin/modules/application/components/ApplicationsExportModal.tsx
+++ src/admin/modules/application/components/ApplicationsExportModal.tsx
@@ -8,18 +8,18 @@ import {
   DialogProps,
   DialogTitle
 } from "@mui/material";
-import { useEffect, useMemo } from "react";
-import { AutocompleteInput, Form, SelectInput } from "react-admin";
+import { FC, useCallback, useMemo } from "react";
+import { AutocompleteInput, Form } from "react-admin";
 import { useFormContext, useWatch } from "react-hook-form";
-import { Case, Else, If, Switch, Then } from "react-if";
 import * as yup from "yup";
 
 import { useExportContext } from "@/admin/modules/application/context/export.provider";
+import { Choice } from "@/admin/types/common";
 import { validateForm } from "@/admin/utils/forms";
 import { useFundingProgrammes } from "@/connections/FundingProgramme";
-import { useGetV2FundingProgrammeStage } from "@/generated/apiComponents";
-import { StageRead } from "@/generated/apiSchemas";
+import { useValueChanged } from "@/hooks/useValueChanged";
 import { Option } from "@/types/common";
+import Log from "@/utils/log";
 import { optionToChoices } from "@/utils/options";
 
 interface ApplicationsExportModalProps extends DialogProps {
@@ -27,23 +27,15 @@ interface ApplicationsExportModalProps extends DialogProps {
 }
 
 const validationSchema = yup.object({
-  funding_programme_uuid: yup.string(),
-  stage_uuid: yup.string(),
-  export_choice: yup.string().required()
+  fundingProgrammeUuid: yup.string(),
+  stageUuid: yup.string()
 });
 
-const FormContent = ({ handleClose }: { handleClose: ApplicationsExportModalProps["handleClose"] }) => {
+const FormContent: FC<{ handleClose: ApplicationsExportModalProps["handleClose"] }> = ({ handleClose }) => {
   const { control, setValue } = useFormContext();
-  const choice = useWatch({ control, name: "export_choice" });
+  const fundingProgrammeUuid = useWatch({ control, name: "fundingProgrammeUuid" });
 
   const [fpsLoaded, { data: fundingProgrammes }] = useFundingProgrammes({ translated: false });
-  const { data: stageData, isLoading: stagesLoading } = useGetV2FundingProgrammeStage({
-    queryParams: {
-      per_page: 100,
-      // @ts-ignore
-      light: true
-    }
-  });
 
   const fpChoices = useMemo(() => {
     const data = fundingProgrammes?.map(({ name, uuid }) => ({ title: name, value: uuid })) as Option[];
@@ -52,83 +44,60 @@ const FormContent = ({ handleClose }: { handleClose: ApplicationsExportModalProp
   }, [fundingProgrammes]);
 
   const stageChoices = useMemo(() => {
-    // @ts-ignore incorrect docs
-    const data = stageData?.data
-      // @ts-ignore incorrect docs
-      ?.filter(item => !!item.form)
-      // @ts-ignore incorrect docs
-      .map(f => ({
-        title: f.name ?? "",
-        value: f.uuid ?? ""
-      })) as Option[];
+    const fundingProgramme =
+      fundingProgrammeUuid == null ? undefined : fundingProgrammes?.find(({ uuid }) => uuid === fundingProgrammeUuid);
+    if (fundingProgramme == null) return [];
 
-    return optionToChoices(data ?? []);
-  }, [stageData?.data]);
+    const defaultChoice: Choice = { name: "All Stages in Funding Programme", id: "all" };
+    return [
+      defaultChoice,
+      ...(fundingProgramme.stages ?? []).map(({ name, uuid }): Choice => ({ name: name ?? "<no name>", id: uuid }))
+    ];
+  }, [fundingProgrammeUuid, fundingProgrammes]);
 
-  /**
-   * Ensure that fields have a default value when type changes
-   */
-  useEffect(() => {
-    setValue("funding_programme_uuid", fpChoices[0]?.id);
-    setValue("stage_uuid", stageChoices[0]?.id);
-  }, [fpChoices, setValue, stageChoices, choice]);
+  useValueChanged(fpsLoaded, () => {
+    if (fpsLoaded) setValue("fundingProgrammeUuid", fpChoices[0]?.id);
+  });
+  useValueChanged(fundingProgrammeUuid, () => {
+    setValue("stageUuid", stageChoices[0]?.id);
+  });
 
   return (
     <>
       <DialogTitle>Please select the funding framework or stage you&apos;d like to export.</DialogTitle>
       <DialogContent>
-        <If condition={!fpsLoaded || stagesLoading}>
-          <Then>
-            <CircularProgress size={22} />
-          </Then>
-          <Else>
-            <DialogContentText>Export type.</DialogContentText>
-            <SelectInput
-              source="export_choice"
-              label="Choose an export type"
-              choices={[
-                { id: "funding-programme", name: "Funding Programme" },
-                { id: "stage", name: "Stage" }
-              ]}
+        {!fpsLoaded ? (
+          <CircularProgress size={22} />
+        ) : (
+          <>
+            <DialogContentText>Select Funding Programme and Stage (or all stages)</DialogContentText>
+            <AutocompleteInput
+              loading={!fpsLoaded}
+              source="fundingProgrammeUuid"
+              label="Funding Programme"
+              choices={fpChoices}
               fullWidth
               margin="dense"
+              defaultValue={fpChoices[0]?.id}
+              disableClearable
             />
-            <Switch>
-              <Case condition={choice === "funding-programme"}>
-                <DialogContentText>You can export one framework at a time.</DialogContentText>
-
-                <AutocompleteInput
-                  loading={!fpsLoaded}
-                  source="funding_programme_uuid"
-                  label="Funding Programme"
-                  choices={fpChoices}
-                  fullWidth
-                  margin="dense"
-                  defaultValue={fpChoices[0]?.id}
-                  disableClearable
-                />
-              </Case>
-              <Case condition={choice === "stage"}>
-                <DialogContentText>You can export one stage at a time.</DialogContentText>
-                <AutocompleteInput
-                  loading={stagesLoading}
-                  source="stage_uuid"
-                  label="Stage"
-                  choices={stageChoices}
-                  fullWidth
-                  margin="dense"
-                  defaultValue={stageChoices[0]?.id}
-                  disableClearable
-                />
-              </Case>
-            </Switch>
-          </Else>
-        </If>
+            <AutocompleteInput
+              loading={!fpsLoaded}
+              source="stageUuid"
+              label="Stage"
+              choices={stageChoices}
+              fullWidth
+              margin="dense"
+              defaultValue={stageChoices[0]?.id}
+              disableClearable
+            />
+          </>
+        )}
       </DialogContent>
 
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
-        <Button variant="contained" type="submit" disabled={!fpsLoaded || stagesLoading}>
+        <Button variant="contained" type="submit" disabled={!fpsLoaded}>
           Confirm
         </Button>
       </DialogActions>
@@ -136,30 +105,33 @@ const FormContent = ({ handleClose }: { handleClose: ApplicationsExportModalProp
   );
 };
 
-const ApplicationsExportModal = ({ handleClose, ...dialogProps }: ApplicationsExportModalProps) => {
+const ApplicationsExportModal: FC<ApplicationsExportModalProps> = ({ handleClose, ...dialogProps }) => {
   const { exportApplications } = useExportContext();
 
-  const { data: stageData } = useGetV2FundingProgrammeStage({
-    queryParams: {
-      per_page: 100,
-      // @ts-ignore
-      light: true
-    }
-  });
+  const [, { data: fundingProgrammes }] = useFundingProgrammes({ translated: false });
 
-  const handleSave = async (data: any) => {
-    let form_uuid: string = "";
+  const handleSave = useCallback(
+    ({ fundingProgrammeUuid, stageUuid }: { fundingProgrammeUuid?: string; stageUuid?: string }) => {
+      if (fundingProgrammeUuid == null) {
+        Log.error("Funding programme uuid is required");
+      } else {
+        if (stageUuid == null || stageUuid === "all") {
+          exportApplications({ fundingProgrammeUuid }, "Applications.csv");
+        } else {
+          const programme = fundingProgrammes?.find(({ uuid }) => uuid === fundingProgrammeUuid);
+          const stage = programme?.stages?.find(({ uuid }) => uuid === stageUuid);
+          if (stage?.formUuid == null) {
+            Log.error("Unable to find form for stage", { fundingProgrammeUuid, stageUuid });
+          } else {
+            exportApplications({ formUuid: stage.formUuid }, "Applications.csv");
+          }
+        }
+      }
 
-    if (data.stage_uuid) {
-      // Get the form
-      // @ts-ignore incorrect docs
-      const stage: StageRead = stageData?.data?.find((item: StageRead) => item.uuid === data.stage_uuid);
-      // @ts-ignore incorrect docs
-      form_uuid = stage?.form?.uuid;
-    }
-    exportApplications({ ...data, form_uuid }, "Applications.csv");
-    handleClose();
-  };
+      handleClose();
+    },
+    [exportApplications, fundingProgrammes, handleClose]
+  );
 
   return (
     <Dialog {...dialogProps} fullWidth>
