a4f15a5295a7c345a316ba6dca24feed923bf724
diego-morales-flores-1996 <139272044+diego-morales-flores-1996@users.noreply.github.com>
2026-01-12 16:24:44 -0400

    [TM-2836] TM design components icons (#1714)
    * [TM-2828] install wri-design-systems
    
    * [TM-2828] [TM-2840] updating storybook and babel for the wri-design-systems compatibility
    
    * [TM-2840] move large url into constants
    
    * [TM-2840] move large url into constants
    
    * [TM-2836] TM-2836 add icons stories
    
    * [TM-2836] TM-2836 add icons stories
    
    * [TM-2836] TM-2836 fix build
    
    * [TM-2836] TM-2836 fix organizations folders

---
+++ src/redesignComponents/foundations/Icons/ChevronUp.tsx
@@ -0,0 +1,10 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+import React from "react";
+
+export const ChevronUp = (props: IconProps) => (
+  <Icon {...props}>
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 7.23333L1.86667 13.3667L0 11.5L8 3.5L16 11.5L14.1333 13.3667L8 7.23333Z" fill="currentColor" />
+    </svg>
+  </Icon>
+);
