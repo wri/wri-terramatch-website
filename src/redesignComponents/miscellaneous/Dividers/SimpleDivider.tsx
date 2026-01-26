5d9adbae97359a28ac60ac4d5b90ac584aa90dc0
diego-morales-flores-1996 <diego@vizonomy.com>
2026-01-26 16:34:51 -0400

    [TM-2915] fix commentaries on PR

--- src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
+++ src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
@@ -1,7 +1,7 @@
 import { Box, BoxProps } from "@chakra-ui/react";
-import React from "react";
+import React, { FC } from "react";
 
-const SimpleDivider = (props: BoxProps = {}) => {
+const SimpleDivider: FC<BoxProps> = props => {
   const { backgroundColor = "neutral.300", width = "100%", height = "1px", ...rest } = props;
   return <Box width={width} height={height} backgroundColor={backgroundColor} {...rest} />;
 };
