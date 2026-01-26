2f6d3e15cacd71cc57e9ce14e82515aa433bfc17
Dotnara Condori <dottykurmi123@gmail.com>
2026-01-26 16:27:24 -0400

    [TM-2835] tm design components button (#1764)
    * [TM-2828] install wri-design-systems
    
    * [TM-2828] [TM-2840] updating storybook and babel for the wri-design-systems compatibility
    
    * [TM-2840] move large url into constants
    
    * [TM-2840] move large url into constants
    
    * [TM-2835] Implement new button and menu components with stories and styles. Integrate Chakra UI theme colors into Tailwind CSS. Add MultiActionButton, IconButton, and CloseButton with Storybook documentation.
    
    * [TM-2835] Change console log
    
    * [TM-2835] Update story titles in Button, CloseButton, IconButton, Menu, and MultiActionButton stories to reflect the new naming convention "Redesign Components".
    
    * [TM-2835] Refactor Button and IconButton components to use new Edit and MoreVert icons, update styles for focus outlines, and enhance storybook examples. Remove deprecated icon usage and streamline button rendering.
    
    * [TM-2835] Remove TypeScript suppression comments from Button component files to improve type safety.
    
    ---------
    
    Co-authored-by: diego-morales-flores-1996 <diego@vizonomy.com>

--- src/redesignComponents/Forms/Actions/IconButton/IconButton.tsx
+++ src/redesignComponents/Forms/Actions/IconButton/IconButton.tsx
@@ -1,5 +1,8 @@
 import { IconButton as WriIconButton } from "@worldresources/wri-design-systems";
+import classNames from "classnames";
 import { FC } from "react";
+
+import { focusOutlineClass } from "../Button/Button.styles";
 
 interface IconButtonProps {
   icon: React.ReactNode;
@@ -7,7 +10,23 @@ interface IconButtonProps {
 }
 
 const IconButton: FC<IconButtonProps> = ({ icon, disabled, ...props }) => {
-  return <WriIconButton icon={icon} disabled={disabled} {...props} />;
+  return (
+    <WriIconButton
+      icon={icon}
+      disabled={disabled}
+      {...props}
+      className={classNames(
+        focusOutlineClass,
+        "hover:bg-theme-primary-500/20 active:bg-theme-primary-500/40 !h-6 !w-6 rounded-sm"
+      )}
+      css={{
+        width: "24px !important",
+        height: "24px !important",
+        minWidth: "24px !important",
+        maxWidth: "24px !important"
+      }}
+    />
+  );
 };
 
 export default IconButton;
