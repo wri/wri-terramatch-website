4c4312e3aa32f53a28ada24791327f2aa69cef3b
diego-morales-flores-1996 <diego@vizonomy.com>
2026-01-26 16:29:46 -0400

    [TM-2834] fix commentaries on PR

--- src/redesignComponents/containers/Accordion/AccordionHeader.tsx
+++ src/redesignComponents/containers/Accordion/AccordionHeader.tsx
@@ -6,11 +6,14 @@ import TextBadge from "@/redesignComponents/status/Badge/TextBadge";
 
 import type { AccordionHeaderProps } from "./types";
 
+const getStatusIcon = (status: "success" | "error") =>
+  status === "success" ? (
+    <CheckApproved boxSize={4} color="success.500" />
+  ) : (
+    <InformationRequired boxSize={4} color="error.500" />
+  );
+
 const AccordionHeader: FC<AccordionHeaderProps> = ({ label, title, badge, status }) => {
-  const getStatusIcon = (status: "success" | "error") => {
-    if (status === "success") return <CheckApproved boxSize={4} color="success.500" />;
-    if (status === "error") return <InformationRequired boxSize={4} color="error.500" />;
-  };
   return (
     <div className="flex w-full items-center justify-between">
       <div className="flex items-center gap-3">
