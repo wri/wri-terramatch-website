a7ae982cb17b50441d639ad4b6d9fd11183364a3
Dotty <dottykurmi123@gmail.com>
2026-01-27 08:39:09 -0400

    [TM-2835] Refactor Button and CloseButton components to remove unused focusOutlineClass and clean up styles. Update IconButton to streamline class names.

--- src/redesignComponents/Forms/Actions/CloseButton/CloseButton.tsx
+++ src/redesignComponents/Forms/Actions/CloseButton/CloseButton.tsx
@@ -1,14 +1,12 @@
 import { CloseButton as WriCloseButton } from "@worldresources/wri-design-systems";
 import { FC } from "react";
 
-import { focusOutlineClass } from "../Button/Button.styles";
-
 interface ICloseButtonProps {
   disabled?: boolean;
 }
 
 const CloseButton: FC<ICloseButtonProps> = ({ disabled, ...props }) => {
-  return <WriCloseButton disabled={disabled} {...props} className={focusOutlineClass} />;
+  return <WriCloseButton disabled={disabled} {...props} />;
 };
 
 export default CloseButton;
