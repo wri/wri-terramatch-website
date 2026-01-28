9561ae7f2119d8698f3566fbe781bf00bfc8edf9
diego-morales-flores-1996 <diego@vizonomy.com>
2026-01-28 16:47:59 -0400

    [TM-2897] Add Project Map and Project set up boxes

--- src/pages/project/[uuid]/tabs/Overview.tsx
+++ src/pages/project/[uuid]/tabs/Overview.tsx
@@ -1,26 +1,33 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { ButtonProps } from "@chakra-ui/react";
+import { Box, Flex, FlexProps, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 import PageBody from "@/components/extensive/PageElements/Body/PageBody";
 import { ProjectFullDto } from "@/generated/v3/entityService/entityServiceSchemas";
+import { IButtonProps } from "@/redesignComponents/actions/Buttons/Button/Button";
 import Button from "@/redesignComponents/actions/Buttons/Button/Button";
-
+import { ChevronRight, Edit } from "@/redesignComponents/foundations/Icons";
+import { ProgressSteps } from "@/redesignComponents/status/ProgressIndicator/ProgressSteps";
+import { StepProps } from "@/redesignComponents/status/ProgressIndicator/types";
 interface ProjectOverviewTabProps {
   project: ProjectFullDto;
 }
 
 interface OverviewItemProps {
   title: string;
-  buttonProps?: ButtonProps;
+  buttonProps?: IButtonProps;
   children?: ReactNode;
+  flexProps?: FlexProps;
 }
 
-const OverviewItem = ({ title, buttonProps, children }: OverviewItemProps) => {
+const OverviewItem = (props: OverviewItemProps) => {
+  const { title, buttonProps, children, flexProps } = props;
+
   return (
-    <Flex direction="column" gap={4} flex={1}>
+    <Flex direction="column" gap={4} flex={1} {...flexProps}>
       <Flex alignItems="center" justifyContent="space-between">
-        <Text>{title}</Text>
+        <Text color="primary.900" fontSize="20px" lineHeight="28px">
+          {title}
+        </Text>
         {buttonProps ? <Button {...buttonProps} /> : null}
       </Flex>
       {children}
@@ -28,13 +35,108 @@ const OverviewItem = ({ title, buttonProps, children }: OverviewItemProps) => {
   );
 };
 
+const exampleSteps: StepProps[] = [
+  {
+    index: 1,
+    status: "completed",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 2,
+    status: "completed",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 3,
+    status: "completed",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 4,
+    status: "error",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 5,
+    status: "active",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 6,
+    status: "disabled",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  },
+  {
+    index: 7,
+    status: "disabled",
+    label: "Label",
+    actions: (
+      <Button variant="borderless" size="small" leftIcon={<Edit boxSize={3} />}>
+        Edit
+      </Button>
+    )
+  }
+];
+
 const ProjectOverviewTab = ({ project }: ProjectOverviewTabProps) => {
   return (
     <PageBody>
       <Flex direction="column" gap={5}>
         <Flex gap={7}>
-          <OverviewItem title="Map" />
-          <OverviewItem title="Project Set Up" />
+          <OverviewItem
+            title="Map"
+            flexProps={{ flex: 3 }}
+            buttonProps={{
+              variant: "secondary",
+              size: "small",
+              children: "View Sites",
+              rightIcon: <ChevronRight />
+            }}
+          >
+            <Box>MAP</Box>
+          </OverviewItem>
+          <OverviewItem
+            flexProps={{ flex: 1 }}
+            title="Project Set Up"
+            buttonProps={{
+              variant: "primary",
+              size: "small",
+              children: "Continue Editing",
+              rightIcon: <ChevronRight />
+            }}
+          >
+            <ProgressSteps steps={exampleSteps} />
+          </OverviewItem>
         </Flex>
         <OverviewItem title="Key Indicators & Insights">
           <Flex gap={2} flex={1}>
