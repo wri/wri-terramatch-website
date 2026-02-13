1a4d6c245aba580d2c96720604bb69880dbdcd46
Nathan Curtis <nathan.curtis@gmail.com>
2026-02-13 09:07:23 -0800

    [TM-2935] A couple of minor tweaks for restoration data entry.

--- src/components/extensive/TrackingCollapseGrid/types.ts
+++ src/components/extensive/TrackingCollapseGrid/types.ts
@@ -435,7 +435,7 @@ const GOAL_YEARS: Dictionary<string> = {
 const GOAL_STRATEGY: Dictionary<string> = {
   anr: "Assisted Natural Regeneration",
   "direct-seeding": "Direct Seeding",
-  planting: "Planting"
+  "tree-planting": "Tree Planting"
 };
 
 const LAND_USE: Dictionary<string> = {
@@ -462,12 +462,12 @@ const TREES_GOAL: Dictionary<TypeMapValue> = {
   years: {
     title: "Years",
     typeMap: GOAL_YEARS,
-    balanced: true
+    balanced: false
   },
   strategy: {
     title: "Strategy",
     typeMap: GOAL_STRATEGY,
-    balanced: true
+    balanced: false
   }
 };
 
