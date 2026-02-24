3333044b1f98a9f3ac27b25e1429a1a4b89c4a29
Dotty <dottykurmi123@gmail.com>
2026-02-24 18:33:08 -0400

    [TM-3025] Update KeyIndicatorsInsights to use Seedlings icon and add SeedlingsCircle component. Refactor icon imports for consistency across the application.

--- src/redesignComponents/foundations/Icons/Seedlings.tsx
+++ src/redesignComponents/foundations/Icons/Seedlings.tsx
@@ -1,7 +1,7 @@
 import { Icon, IconProps } from "@chakra-ui/react";
 import React from "react";
 
-export const Seeds = (props: IconProps) => (
+export const Seedlings = (props: IconProps) => (
   <Icon {...props}>
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_12964_15719)">
