61347db1b4cf8bdbd8ed673b00cea6d6c6012502
Dotty <dottykurmi123@gmail.com>
2026-02-24 10:17:43 -0400

    [TM-2889] Update TeamMembersTab role label and enhance Table component styles

--- src/redesignComponents/dataDisplay/Table/Table.tsx
+++ src/redesignComponents/dataDisplay/Table/Table.tsx
@@ -182,7 +182,16 @@ const Table: FC<TableProps> = ({
 
   return (
     <Box
-      css={getTableWrapperStyles(sortColumn, columns, selectable, isScrollable, scrollableWidth, scrollableHeight, css)}
+      css={getTableWrapperStyles(
+        sortColumn,
+        columns,
+        selectable,
+        isScrollable,
+        scrollableWidth,
+        scrollableHeight,
+        dataByPage,
+        css
+      )}
       className={className}
     >
       <WriTable
@@ -196,14 +205,14 @@ const Table: FC<TableProps> = ({
           totalItems: actualTotalItems,
           currentPage,
           pageSize,
-          showItemCount
+          showItemCount: actualTotalItems > 0 ? showItemCount : false
         }}
         onAllItemsSelected={selectable ? handleAllItemsSelected : undefined}
         selectedRows={selectedRows}
         selectable={selectable}
         variant={variant}
       />
-      {showItemCount && (
+      {showItemCount && actualTotalItems > 0 && (
         <Text
           textStyle="500"
           fontWeight="400"
