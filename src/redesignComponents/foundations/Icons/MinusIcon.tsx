adeac9f0e3ec52edbb074834f415800cf93dd58b
diego-morales-flores-1996 <diego@vizonomy.com>
2026-02-24 16:48:21 -0400

    [TM-2999] standardize icons name

---
+++ src/redesignComponents/foundations/Icons/MinusIcon.tsx
@@ -0,0 +1,12 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+import type { FC } from "react";
+
+export const MinusIcon: FC<IconProps> = props => (
+  <Icon {...props}>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 13.5V10.5H24V13.5H0Z" fill="currentColor" />
+    </svg>
+  </Icon>
+);
+
+export default MinusIcon;
---
+++ src/redesignComponents/foundations/Icons/NothingReportedIcon.tsx
@@ -0,0 +1,28 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+import React, { FC } from "react";
+
+export const NothingReportedIcon: FC<IconProps> = (props: IconProps) => (
+  <Icon {...props}>
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <mask
+        id="mask0_13685_35160"
+        style={{ maskType: "alpha" }}
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="16"
+        height="16"
+      >
+        <rect width="16" height="16" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_13685_35160)">
+        <path
+          d="M8 16C6.89333 16 5.85333 15.79 4.88 15.37C3.90667 14.95 3.06 14.38 2.34 13.66C1.62 12.94 1.05 12.0933 0.63 11.12C0.21 10.1467 0 9.10667 0 8C0 6.89333 0.21 5.85333 0.63 4.88C1.05 3.90667 1.62 3.06 2.34 2.34C3.06 1.62 3.90667 1.05 4.88 0.63C5.85333 0.21 6.89333 0 8 0C9.10667 0 10.1467 0.21 11.12 0.63C12.0933 1.05 12.94 1.62 13.66 2.34C14.38 3.06 14.95 3.90667 15.37 4.88C15.79 5.85333 16 6.89333 16 8C16 9.10667 15.79 10.1467 15.37 11.12C14.95 12.0933 14.38 12.94 13.66 13.66C12.94 14.38 12.0933 14.95 11.12 15.37C10.1467 15.79 9.10667 16 8 16ZM11.94 13.06C12.1533 12.9 12.3533 12.7267 12.54 12.54C12.7267 12.3533 12.9 12.1533 13.06 11.94L4.06 2.94C3.84667 3.1 3.64667 3.27333 3.46 3.46C3.27333 3.64667 3.1 3.84667 2.94 4.06L11.94 13.06Z"
+          fill="currentColor"
+        />
+      </g>
+    </svg>
+  </Icon>
+);
+
+export default NothingReportedIcon;
