b820906ccf10cc0e37a185b6f14dbac0242238da
Dotty <dottykurmi123@gmail.com>
2026-02-24 17:17:28 -0400

    [TM-2990] Refactor class names in ProfileListCard for improved layout consistency and alignment. Adjusted order of class names for better readability.

--- src/redesignComponents/content/ContentCard/ProfileListCard/ProfileListCard.tsx
+++ src/redesignComponents/content/ContentCard/ProfileListCard/ProfileListCard.tsx
@@ -77,7 +77,7 @@ const ProfileSection: FC<IProfileListCardProps> = ({ title, profiles, onProfileC
                 backgroundColor="transparent"
                 color="secondary.900"
                 width="auto"
-                className="group-hover:bg-theme-primary-500/20 flex items-center gap-1"
+                className="group-hover:bg-theme-primary-500/20 flex items-center gap-1 px-2 py-1.5"
               >
                 {t("Invite Team Member")}
                 <ChevronRight color="neutral.800" className="h-2.5 w-2.5" />
