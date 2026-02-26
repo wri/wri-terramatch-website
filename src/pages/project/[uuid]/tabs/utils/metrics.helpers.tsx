537a3d34f364b9141174fe0c9890a86bad1bf841
Dotty <dottykurmi123@gmail.com>
2026-02-26 10:05:57 -0400

    [TM-2897] Remove unused metrics configuration, helper, and utility files to streamline project codebase.

--- src/pages/project/[uuid]/tabs/utils/metrics.helpers.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from "@chakra-ui/react";
-import { ReactNode } from "react";
-
-export const renderMetricTooltip = (content: ReactNode) => (
-  <Box fontSize="14px" lineHeight="20px">
-    {content}
-  </Box>
-);
--- src/pages/project/[uuid]/tabs/utils/metrics.utils.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectFullDto } from "@/generated/v3/entityService/entityServiceSchemas";
-
-export const calculateTreesRestored = (project: ProjectFullDto) =>
-  (project.treesPlantedCount ?? 0) + (project.regeneratedTreesCount ?? 0) + (project.seedsPlantedCount ?? 0);
-
-export const calculateHectaresPercentage = (restored: number, goal: number) =>
-  goal > 0 ? Math.round((restored / goal) * 100) : undefined;
