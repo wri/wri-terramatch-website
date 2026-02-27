47f86af61c550a2874bc147ca2004c90d6977837
cesarLima1 <105736261+cesarLima1@users.noreply.github.com>
2026-02-27 15:38:40 -0400

    [TM-2875] replace multi endpoints for projects, sites, nurseries and organizations (#1983)

--- src/admin/apiProvider/dataProviders/projectDataProvider.ts
+++ src/admin/apiProvider/dataProviders/projectDataProvider.ts
@@ -1,32 +1,25 @@
-import { DataProvider } from "react-admin";
+import { DataProvider, GetManyParams, GetManyResult } from "react-admin";
 
 import { deleteProject, loadFullProject, loadProjectIndex } from "@/connections/Entity";
-import { fetchGetV2AdminProjectsMulti, GetV2AdminProjectsMultiError } from "@/generated/apiComponents";
+import { ProjectFullDto } from "@/generated/v3/entityService/entityServiceSchemas";
 
-import { getFormattedErrorForRA } from "../utils/error";
+import { v3ErrorForRA } from "../utils/error";
 import { connectionDataProvider } from "../utils/listing";
 
 export const projectDataProvider: Partial<DataProvider> = {
   ...connectionDataProvider("Project", loadProjectIndex, loadFullProject, deleteProject),
 
-  // @ts-ignore
-  async getMany(_, params) {
-    try {
-      const response = await fetchGetV2AdminProjectsMulti({
-        queryParams: {
-          ids: params.ids.join(",")
-        }
-      });
-
-      return {
-        // @ts-ignore
-        data: response.data?.map(item => ({
-          ...item,
-          id: item.uuid
-        }))
-      };
-    } catch (err) {
-      throw getFormattedErrorForRA(err as GetV2AdminProjectsMultiError);
+  async getMany(_: string, params: GetManyParams): Promise<GetManyResult> {
+    const results = await Promise.all(params.ids.map(id => loadFullProject({ id: id as string })));
+    const failed = results.find(r => r.loadFailure != null);
+    if (failed != null) {
+      throw v3ErrorForRA("Project get fetch failed", failed.loadFailure);
     }
+
+    const data = results
+      .map(r => (r.data != null ? { ...r.data, id: r.data.uuid } : null))
+      .filter((item): item is ProjectFullDto & { id: string } => item != null);
+
+    return { data } as GetManyResult;
   }
 };
