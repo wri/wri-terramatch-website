a927f767f713cd5efbb023c3e55d23816ff039cb
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2026-03-02 15:22:52 -0400

    [TM-2947] clear cache on edit report

--- src/pages/entity/[entityName]/edit/[uuid]/EditEntityForm.tsx
+++ src/pages/entity/[entityName]/edit/[uuid]/EditEntityForm.tsx
@@ -77,6 +77,7 @@ const EditEntityForm = ({ entityName, entityUUID }: EditEntityFormProps) => {
 
   const submitEntity = useCallback(() => {
     updateEntity({ status: "awaiting-approval" });
+    ApiSlice.pruneCache("actions");
   }, [updateEntity]);
   useRequestSuccess(
     isSubmitting,
