6f1b24623c944e934ff33b1d3210547560f9f684
Dotnara Condori <dottykurmi123@gmail.com>
2026-03-04 10:16:52 -0400

    [TM-2887] Remove image upload placeholder in ProjectHeader component (#2008)

--- src/redesignComponents/content/headers/PageHeaders/ProjectHeader.tsx
+++ src/redesignComponents/content/headers/PageHeaders/ProjectHeader.tsx
@@ -5,17 +5,12 @@ import { useGadmOptions } from "@/connections/Gadm";
 import { useUserAssociations } from "@/connections/UserAssociation";
 import { ProjectFullDto } from "@/generated/v3/entityService/entityServiceSchemas";
 import {
-  IMAGE_CONTAINER_CLASSES,
-  IMAGE_SIZE
-} from "@/redesignComponents/content/headers/PageHeaders/constants/projectHeader";
-import {
   countryCodeToFlag,
   formatMonthYear,
   mapPlantingStatusToProgressState
 } from "@/redesignComponents/content/headers/PageHeaders/utils/projectHeader";
 import { formatOptionsList } from "@/utils/options";
 
-import ProfileImage from "../../Images/ProfileImage/ProfileImage";
 import ProjectInfo from "./components/ProjectInfo";
 import TeamSection from "./components/TeamSection";
 
@@ -45,9 +40,11 @@ const ProjectHeader: FC<ProjectHeaderProps> = ({ project, onAddTeamClick, gotoTe
   return (
     <Box display="flex" gap={4} px={6} py={5} justifyContent="space-between" background="secondary.neutral">
       <Flex gap={5}>
-        <div className={IMAGE_CONTAINER_CLASSES}>
+        {/* TODO: Add back in when we have a way to upload images */}
+
+        {/* <div className={IMAGE_CONTAINER_CLASSES}>
           <ProfileImage size={IMAGE_SIZE} alt={project.name ?? ""} isAdd />
-        </div>
+        </div> */}
 
         <ProjectInfo
           project={project}
