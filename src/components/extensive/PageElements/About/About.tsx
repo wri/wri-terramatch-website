bb9083bd43c50ab84134bfd291be61eede5333d7
diego-morales-flores-1996 <diego@vizonomy.com>
2026-03-09 09:59:19 -0400

    [TM-3068] add actions to nursery set up

--- src/components/extensive/PageElements/About/About.tsx
+++ src/components/extensive/PageElements/About/About.tsx
@@ -20,13 +20,13 @@ const About: FC<AboutProps> = ({ title, description, links }) => {
   return (
     <Flex direction="column" gap={2} padding={5} backgroundColor="neutral.100" borderRadius={1} minHeight={0}>
       {title && (
-        <Text color="neutral.900" textStyle="400">
+        <Text color="neutral.900" textStyle="400-bold">
           {title}
         </Text>
       )}
       {description}
       <Flex direction="column" gap={2} minHeight={0}>
-        <Text color="neutral.900" textStyle="500">
+        <Text color="neutral.900" textStyle="500-bold">
           {t("Helpful Links")}
         </Text>
         <SimpleDivider />
