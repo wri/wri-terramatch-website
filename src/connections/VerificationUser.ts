48458dc4d7a7f8ae8bf270661ff1c306e3d396f6
Dotnara Condori <dottykurmi123@gmail.com>
2026-03-16 18:08:13 -0400

    [TM-3086] Date range comment (#2079)
    * [TM-3086] Enhance DateRangeInput component: add date parsing functionality, improve locale handling, and update input styling for better readability.
    
    * update user service schemas
    
    * [TM-3086] Update selectVerificationUser to use VerificationUserResponseDto type and improve isSuccess handling
    
    * [TM-3086] Refactor DateRangeInput component: specify return type for parse function to enhance type safety.
    
    ---------
    
    Co-authored-by: LimberHope <limbermv2@gmail.com>

--- src/connections/VerificationUser.ts
+++ src/connections/VerificationUser.ts
@@ -2,11 +2,13 @@ import { createSelector } from "reselect";
 
 import { connectionHook } from "@/connections/util/connectionShortcuts";
 import { verifyUser } from "@/generated/v3/userService/userServiceComponents";
+import { VerificationUserResponseDto } from "@/generated/v3/userService/userServiceSchemas";
 import { ApiDataStore, PendingError } from "@/store/apiSlice";
 import { Connection } from "@/types/connection";
 import { selectorCache } from "@/utils/selectorCache";
 
-export const selectVerificationUser = (store: ApiDataStore) => Object.values(store.verifications)?.[0]?.attributes;
+export const selectVerificationUser = (store: ApiDataStore) =>
+  Object.values(store.verifications)?.[0]?.attributes as VerificationUserResponseDto | undefined;
 
 type VerificationUserConnection = {
   isLoading: boolean;
@@ -35,7 +37,7 @@ const verificationUserConnection: Connection<VerificationUserConnection, Verific
         (isLoading, requestFailed, selector) => ({
           isLoading,
           requestFailed,
-          isSuccess: selector?.verified
+          isSuccess: selector != null ? selector.verified : null
         })
       )
   )
