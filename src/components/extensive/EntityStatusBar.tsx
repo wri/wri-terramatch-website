5b68aa5f19e308b66a267724a0faca4cc40c47c5
Dotnara Condori <dottykurmi123@gmail.com>
2026-03-17 10:48:56 -0400

    [TM-3068] Nursery Page component (#2080)
    * [TM-3068] Enhance ManagerType component by adding tooltips for nursery types, improving user experience with detailed descriptions. Refactor icon and label structure for better readability and maintainability.
    
    * [TM-3068] Refactor EntityStatusBar and introduce EntityStatusModal for improved status handling. Update NurseryOverviewTab and NurseryInfo components to utilize the new modal for displaying entity status feedback, enhancing user experience and code maintainability.

--- src/components/extensive/EntityStatusBar.tsx
+++ src/components/extensive/EntityStatusBar.tsx
@@ -1,16 +1,15 @@
 import { useT } from "@transifex/react";
-import { kebabCase } from "lodash";
-import Link from "next/link";
 import { FC, useCallback, useMemo } from "react";
 
 import Button from "@/components/elements/Button/Button";
 import StatusBar from "@/components/elements/StatusBar/StatusBar";
 import { IconNames } from "@/components/extensive/Icon/Icon";
-import Modal from "@/components/extensive/Modal/Modal";
 import { ModalId } from "@/components/extensive/Modal/ModalConst";
 import { EntityFullDto } from "@/connections/Entity";
 import { FormEntity } from "@/connections/Form";
 import { useModalContext } from "@/context/modal.provider";
+
+import EntityStatusModal, { StatusProps } from "./EntityStatusModal";
 
 type EntityStatusBarProps = {
   entityName: FormEntity;
@@ -29,7 +28,6 @@ type StatusBarStatus = keyof typeof StatusMapping;
 const hasUpdateRequest = ({ updateRequestStatus }: EntityFullDto) =>
   !["draft", "no-update", "approved"].includes(updateRequestStatus ?? "");
 
-type StatusProps = { title: string; icon: IconNames; className: string };
 const getStatusProps = (
   t: typeof useT,
   entity: EntityFullDto,
@@ -65,7 +63,7 @@ const getStatusProps = (
 
 const EntityStatusBar: FC<EntityStatusBarProps> = ({ entityName, entity }) => {
   const t = useT();
-  const { openModal, closeModal } = useModalContext();
+  const { openModal } = useModalContext();
 
   const entityStatus = (hasUpdateRequest(entity) ? entity.updateRequestStatus : entity.status) as StatusBarStatus;
   const needMoreInformation = entityStatus === "needs-more-information";
@@ -78,37 +76,15 @@ const EntityStatusBar: FC<EntityStatusBarProps> = ({ entityName, entity }) => {
     if (statusProps == null) return;
     openModal(
       ModalId.STATUS,
-      <Modal
-        className="min-w-[500px]"
-        iconProps={{ name: statusProps.icon, width: 60, height: 60, className: statusProps.className }}
-        title={statusProps.title}
-        content={entity.feedback ?? t("No feedback provided")}
-        primaryButtonProps={
-          needMoreInformation
-            ? {
-                as: Link,
-                children: t("Provide Feedback"),
-                href: `/entity/${kebabCase(entityName)}/edit/${entity.uuid}?mode=provide-feedback-entity`,
-                onClick: () => {
-                  closeModal(ModalId.STATUS);
-                }
-              }
-            : {
-                children: t("Close"),
-                onClick: () => closeModal(ModalId.STATUS)
-              }
-        }
-        secondaryButtonProps={
-          needMoreInformation
-            ? {
-                children: t("Close"),
-                onClick: () => closeModal(ModalId.STATUS)
-              }
-            : undefined
-        }
+      <EntityStatusModal
+        statusProps={statusProps}
+        feedback={entity.feedback}
+        needMoreInformation={needMoreInformation}
+        entityName={entityName}
+        entityUuid={entity.uuid}
       />
     );
-  }, [closeModal, entity.feedback, entity.uuid, entityName, needMoreInformation, openModal, statusProps, t]);
+  }, [entity.feedback, entity.uuid, entityName, needMoreInformation, openModal, statusProps]);
 
   return projectedEntityStatus == null ? null : (
     <StatusBar status={projectedEntityStatus} title={statusProps?.title ?? ""}>
