7e01a305f0417f3e037383db1f065de0d710fb85
diego-morales-flores-1996 <diego@vizonomy.com>
2026-03-25 12:00:16 -0400

    [TM-3103] add map placeholder

--- src/components/extensive/PageElements/KeyIndicatorsInsightsRow/KeyIndicatorsInsightsRow.tsx
+++ src/components/extensive/PageElements/KeyIndicatorsInsightsRow/KeyIndicatorsInsightsRow.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import { FC, ReactNode } from "react";
 
 export const METRIC_CARD_CLASS_NAME = classNames(
-  "flex-1 max-w-[calc((100%/2)-6px)] ws-1100:max-w-[calc((100%/3)-6px)] md:!max-w-[calc((100%/4)-6px)] lg:!max-w-[calc((100%/4)-1rem)] w-[350px]"
+  "flex-1 max-w-[calc((100%/2)-6px)] ws-1100:max-w-[calc((100%/3)-6px)] md:!max-w-[calc((100%/4)-6px)] lg:!max-w-[calc((100%/4)-1rem)] w-[350px] h-auto"
 );
 
 interface KeyIndicatorsInsightsRowProps {
