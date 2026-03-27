50e680d402e88efee3fd83c3d86cf9a69937da4e
diego-morales-flores-1996 <diego@vizonomy.com>
2026-03-27 16:22:18 -0400

    test commit

--- src/redesignComponents/content/Images/Image.tsx
+++ src/redesignComponents/content/Images/Image.tsx
@@ -54,8 +54,6 @@ const BaseImage: FC<BaseImageProps> = ({
 
   const showNotAvailable = src == null || loadError;
 
-  console.log(loadError);
-
   const hoverContentComponent = (
     <div
       className={classNames(
