7118f99298aa7906759c75c8aef36697795ed037
diego-morales-flores-1996 <diego@vizonomy.com>
2026-03-30 12:03:52 -0400

    [TM-3148] add use t

--- src/components/extensive/PageElements/PageContent/components/DocumentsSection.tsx
+++ src/components/extensive/PageElements/PageContent/components/DocumentsSection.tsx
@@ -4,18 +4,21 @@ import { FC } from "react";
 import { ParsedFile } from "@/components/extensive/WizardForm/FormSummaryRow/parseFilesFromHtml";
 import AttachFileItem from "@/pages/project/[uuid]/components/AttachFileItem";
 import { DocumentIcon } from "@/redesignComponents/foundations/Icons";
+import { useT } from "@transifex/react";
 
 type DocumentsSectionProps = {
   files: ParsedFile[];
   showTitle?: boolean;
 };
 
-const DocumentsSection: FC<DocumentsSectionProps> = ({ files, showTitle = false }) => (
+const DocumentsSection: FC<DocumentsSectionProps> = ({ files, showTitle = false }) =>{
+  const t = useT();
+  return (
   <Flex direction="column" gap={3}>
     {showTitle && (
       <Text display="flex" alignItems="center" gap={1} lineHeight="normal" textStyle="300-bold" color="primary.900">
         {<DocumentIcon boxSize={3.5} color="primary.900" />}
-        Documents
+        {t("Documents")}
       </Text>
     )}
     <Grid templateColumns={files.length === 1 ? "repeat(1, minmax(0, 1fr))" : "repeat(2, minmax(0, 1fr))"} gap={4}>
@@ -29,6 +32,7 @@ const DocumentsSection: FC<DocumentsSectionProps> = ({ files, showTitle = false 
       ))}
     </Grid>
   </Flex>
-);
+  );
+};
 
 export default DocumentsSection;
