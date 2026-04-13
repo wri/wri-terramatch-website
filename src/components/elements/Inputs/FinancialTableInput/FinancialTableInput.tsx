0e077ce90709d05290e2e273e9a8fd1049057772
Nathan Curtis <nathan.curtis@gmail.com>
2026-04-13 12:24:37 -0700

    [TM-3081] Clean up props on FinancialTableInput

--- src/components/elements/Inputs/FinancialTableInput/FinancialTableInput.tsx
+++ src/components/elements/Inputs/FinancialTableInput/FinancialTableInput.tsx
@@ -1,10 +1,9 @@
 import { useT } from "@transifex/react";
-import { useId } from "react";
-import { Control, FieldValues, UseFormReturn } from "react-hook-form";
+import { FC, useId } from "react";
+import { Control, UseFormReturn } from "react-hook-form";
 
 import Table from "@/components/elements/Table/Table";
 
-// import { FormField } from "@/components/extensive/WizardForm/types";
 import InputWrapper, { InputWrapperProps } from "../InputElements/InputWrapper";
 
 export interface FinancialTableInputProps extends Omit<InputWrapperProps, "error"> {
@@ -18,27 +17,19 @@ export interface FinancialTableInputProps extends Omit<InputWrapperProps, "error
   onChange?: (values: any) => void;
   generateUuids?: boolean;
   additionalValues?: any;
-  formHook?: UseFormReturn<FieldValues, any>;
-  control?: Control<FieldValues, any>;
+  formHook?: UseFormReturn;
+  control?: Control;
   onChangeCapture?: (values: any) => void;
-  resetTable?: any;
+  resetTable?: number;
 }
 
-const FinancialTableInput = (props: FinancialTableInputProps) => {
+const FinancialTableInput: FC<FinancialTableInputProps> = ({ resetTable, tableColumns, value, ...props }) => {
   const id = useId();
   const t = useT();
 
   return (
-    <InputWrapper
-      inputId={id}
-      label={props.label ?? t("ADD FINANCIAL DATA")}
-      containerClassName={props.containerClassName}
-      description={props.description}
-      required={props.required}
-      feedbackRequired={props.feedbackRequired}
-      {...props}
-    >
-      <Table columns={props.tableColumns} data={props.value} {...props} key={props.resetTable} {...props} />
+    <InputWrapper inputId={id} {...props} label={props.label ?? t("ADD FINANCIAL DATA")}>
+      <Table key={resetTable} columns={tableColumns} data={value} />
     </InputWrapper>
   );
 };
