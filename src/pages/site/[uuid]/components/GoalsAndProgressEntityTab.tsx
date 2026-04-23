d25bd0a54976704a31ee9df15906f15d722a250a
LimberHope <limbermv2@gmail.com>
2026-04-23 10:25:27 -0400

    [TM-3262] add replanted seccion for site and project overview

--- src/pages/site/[uuid]/components/GoalsAndProgressEntityTab.tsx
+++ src/pages/site/[uuid]/components/GoalsAndProgressEntityTab.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ProgressGoalsDoughnutChart from "@/admin/components/ResourceTabs/MonitoredTab/components/ProgressGoalsDoughnutChart";
 import GoalProgressCard from "@/components/elements/Cards/GoalProgressCard/GoalProgressCard";
 import { IconNames } from "@/components/extensive/Icon/Icon";
+import { usePlantTotalCount } from "@/components/extensive/Tables/TreeSpeciesTable/hooks";
 import { Framework, isTerrafund } from "@/context/framework.provider";
 
 import {
@@ -247,6 +248,12 @@ const GoalsAndProgressEntityTab = ({ entity, project = false }: GoalsAndProgress
   };
   const frameworkKey = entity.frameworkKey as Framework;
   const framework = isTerrafund(frameworkKey) ? Framework.TF : frameworkKey;
+  const totalCountReplanting = usePlantTotalCount({
+    entity: project ? "projects" : "sites",
+    entityUuid: entity?.uuid,
+    collection: "replanting"
+  });
+
   return (
     <div className="flex w-full flex-wrap items-start justify-between gap-4">
       {chartsDataMapping[framework as keyof ChartsData]?.map((chart, index) => (
@@ -282,6 +289,13 @@ const GoalsAndProgressEntityTab = ({ entity, project = false }: GoalsAndProgress
             classNameLabel: " text-neutral-650 uppercase",
             value: entity.treesRegeneratingSpeciesCount,
             tooltipContent: project ? TOOLTIP_TREES_REGENERATING_PROJECT : TOOLTIP_TREES_REGENERATING_SITE
+          },
+          {
+            iconName: IconNames.LEAF_PLANTED_CIRCLE,
+            label: t("Trees Replanted:"),
+            variantLabel: "text-14",
+            classNameLabel: " text-neutral-650 uppercase",
+            value: totalCountReplanting
           }
         ]}
         className="pr-[41px] lg:pr-[150px] mobile:w-[400px] mobile:!pr-0"
