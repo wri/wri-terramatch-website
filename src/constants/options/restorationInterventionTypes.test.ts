c0f8c5bf6e426ae0cfe837ec4c1ba56f88a7c085
Nathan Curtis <nathan.curtis@gmail.com>
2026-04-30 15:03:50 -0700

    [TM-3347] Add option public images.

--- src/constants/options/restorationInterventionTypes.test.ts
+++
@@ -1,6 +0,0 @@
-import { getRestorationInterventionTypeOptions } from "@/constants/options/restorationInterventionTypes";
-
-test("snapShot test getRestorationInterventionTypeOptions", () => {
-  const options = getRestorationInterventionTypeOptions();
-  expect(options).toMatchSnapshot();
-});
