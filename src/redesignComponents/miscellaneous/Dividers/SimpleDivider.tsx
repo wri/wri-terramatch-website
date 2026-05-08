4807c1561fd73e0c17c5d40b57212362d5587181
Dotnara Condori <dottykurmi123@gmail.com>
2026-05-08 12:57:31 -0400

    [TM-3310] TM Design Components: Responsive Typography & Spacing — Geospatial Components (#2337)
    * [TM-3310] add redesign component containers and storybook documentation files
    
    * [TM-3310] add Storybook documentation for CheckboxList component

--- src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
+++ src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
@@ -2,7 +2,7 @@ import { Box, BoxProps } from "@chakra-ui/react";
 import React, { FC } from "react";
 
 const SimpleDivider: FC<BoxProps> = props => {
-  const { backgroundColor = "neutral.300", width = "100%", height = "1px", ...rest } = props;
+  const { backgroundColor = "neutral.300", width = "100%", height = "0.0625rem", ...rest } = props;
   return <Box width={width} height={height} backgroundColor={backgroundColor} {...rest} />;
 };
 
