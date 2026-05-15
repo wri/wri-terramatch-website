88ae00c5dbcc4d1a8d3f01cb37bdabcd498be909
Dotnara Condori <dottykurmi123@gmail.com>
2026-05-15 10:01:18 -0400

    [TM-3266] implement BulkActionToolbar and base Toolbar components with Storybook documentation (#2377)

--- src/redesignComponents/navigation/Toolbar/Toolbar.tsx
+++ src/redesignComponents/navigation/Toolbar/Toolbar.tsx
@@ -3,10 +3,19 @@ import { FC } from "react";
 
 import { ToolbarProps } from "./ToolBar.type";
 
-const Toolbar: FC<ToolbarProps> = ({ contentLeft, contentRight, className, classNameContentRight }) => {
+const Toolbar: FC<ToolbarProps> = ({
+  contentLeft,
+  contentRight,
+  className,
+  classNameContentRight,
+  contentCenter,
+  classNameContentLeft,
+  classNameContentCenter
+}) => {
   return (
     <div className={classNames("flex items-center justify-between bg-white px-4", className)}>
-      <div className="min-w-0 flex-1">{contentLeft}</div>
+      <div className={classNames("flex-shrink-0", classNameContentLeft)}>{contentLeft}</div>
+      <div className={classNameContentCenter}>{contentCenter}</div>
       <div className={classNames("flex-shrink-0", classNameContentRight)}>{contentRight}</div>
     </div>
   );
