d3d9ccae8e50f0744f049b4a2b461bf96685135a
Marcolr-dev <marc.antoni.lr@gmail.com>
2026-05-28 16:13:14 -0400

    [TM-3450] Refactor table utility functions to enforce number types for formatting and comparison, enhancing numeric sorting functionality.

--- src/components/elements/Table/tableUtils.ts
+++ src/components/elements/Table/tableUtils.ts
@@ -1,9 +1,9 @@
 import { Row, RowData } from "@tanstack/react-table";
 
-export const formatTableNumber = (value: unknown, maximumFractionDigits = 0): string =>
-  Number(value ?? 0).toLocaleString("en-US", { maximumFractionDigits });
+export const formatTableNumber = (value: number, maximumFractionDigits = 0): string =>
+  value.toLocaleString("en-US", { maximumFractionDigits });
 
-export const compareNumericValues = (a: unknown, b: unknown): number => Number(a ?? 0) - Number(b ?? 0);
+export const compareNumericValues = (a: number, b: number): number => a - b;
 
 export const numericSortingFn = <TData extends RowData>(rowA: Row<TData>, rowB: Row<TData>, columnId: string): number =>
-  compareNumericValues(rowA.getValue(columnId), rowB.getValue(columnId));
+  compareNumericValues(Number(rowA.getValue(columnId) ?? 0), Number(rowB.getValue(columnId) ?? 0));
