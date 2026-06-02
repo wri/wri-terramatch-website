3fa70408b52720e37422f0ff855e93798c353b5d
Diego <diego@vizonomy.com>
2026-06-02 10:24:05 -0400

    [TM-3483] [TM-3283] create message input and card

--- src/redesignComponents/Forms/Inputs/Textarea.tsx
+++ src/redesignComponents/Forms/Inputs/Textarea.tsx
@@ -1,7 +1,22 @@
+import { Box } from "@chakra-ui/react";
+import { css } from "@emotion/react";
 import { Textarea as WriTextarea } from "@worldresources/wri-design-systems";
-import type { ComponentProps } from "react";
-import { FC } from "react";
+import type { ComponentProps, FC } from "react";
 
-const Textarea: FC<ComponentProps<typeof WriTextarea>> = props => <WriTextarea {...props} />;
+type WriTextareaProps = ComponentProps<typeof WriTextarea>;
+
+export type TextareaProps = WriTextareaProps;
+
+const Textarea: FC<TextareaProps> = ({ ...rest }) => (
+  <Box
+    css={css`
+      & > div {
+        margin-bottom: 0 !important;
+      }
+    `}
+  >
+    <WriTextarea {...rest} />
+  </Box>
+);
 
 export default Textarea;
