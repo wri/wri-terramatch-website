512235f1084d12ccbeb137ffc981decb97420682
Dotnara Condori <dottykurmi123@gmail.com>
2026-06-02 09:38:08 -0400

    [TM-3366] Update validation messages and UI components in PolygonBulk… (#2471)
    * [TM-3351] update site and nursery tables (#2466)
    
    * [TM-3476] Enhance GoalProgressCardItem to support optional icon prop … (#2469)
    
    * [TM-3476] Enhance GoalProgressCardItem to support optional icon prop and update GoalsAndProgressTab to use SeedlingsCircleIcon for improved visual representation of seedling growth.
    
    * [TM-3476] Update GoalProgressCardItem to require iconName prop and add SEEDLINGS_CIRCLE_PD icon to IconNames enum; modify GoalsAndProgressTab to utilize new icon for seedling growth display.
    
    * [TM-3447] add new fields for site polygons (#2470)
    
    * [TM-3366] Update validation messages and UI components in PolygonBulkActionToolbar and ValidationSection. Adjusted toast message for polygon validation based on selection count, modified icon sizes in ValidationDetail, and enhanced layout in ValidationSection for better visual clarity and user interaction.
    
    * [TM-3366] Add new fields to SitePolygonLightDto in MapPolygonPanel stories
    
    * [TM-3366] Add projectCountry and ppcExternalId fields to SitePolygonLightDto in MapSidePanel stories
    
    ---------
    
    Co-authored-by: Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
    Co-authored-by: Marcolr-dev <80992287+Marcolr-dev@users.noreply.github.com>

--- src/pages/project/[uuid]/tabs/GoalsAndProgress.tsx
+++ src/pages/project/[uuid]/tabs/GoalsAndProgress.tsx
@@ -265,7 +265,7 @@ const GoalsAndProgressTab = ({ project }: GoalsAndProgressProps) => {
                   classNameCard="!pl-0"
                   items={[
                     {
-                      iconName: IconNames.NURSERY_CIRCLE,
+                      iconName: IconNames.SEEDLINGS_CIRCLE_PD,
                       label: t("Number of Seedlings Growing:"),
                       variantLabel: "text-14",
                       classNameLabel: " text-neutral-650 uppercase !w-auto",
