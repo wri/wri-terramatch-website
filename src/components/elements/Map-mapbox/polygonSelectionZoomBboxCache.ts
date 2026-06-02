54c90afd3078064d75babd0304d032fa7134bf2a
JORGE <egrojmonroy@gmail.com>
2026-06-02 11:47:58 -0400

    [TM-3363] remove comments

--- src/components/elements/Map-mapbox/polygonSelectionZoomBboxCache.ts
+++ src/components/elements/Map-mapbox/polygonSelectionZoomBboxCache.ts
@@ -1,6 +1,5 @@
 import type { BBox } from "./GeoJSON";
 
-/** Client cache for fitBounds on selection; keyed by sorted polygon UUID selection key. */
 export const polygonSelectionZoomBboxCache = new Map<string, BBox | null>();
 
 export const clearPolygonSelectionZoomBboxCache = () => {
