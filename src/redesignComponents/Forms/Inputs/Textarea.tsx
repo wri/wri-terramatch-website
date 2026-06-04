2b226cba4be6eb88b0402e0cdc5935b91304f880
Diego <diego@vizonomy.com>
2026-06-04 12:13:16 -0400

    [TM-3483] [TM-3283] fix margion text area

--- src/redesignComponents/Forms/Inputs/Textarea.tsx
+++ src/redesignComponents/Forms/Inputs/Textarea.tsx
@@ -1,5 +1,4 @@
-import { Box } from "@chakra-ui/react";
-import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { Textarea as WriTextarea } from "@worldresources/wri-design-systems";
 import type { ComponentProps, FC } from "react";
 
@@ -7,16 +6,28 @@ type WriTextareaProps = ComponentProps<typeof WriTextarea>;
 
 export type TextareaProps = WriTextareaProps;
 
+const StyledWrapper = styled.div`
+  & textarea {
+    margin-top: 0 !important;
+  }
+  ,
+  & > div {
+    margin-bottom: 0 !important;
+  }
+  ,
+  & > div > div:has(label, span) textarea {
+    margin-top: 0.5rem !important;
+  }
+
+  & > div > div:not(:has(label, span)) textarea {
+    margin-top: 0 !important;
+  }
+`;
+
 const Textarea: FC<TextareaProps> = ({ ...rest }) => (
-  <Box
-    css={css`
-      & > div {
-        margin-bottom: 0 !important;
-      }
-    `}
-  >
+  <StyledWrapper>
     <WriTextarea {...rest} />
-  </Box>
+  </StyledWrapper>
 );
 
 export default Textarea;
