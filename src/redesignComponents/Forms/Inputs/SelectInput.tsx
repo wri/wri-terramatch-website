bf1c30d3951e1a0347e8f7f6488afa5b5ab10488
Diego <diego@vizonomy.com>
2026-06-04 17:47:27 -0400

    [TM-3403] Design Bulk Attributes Editing panel for polygons

--- src/redesignComponents/Forms/Inputs/SelectInput.tsx
+++ src/redesignComponents/Forms/Inputs/SelectInput.tsx
@@ -1,7 +1,24 @@
+import styled from "@emotion/styled";
 import { Select as WriSelect } from "@worldresources/wri-design-systems";
 import type { ComponentProps } from "react";
 import { FC } from "react";
 
-const SelectInput: FC<ComponentProps<typeof WriSelect>> = props => <WriSelect {...props} />;
+const StyledWrapper = styled.div`
+  & {
+    width: 100%;
+  }
+  & label:is(:disabled, [disabled], [data-disabled], [aria-disabled="true"]) {
+    opacity: 1;
+  }
+  & button:is(: disabled, [disabled], [data-disabled], [aria-disabled="true"]) {
+    opacity: 1;
+  }
+`;
+
+const SelectInput: FC<ComponentProps<typeof WriSelect>> = props => (
+  <StyledWrapper>
+    <WriSelect {...props} />
+  </StyledWrapper>
+);
 
 export default SelectInput;
