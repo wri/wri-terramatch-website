28c02413b3d63fbbe3a5c398bb1365d26d1afcb8
JORGE <egrojmonroy@gmail.com>
2026-06-15 16:40:02 -0400

    [TM-3423] remove comments

--- src/pages/site/[uuid]/constants/sitePolygonMapSizing.ts
+++ src/pages/site/[uuid]/constants/sitePolygonMapSizing.ts
@@ -1,5 +1,4 @@
 import { resolveRemSizeValue } from "@/lib/sizing";
 
-// Matches ResizeBox initialHeight in SitePolygonMapSection (100 * 0.25rem = 25rem).
 export const SITE_POLYGON_MAP_INITIAL_HEIGHT_UNITS = 100;
 export const SITE_POLYGON_MAP_INITIAL_HEIGHT = resolveRemSizeValue(SITE_POLYGON_MAP_INITIAL_HEIGHT_UNITS);
