085ebcbd62dba6870a679fb7c1784f525a89c71b
Dotty <dottykurmi123@gmail.com>
2026-06-17 12:40:05 -0400

    [TM-3520] Refactor polygon submission modals and improve toast notifications

--- src/pages/site/[uuid]/components/PolygonEditContent.tsx
+++ src/pages/site/[uuid]/components/PolygonEditContent.tsx
@@ -50,7 +50,6 @@ import { getSingleSitePolygonSubmitTooltip, isSitePolygonSubmittable } from "@/u
 
 import {
   closePolygonProgressToast,
-  getDeletingProgressLabel,
   getDownloadingPolygonsProgressLabel,
   getPolygonOperationToastLabels,
   getSubmittingProgressLabel,
@@ -659,7 +658,6 @@ const PolygonEditContent: FC<PolygonEditContentProps> = ({
     }
 
     onDeletingChange?.(true, 1);
-    showPolygonProgressToast(t, getDeletingProgressLabel(t, 1), POLYGON_TOAST_IDS.deleting);
 
     try {
       await deleteSitePolygon(polygon.uuid);
