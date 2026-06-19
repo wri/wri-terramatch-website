0a1373a07864acb3eef2f1dc78845159fee0f836
Diego <diego@vizonomy.com>
2026-06-19 17:30:14 -0400

    [TM-3525] add floating action toolbar

--- src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
+++ src/redesignComponents/miscellaneous/Dividers/SimpleDivider.tsx
@@ -1,9 +1,22 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 import React, { FC } from "react";
+import { twMerge } from "tailwind-merge";
 
-const SimpleDivider: FC<BoxProps> = props => {
-  const { backgroundColor = "neutral.300", width = "100%", height = "0.0625rem", ...rest } = props;
-  return <Box width={width} height={height} backgroundColor={backgroundColor} {...rest} />;
+export interface SimpleDividerProps extends BoxProps {
+  variant?: "horizontal" | "vertical";
+}
+
+const SimpleDivider: FC<SimpleDividerProps> = props => {
+  const { variant = "horizontal", backgroundColor = "neutral.300", width, height, className, ...rest } = props;
+  return (
+    <Box
+      className={twMerge("shrink-0", className)}
+      width={width != null ? width : variant === "horizontal" ? "100%" : "0.063rem"}
+      height={height != null ? height : variant === "vertical" ? "100%" : "0.063rem"}
+      backgroundColor={backgroundColor}
+      {...rest}
+    />
+  );
 };
 
 export default SimpleDivider;
