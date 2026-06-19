39db815dda90fe25f787827beb888c9374c48eb2
Dotty <dottykurmi123@gmail.com>
2026-06-19 18:05:15 -0400

    [TM-3556] Remove console log from Button component for cleaner code.

--- src/redesignComponents/actions/Buttons/Button/Button.tsx
+++ src/redesignComponents/actions/Buttons/Button/Button.tsx
@@ -16,7 +16,6 @@ export interface IButtonProps extends Omit<ButtonProps, "size" | "variant" | "co
 }
 
 const Button = ({ children, className, variant = "primary", ...props }: IButtonProps) => {
-  console.log("className", className);
   return (
     <WriButton variant={variant} {...props} className={classNames("shadow-monitored", className)}>
       {children}
