5b02bc61de04ababe9ab72b9916b290e3569be3a
LimberHope <limbermv2@gmail.com>
2026-06-23 10:06:50 -0400

    [TM-3587] rename progress and goals tab

--- src/pages/reports/nursery-report/tabs/GoalsAndProgress.tsx
+++ src/pages/reports/nursery-report/tabs/GoalsAndProgress.tsx
@@ -25,7 +25,7 @@ const GoalsAndProgressTab: FC<GoalsAndProgressTabProps> = ({ nurseryReport }) =>
   return (
     <PageBody className="!space-y-10 bg-theme-neutral-200 pt-5 text-darkCustom">
       <PageRow className="mx-0 w-full !max-w-full px-6">
-        <PageCard title={t("Progress & Goals")}>
+        <PageCard title={t("Key Indicators & Insights")}>
           <GoalsAndProgressReportEntityTab
             entity="nurseryReports"
             entityUuid={nurseryReport.uuid}
