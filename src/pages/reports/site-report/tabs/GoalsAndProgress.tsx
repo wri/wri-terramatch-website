fc5abe7b17281a629b0bb43e164439cfb681b243
Limber Mamani <154026979+LimberHope@users.noreply.github.com>
2026-06-24 10:50:05 -0400

    [TM-3587] udpate Key Indicators & Insights to Indicators & Insights (#2679)

--- src/pages/reports/site-report/tabs/GoalsAndProgress.tsx
+++ src/pages/reports/site-report/tabs/GoalsAndProgress.tsx
@@ -33,7 +33,7 @@ const GoalsAndProgressTab: FC<GoalsAndProgressTabProps> = ({ siteReport, site, w
   return (
     <PageBody className="!space-y-10 bg-theme-neutral-200 pt-5 text-darkCustom">
       <PageRow className="mx-0 w-full !max-w-full px-6">
-        <PageCard title={t("Key Indicators & Insights")}>
+        <PageCard title={t("Indicators & Insights")}>
           <GoalsAndProgressReportEntityTab
             entity="siteReports"
             entityUuid={siteReport.uuid}
