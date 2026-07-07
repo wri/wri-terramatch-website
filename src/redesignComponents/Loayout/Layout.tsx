882a0d6f16c2d30d743f2119c2ff2f32f174d88e
Dotnara Condori <dottykurmi123@gmail.com>
2026-07-07 08:57:28 -0400

    [TM-3529] Add Notifications and Messages icons to Layout component (#2760)

--- src/redesignComponents/Loayout/Layout.tsx
+++ src/redesignComponents/Loayout/Layout.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 import { UserIcon } from "../foundations/Icons/Function/UserIcon";
 import { DashboardIcon } from "../foundations/Icons/NavigationSections/DashboardIcon";
+import { MessagesIcon } from "../foundations/Icons/NavigationSections/MessagesIcon";
+import { NotificationIcon } from "../foundations/Icons/NavigationSections/NotificationIcon";
 import { NurseryIcon } from "../foundations/Icons/NavigationSections/NurseryIcon";
 import { OrganizationIcon } from "../foundations/Icons/NavigationSections/OrganizationIcon";
 import { ProgrammeIcon } from "../foundations/Icons/NavigationSections/ProgrammeIcon";
@@ -30,6 +32,20 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
           collapsed={true}
           isCollapsedDisabled={isSidebarCollapseDisabled}
           groups={[
+            {
+              links: [
+                {
+                  href: "#",
+                  icon: <NotificationIcon boxSize={4} />,
+                  label: "Notifications"
+                },
+                {
+                  href: "#",
+                  icon: <MessagesIcon boxSize={4} />,
+                  label: "Messages"
+                }
+              ]
+            },
             {
               links: [
                 {
