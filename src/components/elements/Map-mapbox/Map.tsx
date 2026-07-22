b2b5dba584c4c88ccd629c8a789f2d072b53e947
cesarLima1 <cesmaulim@gmail.com>
2026-07-22 16:04:38 -0400

    [TM-3743] keep polygon style when changing basemap on polygons tab

--- src/components/elements/Map-mapbox/Map.tsx
+++ src/components/elements/Map-mapbox/Map.tsx
@@ -424,6 +424,16 @@ const MapContainerInner: FC<MapContainerInnerProps> = ({
 
   const editFocus = useMapEditFocus({ polygonFromMap, editPolygon });
 
+  usePolygonTableHighlightStyle({
+    map,
+    styleReady,
+    styleVersion,
+    sourcesAdded,
+    tileLoadRequestId,
+    highlight: polygonTableHighlight,
+    editFocus
+  });
+
   usePolygonEditFocusStyle({
     map,
     styleReady,
@@ -432,15 +442,6 @@ const MapContainerInner: FC<MapContainerInnerProps> = ({
     tileLoadRequestId,
     isEditFocusActive: editFocus.isEditFocusActive,
     editedPolygonUuid: editFocus.editedPolygonUuid
-  });
-
-  usePolygonTableHighlightStyle({
-    map,
-    styleReady,
-    styleVersion,
-    sourcesAdded,
-    highlight: polygonTableHighlight,
-    editFocus
   });
 
   usePolygonSelectionZoom({
