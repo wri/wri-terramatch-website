6e995020d5dd23deee56d5ed8251b19d9c219c95
Dotty <dottykurmi123@gmail.com>
2026-07-28 14:46:33 -0400

    [TM-3710] Update MapPopUp component styles for improved layout and responsiveness

--- src/redesignComponents/geospatial/MapPopUp/MapPopUp.stories.tsx
+++ src/redesignComponents/geospatial/MapPopUp/MapPopUp.stories.tsx
@@ -82,7 +82,7 @@ export const Icon: Story = {
       </div>
     ),
     content: (
-      <div style={{ padding: "0.75rem" }}>
+      <div style={{ padding: "0.75rem", maxWidth: "18rem" }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit
         amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a,
         semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum
