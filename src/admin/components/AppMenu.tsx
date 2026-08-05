5808132fdd71dd9d12c44aeca78f149b41862374
Nathan Curtis <nathan.curtis@gmail.com>
2026-08-05 16:35:54 -0700

    [TM-3699] Quiet this error down if the resources aren't available yet.

--- src/admin/components/AppMenu.tsx
+++ src/admin/components/AppMenu.tsx
@@ -1,5 +1,6 @@
 import DefaultIcon from "@mui/icons-material/ViewList";
 import classNames from "classnames";
+import { isEmpty } from "lodash";
 import { createElement, FC } from "react";
 import { Menu, useGetResourceLabel, useResourceDefinitions } from "react-admin";
 import { useLocation } from "react-router-dom";
@@ -13,7 +14,9 @@ const FilterClearMenuItem: FC<{ resourceName: string }> = ({ resourceName }) => 
   const getResourceLabel = useGetResourceLabel();
   const resource = resources?.[resourceName];
   if (resource == null) {
-    Log.error("Resource not found for admin nav", { resourceName });
+    if (!isEmpty(resources)) {
+      Log.error("Resource not found for admin nav", { resourceName });
+    }
     return null;
   }
 
